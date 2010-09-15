201b3633270df10c5bb3229dff1cbdfe5673772b
Pierre-Emmanuel Viver <pierre-emmanuel.viver@libriciel.coop>
2010-09-15 15:35:35 +0000

    Problème d'encodage ISO <--> UTF8 sur les dates à partir d'un timestamp ou d'un format du type aaaa-mm-dd

--- public.ssl/modules/actes/actes_transac_add.php
+++ public.ssl/modules/actes/actes_transac_add.php
@@ -353,8 +353,7 @@ $html .= "    </script>\n";
 $html .= "    <a href=\"#datepicker\" id=\"datepicker_decision_date_link\" class=\"datepicker_link\" onclick=\"javascript:obj_decision_date.toggleDatePicker(); return false;\">";
 
 if ($decision_date) {
-  setlocale(LC_TIME, "fr_FR.ISO-8859-15@euro");
-  $html .= strftime("%e %B %Y", Helpers :: ansiDateToTimestamp($decision_date));
+  $html .= utf8_decode(strftime("%e %B %Y", Helpers :: ansiDateToTimestamp($decision_date)));
 } else {
   $html .= "[&nbsp;Choisir une date&nbsp;]";
 }
