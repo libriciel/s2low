38f588b40da5a68902967429f4fa9c409d3c3c89
Eric Pommateau <eric.pommateau@libriciel.coop>
2011-09-13 08:08:14 +0000

    Intégration du message de retour. Fixation du nom des scripts.

--- cron/archiveTransferReply.php
+++ cron/archiveTransferReply.php
@@ -3,30 +3,6 @@
 require_once( __DIR__ . "/../init/init.php");
 $actesTransactionsSQL = new ActesTransactionsSQL($sqlQuery);
 
-
-$allTransactions = $actesTransactionsSQL->getArchiveFromStatus(12);
-
-echo count($allTransactions). " transactions trouvées dans l'état <envoyé au SAE>\n";
-
-foreach($allTransactions as $transactionInfo){
-	
-	echo "Transaction {$transactionInfo['unique_id']} : ";
-	$asalae = new Asalae($transactionInfo);
-	$message = $asalae->getAcuseReception($transactionInfo['unique_id']);
-	if  ( ! $message ){
-		echo  $asalae->getLastError() ."\n";	
-	} else {
-		
-		$msg = "La transaction {$transactionInfo['id']} a été reçu par le SAE ";
-		$actesTransactionsSQL->updateStatus($transactionInfo['id'],15,$msg,$message);
-		
-		Log::newEntry(LOG_ISSUER_NAME, $msg, 1, false, 'USER', "actes", false);
-	    echo $msg . "\n";
-	}
-}
-
-
-
 $allTransactions = $actesTransactionsSQL->getArchiveFromStatus(15);
 
 echo count($allTransactions). " transactions trouvées dans l'état <reçu par le SAE>\n";
@@ -46,12 +22,11 @@ foreach($allTransactions as $transactionInfo){
 			$msg = "La transaction {$transactionInfo['id']} a été acceptée par le SAE ";
 			$actesTransactionsSQL->updateStatus($transactionInfo['id'],13,$msg,$message);
 		} else {
-			$msg = "La transaction {$transactionInfo['id']} a été refusée par le SAE ";
+			$msg = "La transaction {$transactionInfo['id']} a été refusé par le SAE: \n" . strval($xml->ReplyCode) . " - " . strval($xml->Comment);			
 			$actesTransactionsSQL->updateStatus($transactionInfo['id'],14,$msg,$message);
 		}
 		Log::newEntry(LOG_ISSUER_NAME, $msg, 1, false, 'USER', "actes", false);
 	    echo $msg . "\n";
 	}
 }
-
 
