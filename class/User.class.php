8425db3379c365c5f5e57ef5dc61a3c2dd93d62c
Pierre-Emmanuel Viver <pierre-emmanuel.viver@libriciel.coop>
2011-10-19 15:37:59 +0000

    Trie des utilisateurs par ordre alphabetique ticket h#76

--- class/User.class.php
+++ class/User.class.php
@@ -754,7 +754,7 @@ class User extends DataObject {
    * \return Un tableau contenant les données des utilisateurs
   */
   public function getUsersList($cond = "") {
-	if (! $this->pagerInit('users.id, users.name, users.givenname, users.email, users.role, users.authority_group_id, users.telephone, users.status, users.authority_id, authorities.name AS authority_name', 'users LEFT OUTER JOIN authorities ON users.authority_id=authorities.id', $cond)) {
+	if (! $this->pagerInit('users.id, users.name, users.givenname, users.email, users.role, users.authority_group_id, users.telephone, users.status, users.authority_id, authorities.name AS authority_name', 'users LEFT OUTER JOIN authorities ON users.authority_id=authorities.id', $cond, 'users.name', null, null, "ASC")) {
 	  return false;
 	}
 
