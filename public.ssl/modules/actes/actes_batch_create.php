7ff6c93c8371bb2da3f70d8ac8a6c580fcee0094
Pierre-Emmanuel Viver <pierre-emmanuel.viver@libriciel.coop>
2012-10-01 16:05:22 +0000

    Mise en place du nouveau traitement par lot

--- public.ssl/modules/actes/actes_batch_create.php
+++ public.ssl/modules/actes/actes_batch_create.php
@@ -29,31 +29,82 @@ $zeBatch->set("description", $description);
 $zeBatch->set("num_prefix", $num_prefix);
 $zeBatch->set("user_id", $me->getId());
 
-if (count($_FILES) > 0) {
-  if (! $zeBatch->importFilesFromForm($_FILES)) {
-	Helpers::returnAndExit(1, $zeBatch->getErrorMsg(), WEBSITE_SSL . "/modules/actes/actes_batch_add.php");
+//Stéphane Sampaio Edit
+//conversion du tableau $_FILES reçu pour adaptation au traitement
+$converted = array();
+$filerefs = array();
+for ($i = 0; $i < count($_FILES['files']['name']); $i++) {
+  $filerefs[$_FILES['files']['name'][$i]] = $i;
+  $converted[$_FILES['files']['name'][$i]] = array(
+      'name' => '',
+      'type' => '',
+      'tmp_name' => '',
+      'error' => 0,
+      'size' => 0
+  );
+}
+for ($i = 0; $i <= max($filerefs);$i++){
+  $name = $_FILES['files']['name'][$i];
+  $converted[$name]['name'] = $name;
+  $converted[$name]['type'] = $_FILES['files']['type'][$i];
+  $converted[$name]['tmp_name'] = $_FILES['files']['tmp_name'][$i];
+  $converted[$name]['error'] = $_FILES['files']['error'][$i];
+  $converted[$name]['size'] = $_FILES['files']['size'][$i];
+}
+//Fin
+$alljson = array();
+
+$elvl = 0;
+$msg = '';
+$id = 0;
+if (count($converted) > 0) {
+  if (!$zeBatch->importFilesFromForm($converted)) {
+  $msg = $zeBatch->getErrorMsg();
+  $elvl = 1;
+    //Helpers::returnAndExit(1, $zeBatch->getErrorMsg(), WEBSITE_SSL . "/modules/actes/actes_batch_add.php");
   } else {
-	if (! $zeBatch->save()) {
-	  $msg = "Erreur lors de l'enregistrement du lot : " . $zeBatch->getErrorMsg();
-	  
-	  if (! Log::newEntry(LOG_ISSUER_NAME, $msg, 3, false, 'USER', $module->get("name"), $me)) {
-		$msg .= "\nErreur de journalisation.";
-	  }
+    if (!$zeBatch->save()) {
+      $msg = "Erreur lors de l'enregistrement du lot : " . $zeBatch->getErrorMsg();
 
-	  Helpers::returnAndExit(1, $msg, WEBSITE_SSL . "/modules/actes/actes_batch_add.php");
-	} else {
-	  $msg = "Lot n°" . $zeBatch->getId() . " créé avec succès.";
-	  
-	  if (! Log::newEntry(LOG_ISSUER_NAME, $msg, 1, false, 'USER', $module->get("name"), $me)) {
-		$msg .= "\nErreur de journalisation.";
-	  }
+      if (!Log::newEntry(LOG_ISSUER_NAME, $msg, 3, false, 'USER', $module->get("name"), $me)) {
+        $msg .= "\nErreur de journalisation.";
+      }
 
-	  Helpers::returnAndExit(0, $msg, WEBSITE_SSL . "/modules/actes/actes_batch_show.php?id=" . $zeBatch->getId(), $zeBatch->getId());
-	}
-  }
-} else {
-	Helpers::returnAndExit(1, "Aucun fichier soumis.", WEBSITE_SSL . "/modules/actes/actes_batch_add.php");
+//      Helpers::returnAndExit(1, $msg, WEBSITE_SSL . "/modules/actes/actes_batch_add.php");
+$elvl = 1;
+
+
+    } else {
+
+      if (!Log::newEntry(LOG_ISSUER_NAME, $msg, 1, false, 'USER', $module->get("name"), $me)) {
+        $msg .= "\nErreur de journalisation.";
+      }
+$elvl = 0;
+
+
+foreach ($converted as $kFile => $file){
+       $jsontest = new stdClass();
+        $jsontest->name = $file['name'];
+        $jsontest->size = $file['size'];
+        $jsontest->type = $file['type'];
+        $alljson[] = $jsontest;
 }
 
+        $id = $zeBatch->getId();
+        $msg = "Lot n&deg;" . $id . " cr&eacute;&eacute; avec succ&egrave;s.";
+     // Helpers::returnAndExit(0, $msg, WEBSITE_SSL . "/modules/actes/actes_batch_show.php?id=" . $zeBatch->getId(), $zeBatch->getId());
+    }
+  }
+} else {
+$elvl = 1;
+ $msg = "Aucun fichier soumis";
+  //Helpers::returnAndExit(1, "Aucun fichier soumis.", WEBSITE_SSL . "/modules/actes/actes_batch_add.php");
+}
+$alljson[] = array('msg' => $msg, 'elvl' => $elvl, 'id' => $id);
+
+//header('Content-Type: application/json');
+//header('Content-Type: text/html');
+//echo json_encode(array('msg' => $msg, 'errorlevel' => $elvl));
+echo json_encode($alljson);
 
 ?>
