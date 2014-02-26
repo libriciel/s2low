56d89ad27cf6934319e32905c0ce826d6059cdde
Eric Pommateau <eric.pommateau@libriciel.coop>
2014-02-26 06:28:08 +0000

    Ajout de l'état validé pour pouvoir archiver une transaction vie la SAE

--- class/actes/ActesArchiveControler.class.php
+++ class/actes/ActesArchiveControler.class.php
@@ -80,8 +80,8 @@ class ActesArchiveControler {
 			return false;
 		}
 		
-		if ($transactionsInfo['last_status_id'] != 4 && $transactionsInfo['type'] != 1) {
-			$this->lastError = "Impossible d'archiver une transaction qui n'est pas en état « Acquittement reçu ».";
+		if (! in_array($transactionsInfo['last_status_id'],array(4,5))  && $transactionsInfo['type'] != 1) {
+			$this->lastError = "Impossible d'archiver une transaction qui n'est pas en état « Acquittement reçu » ou « Validé ».";
 			return false;
 		}
 		
