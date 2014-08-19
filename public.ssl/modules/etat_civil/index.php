1285a2378562d035a3bd882dbb86930c4b8e1748
Pierre-Emmanuel Viver <pierre-emmanuel.viver@libriciel.coop>
2014-08-19 16:45:39 +0000

    suppression du \n qui s'affichait dans la boite de dialogue

--- public.ssl/modules/etat_civil/index.php
+++ public.ssl/modules/etat_civil/index.php
@@ -173,7 +173,7 @@ function afficheWarning(){
       if(liste[i].checked) n++;
     }
   }
-  var msg = "Voulez-vous vraiment affecter les " + n + " transactions sélectionnées ?\\n";
+  var msg = "Voulez-vous vraiment affecter les " + n + " transactions sélectionnées ? ";
   msg += "Cette action est non réversible et est sous votre entière responsabilité";
   return confirm(msg);
 }
