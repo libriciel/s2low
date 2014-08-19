a104d8e6fbdef1280532922c50fb7d5448bb6c04
Pierre-Emmanuel Viver <pierre-emmanuel.viver@libriciel.coop>
2014-08-19 16:45:58 +0000

    suppression du \n qui s'affichait dans la boite de dialogue

--- public.ssl/modules/helios/index.php
+++ public.ssl/modules/helios/index.php
@@ -148,7 +148,7 @@ function afficheWarning(){
       if(liste[i].checked) n++;
     }
   }
-  var msg = "Voulez-vous vraiment affecter les " + n + " transactions sélectionnés ?\\n";
+  var msg = "Voulez-vous vraiment affecter les " + n + " transactions sélectionnés ? ";
   msg += "Cette action est non réversible et est sous votre entière responsabilité";
   return confirm(msg);
 }
