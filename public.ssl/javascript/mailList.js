060faad4f082856ca01eef7a998f00da8a5077fe
Pierre-Emmanuel Viver <pierre-emmanuel.viver@libriciel.coop>
2014-08-19 16:54:10 +0000

    problème d'accent

--- public.ssl/javascript/mailList.js
+++ public.ssl/javascript/mailList.js
@@ -57,7 +57,7 @@ function afficheWarning(){
       if(liste[i].checked) n++;
     }
   }
-  var msg = "Voulez-vous vraiment affecter les " + n + " transactions s�lectionn�es ?";
-  msg += "Cette action est non r�versible et est sous votre enti�re responsabilit�.";
+  var msg = "Voulez-vous vraiment affecter les " + n + " transactions sélectionnées ?";
+  msg += "Cette action est non réversible et est sous votre entière responsabilité.";
   return confirm(msg);
 }
