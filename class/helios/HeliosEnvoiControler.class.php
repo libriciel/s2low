57f6bad2429291c55e42a921e92b809044cf585e
Pierre-Emmanuel Viver <pierre-emmanuel.viver@libriciel.coop>
2014-09-15 09:03:43 +0000

    ajout d'instruction dans la fonction d'analyse des XML lorsque ceux-ci sont trop volumineux

--- class/helios/HeliosEnvoiControler.class.php
+++ class/helios/HeliosEnvoiControler.class.php
@@ -44,7 +44,7 @@ class HeliosEnvoiControler {
 				continue;
 			}*/
 			
-			$pes_xml = simplexml_load_string($pes_content);			
+			$pes_xml = simplexml_load_string($pes_content, 'SimpleXMLElement', LIBXML_PARSEHUG);			
 			if (!$pes_xml){
 				$message = "Transaction $transaction_id : ce fichier n'est pas en XML";
 				$this->updateStatus($transaction_id,HeliosTransactionsSQL::ERREUR,$message,$transactionInfo['user_id']);
