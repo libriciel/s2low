fba9ec0c357d7d2d4a84768fd581ca3a49fb0678
Pierre-Emmanuel Viver <pierre-emmanuel.viver@libriciel.coop>
2014-11-19 15:35:36 +0000

    remise en place du versement par lot

--- class/actes/ListeActesHTML.class.php
+++ class/actes/ListeActesHTML.class.php
@@ -216,6 +216,7 @@ class ListeActesHTML {
                                 <select id ="change-status" name="status" class="form-control">
                                     <option value="valid">Validé</option>
                                     <option value="invalid">Refusé</option>
+                                    <option value="sae">Envoyé au SAE</option>
                                 </select>
                                 </div>
                             <button type="submit" class="btn btn-default col-md-offset-3 col-md-2">Exécuter</button>
