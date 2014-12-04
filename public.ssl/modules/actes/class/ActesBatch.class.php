4ceb49c54186713b25512785a767252d4a643cc3
Pierre-Emmanuel Viver <pierre-emmanuel.viver@libriciel.coop>
2014-12-04 15:27:32 +0000

    suppression du code de vérification des signatures dans les enveloppes car pas au bon endroit. Bonne rev 541 de EP

--- public.ssl/modules/actes/class/ActesBatch.class.php
+++ public.ssl/modules/actes/class/ActesBatch.class.php
@@ -454,30 +454,6 @@ class ActesBatch extends DataObject {
               $foundSig = false;
               $sign = "";
               reset($filesDup);
-              while ((list ($key2, $sigFile) = each($filesDup)) && !$foundSig) {
-              	// on recherche un fichier de même nom que le fichier courant avec .sig à la fin
-              	if (strcmp($sigFile["name"], $filename . ".sig") == 0) {
-              		$foundSig = true;
-              	  
-              		if (is_uploaded_file($sigFile["tmp_name"])) {
-              			// Vérification de la signature
-              			$parapheur = new Parapheur(file_get_contents($file["tmp_name"]));
-              			$ret = $parapheur->verify(file_get_contents($sigFile["tmp_name"]));
-              			 
-              			if (!$ret) {
-              				$this->errorMsg .= "La signature numérique du fichier " . $filename . " est incorrecte : " . $parapheur->getLastError() . "\n";
-              				$ret_value = false;
-              			}
-              			elseif (! $sign = file_get_contents($sigFile["tmp_name"])) {
-              				$this->errorMsg .= "Erreur de récupération de la signature du fichier " . $filename . "\n";
-              				$ret_value = false;
-              			}
-              		} else {
-              			$this->errorMsg .= "Envoi de fichier incorrect.";
-              			$ret_value = false;
-              		}
-              	}
-              }
             }
 
             if ($ret_value) {
