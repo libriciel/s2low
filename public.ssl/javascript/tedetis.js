6cdc5d9bc72859ba332d2a74b80d13d9e9bcb4d4
Pierre-Emmanuel Viver <pierre-emmanuel.viver@libriciel.coop>
2015-03-20 15:42:23 +0000

    Correction accents

--- public.ssl/javascript/tedetis.js
+++ public.ssl/javascript/tedetis.js
@@ -57,7 +57,7 @@ function afficheWarning(){
       if(liste[i].checked) n++;
     }
   }
-  var msg = "Voulez-vous vraiment affecter les " + n + " transactions sélectionnées ? ";
-  msg += "Cette action est non réversible et est sous votre entière responsabilité.";
+  var msg = "Voulez-vous vraiment affecter les " + n + " transactions selectionnees ? ";
+  msg += "Cette action est non reversible et est sous votre entiere responsabilite.";
   return confirm(msg);
 }
