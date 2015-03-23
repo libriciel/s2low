040c3b3fa21fb5b948bb10813ac04a8b0f18639b
Pascal Bonifacj <pascal.bonifacj@berger-levrault.com>
2015-03-23 11:53:56 +0000

    - m47924 : purge des fichiers des anciennes transactions Hélios et Actes (suite) : tracer l'avancement

--- 1.5.01/cron/menage.php
+++ 1.5.01/cron/menage.php
@@ -15,10 +15,13 @@ $allEnvelopes = $actesTransactionsSQL->getEnvelopeToDelete();
 
 $actesEnvelope = new ActesEnvelope(ACTES_FILES_UPLOAD_ROOT);
 
-echo count($allEnvelopes). " transactions trouvées dans l'état <archivé par le SAE>\n";
+$count = count($allEnvelopes);
+echo $count . " transactions trouvées dans l'état <archivé par le SAE>\n";
 
+$index = 0;
 foreach($allEnvelopes as $envelopeInfo){
     checkBatchStop();
+    $index++;
 	$actesEnvelope->deleteFiles($envelopeInfo['file_path']);
 	
 	$msg = "Les fichiers de l'envelope {$envelopeInfo['id']} ont été détruits";
@@ -26,16 +29,19 @@ foreach($allEnvelopes as $envelopeInfo){
 	$actesTransactionsSQL->updateStatus($envelopeInfo['transaction_id'],16,$msg);
 	Log::newEntry(LOG_ISSUER_NAME, $msg, 1, false, 'USER', "actes", false,$envelopeInfo['user_id']);
 	
-	echo $msg."\n";	
+	echo "Actes ($index/$count) - $msg.\n";	
 }
 
 $heliosTransactionsSQL = new HeliosTransactionsSQL($sqlQuery);
 $allTransaction = $heliosTransactionsSQL->getTransactionToDelete();
 $heliosFile = new HeliosFiles(HELIOS_FILES_UPLOAD_ROOT); 
 
-echo count($allTransaction) . " transactions Helios trouvées dans l'état <archivé par le SAE>\n";
+$count = count($allTransaction);
+echo $count . " transactions Helios trouvées dans l'état <archivé par le SAE>\n";
+$index = 0;
 foreach($allTransaction as $transactionInfo){
     checkBatchStop();
+    $index++;
 	$heliosFile->deleteFiles($transactionInfo);
 	
 	$msg = "Les fichiers de la transaction {$transactionInfo['id']} ont été détruits";
@@ -43,5 +49,5 @@ foreach($allTransaction as $transactionInfo){
 	$heliosTransactionsSQL->updateStatus($transactionInfo['id'],12,$msg);
 	Log::newEntry(LOG_ISSUER_NAME, $msg, 1, false, 'USER', "helios", false,$transactionInfo['user_id']);
 	
-	echo $msg."\n";	
+	echo "Helios ($index/$count) - $msg.\n";	
 }
