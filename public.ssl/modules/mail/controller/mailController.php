38acdeecc05ab7325ba21ea4d2953988c673fce3
Eric Pommateau <eric.pommateau@libriciel.coop>
2015-09-16 12:52:25 +0000

    Correction Mail Sécurisé : - la recherche du sujet utilisait une fonction mysql non disponible sur S2low (qui tourne en Pgsql !) - le sujet n'était pas échappé - le formulaire est bizzarement encodé en UTF-8

--- public.ssl/modules/mail/controller/mailController.php
+++ public.ssl/modules/mail/controller/mailController.php
@@ -96,7 +96,7 @@ class mailController {
   	 	$tabStatus = mail_transaction::getTabStatus();
   	 	$etat_string = $tabStatus[$etat];
   	 	
-  	 	$sujet=Helpers :: getVarFromPost("sujet");
+  	 	$sujet = utf8_decode(Helpers :: getVarFromPost("sujet"));
   	 	$SendDateFrom=Helpers :: getVarFromPost("SendDateFrom");
   	 	$SendDateTo=Helpers :: getVarFromPost("SendDateTo");
   	 	$cond=" user_id=".$me->getId();
@@ -104,7 +104,7 @@ class mailController {
   	 	if ($etat_string)
   	 		$cond.=" and status='".$etat_string."'";
   	 	if ($sujet)
-  	 		$cond.=" and objet ILIKE '%".mysql_real_escape_string($sujet)."%'";
+  	 		$cond.=" and objet ILIKE '%".addslashes($sujet)."%'";
   	 	if ($SendDateFrom)
   	 		$cond.=" and date_envoi >='".addslashes($SendDateFrom)."'";
   	 	if ($SendDateTo)
