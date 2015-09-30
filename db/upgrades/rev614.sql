32a914ffd04712ae939a26dab3f017fa47365454
Pierre-Emmanuel Viver <pierre-emmanuel.viver@libriciel.coop>
2015-09-30 15:58:16 +0000

    mise a jour des authority_districts

--- db/upgrades/rev614.sql
+++ db/upgrades/rev614.sql
@@ -1,9 +1,12 @@
 INSERT INTO authority_departments(id,code,name) VALUES (201,987,'Polynésie française');
 
-INSERT INTO authority_districts(id,authority_department_id,code,name) VALUES (1027,201,1,'DIRAJ - Direction de la règlementation et des affaires juridiques');
-INSERT INTO authority_districts(id,authority_department_id,code,name) VALUES (1028,201,2,'SAIDV - Subdivision administrative des îles du Vent');
-INSERT INTO authority_districts(id,authority_department_id,code,name) VALUES (1029,201,3,'SAISLV - Subdivision administrative des îles Sous-Le-Vent');
-INSERT INTO authority_districts(id,authority_department_id,code,name) VALUES (1030,201,4,'SAIA - Subdivision administrative des îles Australes');
-INSERT INTO authority_districts(id,authority_department_id,code,name) VALUES (1031,201,5,'SAITG - Subdivision administrative des îles des Tuamotu-Gambier');
-INSERT INTO authority_districts(id,authority_department_id,code,name) VALUES (1032,201,6,'SAIM - Subdivision administrative des îles Marquises');
+INSERT INTO authority_districts(id,authority_department_id,code,name) VALUES (1027,134,6,'Arcachon');
+INSERT INTO authority_districts(id,authority_department_id,code,name) VALUES (1028,160,9'SGAR Nord Pas de Calais');
+INSERT INTO authority_districts(id,authority_department_id,code,name) VALUES (1029,114,9'SGAR de Caen');
+INSERT INTO authority_districts(id,authority_department_id,code,name) VALUES (1030,201,1,'DIRAJ - Direction de la règlementation et des affaires juridiques');
+INSERT INTO authority_districts(id,authority_department_id,code,name) VALUES (1031,201,2,'SAIDV - Subdivision administrative des îles du Vent');
+INSERT INTO authority_districts(id,authority_department_id,code,name) VALUES (1032,201,3,'SAISLV - Subdivision administrative des îles Sous-Le-Vent');
+INSERT INTO authority_districts(id,authority_department_id,code,name) VALUES (1033,201,4,'SAIA - Subdivision administrative des îles Australes');
+INSERT INTO authority_districts(id,authority_department_id,code,name) VALUES (1034,201,5,'SAITG - Subdivision administrative des îles des Tuamotu-Gambier');
+INSERT INTO authority_districts(id,authority_department_id,code,name) VALUES (1035,201,6,'SAIM - Subdivision administrative des îles Marquises');
 
