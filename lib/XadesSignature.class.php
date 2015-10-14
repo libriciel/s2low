ffd3ea3e01a239605f6818e8f250f63cc9a06e83
Eric Pommateau <eric.pommateau@libriciel.coop>
2015-10-14 15:48:59 +0000

    Suite intégration XADES

--- lib/XadesSignature.class.php
+++ lib/XadesSignature.class.php
@@ -3,15 +3,11 @@
 //http://users.dcc.uchile.cl/~pcamacho/tutorial/web/xmlsec/xmlsec.html
 class XadesSignature {
 
-	public function sign($xml_file_to_sign,$p12_certificate_path,$p12_password, $xml_file_signed){
-		libxml_use_internal_errors(true);
+	const NS_DS_URI = "http://www.w3.org/2000/09/xmldsig#";
 
-		$domDocument = new DOMDocument();
-		$result = $domDocument->load($xml_file_to_sign, LIBXML_PARSEHUGE);
-		if (! $result){
-			$errors = libxml_get_errors();
-			throw new Exception($errors[0]->message);
-		}
+
+	public function sign($xml_file_to_sign,$p12_certificate_path,$p12_password, $xml_file_signed){
+		$domDocument = $this->loadDomDocument($xml_file_to_sign);
 
 		if (! $domDocument->documentElement->attributes->getNamedItem('Id')){
 			throw new Exception("Le document XML ne contient pas d'Id");
@@ -21,30 +17,63 @@ class XadesSignature {
 
 		$signatureTemplate = $this->getXMLSignatureTemplate();
 
-		$child  = $signatureTemplate->children("http://www.w3.org/2000/09/xmldsig#");
-		$child->SignedInfo->Reference["URI"] = "#$id";
+		$child  = $signatureTemplate->children(self::NS_DS_URI);
+		$child->SignedInfo->Reference->attributes()->URI = "#$id";
+
+		$signature_id = "{$id}_SIG";
+		$signatureTemplate->attributes(self::NS_DS_URI)->Id = $signature_id;
+		//TODO NOT WORKING !
+			print_r($signatureTemplate->attributes(self::NS_DS_URI));
+
+		$xad = $child->Object->children("http://uri.etsi.org/01903/v1.1.1#");
+
+		$signedSignatureProperties = $xad->QualifyingProperties->SignedProperties->SignedSignatureProperties;
+
+		$signedSignatureProperties->SigningTime = gmdate('Y-m-d\TH:i:s\Z');
+
+		$pkcs12 = new PKCS12();
+		$all = $pkcs12->getAll($p12_certificate_path,$p12_password);
+
+		$x509Certificate = new X509Certificate();
+		$info = $x509Certificate->getInfo($all['cert']);
+
+		$serialNumber = $info['serialNumber'];
+
+		$issuerName = "";
+		foreach(array_reverse($info['issuer']) as $id => $value){
+			$issuerName[] = "$id=$value";
+		}
+		$issuerName = implode(", ",$issuerName);
+
+		$issuer_serial_child = $signedSignatureProperties->SigningCertificate->Cert->IssuerSerial->children("http://uri.etsi.org/01903/v1.1.1#");
+		$issuer_serial_child->X509IssuerName = $issuerName;
+		$issuer_serial_child->X509SerialNumber = $serialNumber;
+
+		$tmp_file = sys_get_temp_dir()."/".uniqid("x509_pem");
+		file_put_contents($tmp_file,$all['cert']);
+
+		$command = "openssl x509 -in $tmp_file -outform der | openssl sha1 -binary | openssl base64";
+		exec($command,$output,$return_var);
+		$certDigest = $output[0];
+
+		$cert_digest_child = $signedSignatureProperties->SigningCertificate->Cert->CertDigest->children("http://uri.etsi.org/01903/v1.1.1#");
+		$cert_digest_child->DigestValue = $certDigest;
+
+
+		$signedSignatureProperties->SignatureProductionPlace->City = 'Lyon';
+		$signedSignatureProperties->SignatureProductionPlace->PostalCode = '69003';
+		$signedSignatureProperties->SignatureProductionPlace->CountryName = 'France';
+		$signedSignatureProperties->SignerRole->ClaimedRoles->ClaimedRole = utf8_encode('Tiers de Télétransmission S2low Adullact');
 
 		$signatureTemplateDOM = dom_import_simplexml($signatureTemplate);
-
-		$rootName = $domDocument->documentElement->nodeName;
-
-		echo $rootName;
-
-
-		//TODO Vérifier avec un $pes_aller_id contenant des caractère de controle XML
-
-		//TODO Ajouter les champs xades
-		//TODO CertDigest : openssl x509 -in ../../certificat/user.crt -outform der | openssl sha1 -binary | openssl base64
-
-
 		$node = $domDocument->importNode($signatureTemplateDOM, true);
 		$domDocument->documentElement->appendChild($node);
 
 		$tmp_file = sys_get_temp_dir()."/".uniqid("xmlsigtmp");
 		$domDocument->save($tmp_file);
 
-
-		$command = "xmlsec1 --sign --id-attr:Id toto --output $xml_file_signed --pkcs12 $p12_certificate_path --pwd $p12_password $tmp_file 2>&1";
+		$rootNodeName = $this->getLocalName($domDocument);
+		$command = "xmlsec1 --sign --id-attr:Id $rootNodeName --output $xml_file_signed --pkcs12 $p12_certificate_path --pwd $p12_password $tmp_file 2>&1";
 		exec($command,$output,$return_var);
 		unlink($tmp_file);
 		if ($return_var != 0){
@@ -53,21 +82,32 @@ class XadesSignature {
 	}
 
 	public function verify($xml_file_signed,$trusted_pem_path){
-		$command = "xmlsec1 --verify --id-attr:Id toto --trusted-pem $trusted_pem_path $xml_file_signed 2>&1";
+		$domDocument = $this->loadDomDocument($xml_file_signed);
+		$rootNodeName = $this->getLocalName($domDocument);
+		$command = "xmlsec1 --verify --id-attr:Id $rootNodeName --trusted-pem $trusted_pem_path $xml_file_signed 2>&1";
 		exec($command,$output,$return_var);
 		return $return_var == 0;
 	}
 
-	private function getXMLSignatureTemplate(){
-		return simplexml_load_file(__DIR__."/xades-template.xml");
+	private function loadDomDocument($xml_file_path){
+		libxml_use_internal_errors(true);
 
-		/*$signatureTemplate = new DOMDocument();
-		$signatureTemplate->load(__DIR__."/xades-template.xml");
-		return $signatureTemplate;*/
+		$domDocument = new DOMDocument();
+		$result = $domDocument->load($xml_file_path, LIBXML_PARSEHUGE);
+		if (! $result){
+			$errors = libxml_get_errors();
+			throw new Exception($errors[0]->message);
+		}
+		return $domDocument;
+	}
+
+	private function getLocalName(DomDocument $domDocument){
+		return $domDocument->documentElement->localName;
 	}
 
 
-
-//xmlsec1 --verify --id-attr:Id http://www.minefi.gouv.fr/cp/helios/pes_v2/Rev0/aller:PES_Aller --trusted-pem ~/Desktop/AC_AGENTS.cer --trusted-pem ~/Desktop/AC_Racine_G3.cer pes_aller.xml
+	private function getXMLSignatureTemplate(){
+		return simplexml_load_file(__DIR__."/xades-template.xml");
+	}
 
 }
--- test/PHPUnit/lib/XadesSignatureTest.php
+++ test/PHPUnit/lib/XadesSignatureTest.php
@@ -30,8 +30,17 @@ class XadesSignatureTest extends PHPUnit_Framework_TestCase {
 	public function testBadPassword(){
 		$signed_file = sys_get_temp_dir()."/".uniqid("phpunit");
 		$xmlSecWrapper = new XadesSignature();
-		$this->setExpectedException("Exception","Error: failed to load pkcs12 key from");
+		$this->setExpectedException("Exception","Impossible de lire le certificat PKCS#12");
 		$xmlSecWrapper->sign(__DIR__."/fixtures/test.xml",__DIR__."/fixtures/robert_petitpoids.p12","bad password",$signed_file);
+	}
+
+	public function testSignPES_Aller(){
+		$signed_file = sys_get_temp_dir()."/".uniqid("phpunit");
+		$xmlSecWrapper = new XadesSignature();
+		$xmlSecWrapper->sign(__DIR__."/fixtures/HELIOS_SIMU_ALR2_1444811220_681372666.xml",__DIR__."/fixtures/robert_petitpoids.p12","robert_petitpoids",$signed_file);
+		$this->assertTrue(file_exists($signed_file));
+		$this->assertTrue($xmlSecWrapper->verify($signed_file,__DIR__."/fixtures/autorite_a_effacer-cert.pem"));
+		copy($signed_file,"/Users/eric/Desktop/pes_signed.xml");
 	}
 
 }
