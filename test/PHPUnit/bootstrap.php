59ee93fe70f089e2e4528b46bddda0c6d90eb787
Eric Pommateau <eric.pommateau@libriciel.coop>
2015-10-23 15:30:08 +0000

    Ajout création symlink pour Jenkins

--- test/PHPUnit/bootstrap.php
+++ test/PHPUnit/bootstrap.php
@@ -12,3 +12,6 @@ require_once(PHP_UNIT_AUTOLOADER);
 require_once(__DIR__."/S2lowTestCase.class.php");
 
 
+if (! file_exists(__DIR__."/../../public/custom")){
+	link(__DIR__."/../../public.ssl/custom",__DIR__."/../../public/custom");
+}
