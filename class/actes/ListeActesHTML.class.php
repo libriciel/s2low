4eed6a855f90552ab48c1a9d5903b5fd84fc4230
Eric Pommateau <eric.pommateau@libriciel.coop>
2015-11-12 15:44:03 +0000

    Bugfix : ACTES : versement par lot des actes validé

--- class/actes/ListeActesHTML.class.php
+++ class/actes/ListeActesHTML.class.php
@@ -279,7 +279,7 @@ class ListeActesHTML {
                                     <tbody>
 					<tr>
                                             <td headers="selection">
-                                                <?php if ($envelope['type'] == 1 && ($envelope['current_status'] == 4 || $envelope['current_status'] == 18)): ?>
+                                                <?php if ($envelope['type'] == 1 && (in_array($envelope['current_status'],array(4,18,5)))): ?>
                                                     <input type="checkbox" 
                                                                     name="liste_id[]" 
                                                                     value="<?php hecho($envelope['transaction_id']) ;?>" 
