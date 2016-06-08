70eb12816c4fe8a2bf3b360aa55baf4861d74239
Eric Pommateau <eric.pommateau@libriciel.coop>
2016-06-08 09:35:29 +0000

    Test cassé -> réparé

--- test/PHPUnit/class/actes/ActeTamponneTest.php
+++ test/PHPUnit/class/actes/ActeTamponneTest.php
@@ -15,10 +15,8 @@ class ActeTamponneTest extends PHPUnit_Framework_TestCase {
 
 		$acteTamponne = new ActeTamponne($actesTransactionsSQL);
 
-		$tampon = $acteTamponne->tamponnerPDF(__DIR__."/../fixtures/vide.pdf","12");
-		
-		file_put_contents("/Users/eric/Desktop/test.pdf",$tampon);
-		
+		$$acteTamponne->tamponnerPDF(__DIR__."/../fixtures/vide.pdf","12");
+
 	}
 
 }
