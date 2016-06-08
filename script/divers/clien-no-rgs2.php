f4e691fddcc2f3c57c63c6729cc19eadb51b64a3
Eric Pommateau <eric.pommateau@libriciel.coop>
2016-06-08 14:28:25 +0000

    Ajout d'un script pour tester les RGS actes

---
+++ script/divers/clien-no-rgs2.php
@@ -0,0 +1,35 @@
+<?php
+
+//Liste des clients qui n'ont pas un certificat RGS
+
+require_once( __DIR__."/../../init/init.php");
+
+$rgsCertificate = new RgsCertificate(OPENSSL_PATH,RGS_VALIDCA_PATH);
+
+$handle = fopen("php://output","w");
+
+
+$date = '2016-01-01';
+
+$sql = "SELECT DISTINCT actes_transactions.user_id FROM actes_transactions WHERE decision_date > ? AND type='1' ";
+
+$sql_user = "SELECT users.id,certificate,givenname,users.name,users.email, users.authority_id, authorities.name as authority_name, users.authority_group_id, authority_groups.name as group_name FROM users " .
+			" LEFT JOIN authorities ON users.authority_id = authorities.id ".
+			" LEFT JOIN authority_groups ON users.authority_group_id = authority_groups.id " .
+			" WHERE users.id=?";
+
+echo $sql_user;
+
+
+foreach($sqlQuery->query($sql,$date) as $line){
+
+	$info = $sqlQuery->queryOne($sql_user,$line['user_id']);
+
+	if (! $rgsCertificate->isRgsCertificate($info['certificate'])){
+		unset($info['certificate']);
+		fputcsv($handle,$info);
+	}
+}
+
+fclose($handle);
+
--- script/divers/client-no-rgs.php
+++ script/divers/client-no-rgs.php
@@ -4,52 +4,6 @@
 
 require_once( __DIR__."/../../init/init.php");
 
-//if (! class_exists("RgsCertificate")){
-
-	class RgsCertificate {
-
-		private $openssl_path;
-		private $validca_path;
-		private $last_message;
-
-		/**
-		 * @param $openssl_path string chemin vers l'executable OpenSSL
-		 * @param $validca_path string chemin vers un répertoire contenant des autorités de certification "hasher" : man c_rehash
-		 */
-		public function __construct($openssl_path,$validca_path){
-			$this->validca_path = $validca_path;
-			$this->openssl_path = $openssl_path;
-		}
-
-		public function getLastMessage(){
-			return $this->last_message;
-		}
-
-		public function isRgsCertificate($x509_pem_certificate){
-			$tmp_file = "/tmp/s2low-lib-rgscertificate-".mt_rand(0,mt_getrandmax()).".pem";
-			file_put_contents($tmp_file,$x509_pem_certificate);
-
-			//Il semble qu'il n'y a pas de fonction php openssl_* qui permettent la vérification d'un certificat
-			$command = "cat $tmp_file | {$this->openssl_path} verify -verbose -CApath {$this->validca_path} 2>&1";
-			exec($command,$output,$return_var);
-			unlink($tmp_file);
-
-			$output = implode("\n",$output);
-
-			if (preg_match("#stdin: OK#",$output)){
-				$result = true;
-			} else {
-				$result = false;
-				$this->last_message = $output;
-			}
-
-			return $result;
-		}
-
-	}
-//}
-
-
 $rgsCertificate = new RgsCertificate(OPENSSL_PATH,RGS_VALIDCA_PATH);
 
 $handle = fopen("php://output","w");
