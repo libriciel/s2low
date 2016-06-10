b55215c9308c3ca892099b19281e459da42ea6f5
Eric Pommateau <eric.pommateau@libriciel.coop>
2016-06-10 06:08:58 +0000

    Ajout d'une transaction pour le déplacement du log

--- model/LogsHistoriqueSQL.class.php
+++ model/LogsHistoriqueSQL.class.php
@@ -5,11 +5,19 @@ class LogsHistoriqueSQL extends SQL {
 	public function vidange($nb_month_to_keep){
 		$date = date("Y-m-d H:i:s",strtotime("-{$nb_month_to_keep} month"));
 
-		$sql = "INSERT INTO logs_historique SELECT * FROM logs WHERE logs.date<?";
-		$this->query($sql,$date);
+		$this->query("BEGIN");
+		try {
+			$sql = "INSERT INTO logs_historique SELECT * FROM logs WHERE logs.date<?";
+			$this->query($sql,$date);
 
-		$sql = "DELETE FROM logs WHERE logs.date<?";
-		$this->query($sql,$date);
+			$sql = "DELETE FROM logs WHERE logs.date<?";
+			$this->query($sql,$date);
+
+			$this->query("COMMIT");
+		} catch (Exception $e){
+			$this->query("ROLLBACK");
+		}
+
 	}
 	
 }
