28a6354ca6aa5bc7a58443cd04ac43c4d09e096a
Eric Pommateau <eric.pommateau@libriciel.coop>
2016-08-17 13:30:13 +0000

    Correction lien bandeau mailsec

--- public.ssl/custom/templates/xhtml_mail_ssl.tpl.php
+++ public.ssl/custom/templates/xhtml_mail_ssl.tpl.php
@@ -15,8 +15,8 @@
 	<?php if (defined("NEW_BANNER")) : ?>
 
 	<div id="bandeau_s2low" class="container">
-		<a href='<?php echo WEBSITE ?>'>
-			<img src="<?php echo WEBSITE ?>/custom/images/bandeau_s2low.jpg"  />
+		<a href='<?php echo WEBSITE_SSL ?>'>
+			<img src="<?php echo WEBSITE_SSL ?>/custom/images/bandeau_s2low.jpg"  />
 		</a>
 	</div>
 	<?php else: ?>
