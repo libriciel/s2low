2a12acb57ebae732ce464b2cef74b5ea4adf13dd
Eric Pommateau <eric.pommateau@libriciel.coop>
2016-09-21 13:18:22 +0000

    Ajout d'une page permettant de voir les utilisateurs de l'ancien système de notification (bugfix)

--- public.ssl/admin/ancien_systeme_notif.php
+++ public.ssl/admin/ancien_systeme_notif.php
@@ -9,20 +9,37 @@ if ($userInfo['role'] != 'SADM'){
 }
 
 
-$sql = " SELECT authorities.name, authorities.id, authority_groups.name as group_name FROM authorities " .
-		" JOIN authority_groups ON authorities.authority_group_id = authority_groups.id ".
-		" JOIN modules_authorities ON authorities.id=modules_authorities.authority_id ".
-		" JOIN modules ON modules_authorities.module_id=modules.id AND modules.name=?".
-		" WHERE new_notification=? ORDER BY authority_groups.name, authorities.name";
-
-$authorities_list = $sqlQuery->query($sql,'actes',0);
-
-
-$sql_user = " SELECT DISTINCT email from users " .
+$sql = " SELECT DISTINCT users.email,authorities.name,authority_groups.name as group_name from users " .
 			" JOIN users_perms ON users_perms.user_id=users.id ".
 			" JOIN modules ON users_perms.module_id=modules.id AND modules.name='actes' AND (users_perms.perm='RO' OR users_perms.perm='RW')".
-			" WHERE users.authority_id=?";
+			" JOIN authorities ON authorities.id=users.authority_id ".
+			" JOIN authority_groups ON authorities.authority_group_id = authority_groups.id ".
+			" JOIN modules_authorities ON authorities.id=modules_authorities.authority_id ".
+			" JOIN modules m2 ON modules_authorities.module_id=m2.id AND m2.name='actes'".
+			" WHERE users.status = 1 " .
+			"ORDER BY authority_groups.name, authorities.name, users.email";
 
+
+$user_list = $sqlQuery->query($sql);
+
+
+$csv = isset($_GET['csv']) && $_GET['csv'];
+
+if ($csv){
+	header("Content-type: text/csv; charset=iso-8859-1");
+	header("Content-disposition: attachment; filename=ancien-system-notif.csv");
+	header("Expires: 0");
+	header("Cache-Control: must-revalidate, post-check=0,pre-check=0");
+	header("Pragma: public");
+
+	$out = fopen("php://output", 'w');
+	foreach($user_list as $user_info) {
+		fputcsv($out, $user_info);
+	}
+	fclose($out);
+
+	exit;
+}
 
 $menuHTML = new MenuHTML();
 $pagerHTML  = new PagerHTML();
@@ -48,27 +65,20 @@ ob_start();
 	<div class="alert alert-info">
 		Sur cette page, on ne présente que les collectivités abonnés au module actes qui utilise l'ancien système de notification.
 	</div>
-
+<p>
+	<a href="/admin/ancien_systeme_notif.php?csv=true" class="btn btn-primary">CSV</a>
+</p>
 <table class="data-table table table-striped">
 	<tr>
 		<th>Collectivité</th>
 		<th>Groupes</th>
 		<th>Email</th>
 	</tr>
-	<?php foreach($authorities_list as $authority_info) :
-		$user_list = $sqlQuery->query($sql_user,$authority_info['id']);
-
-		?>
+	<?php foreach($user_list as $user_info) : ?>
 		<tr>
-			<td><?php hecho($authority_info['name'])?></td>
-			<td><?php hecho($authority_info['group_name'])?></td>
-			<td>
-				<ul>
-				<?php foreach($user_list as $user_info):  ?>
-					<li><?php echo $user_info['email'] ?></li>
-				<?php endforeach; ?>
-				</ul>
-			</td>
+			<td><?php hecho($user_info['name'])?></td>
+			<td><?php hecho($user_info['group_name'])?></td>
+			<td><?php echo $user_info['email'] ?></td>
 		</tr>
 	<?php endforeach; ?>
 
