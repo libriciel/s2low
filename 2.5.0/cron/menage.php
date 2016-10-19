67a08dc5499532695484030c7ca365f6823a57a1
Pascal Bonifacj <pascal.bonifacj@berger-levrault.com>
2016-10-19 08:49:19 +0000

    - backport BL 2.3.1 rev 858     . m47924 : purge des fichiers des anciennes transactions Hélios et Actes (suite) : tracer l'avancement

--- 2.5.0/cron/menage.php
+++ 2.5.0/cron/menage.php
@@ -1,14 +1,27 @@
 <?php 
 require_once( __DIR__ . "/../init/init.php");
+
+function checkBatchStop() {
+    $stop_files = glob('/tmp/batch.stop');
+    if ($stop_files) {
+        echo "Interruption par fichier flag.\n";
+        die(1);
+    }
+}
+
 $actesTransactionsSQL = new ActesTransactionsSQL($sqlQuery);
 
 $allEnvelopes = $actesTransactionsSQL->getEnvelopeToDelete();
 
 $actesEnvelope = new ActesFiles(ACTES_FILES_UPLOAD_ROOT);
 
-echo count($allEnvelopes). " transactions trouvées dans l'état <archivé par le SAE>\n";
+$count = count($allEnvelopes);
+echo $count . " transactions trouvées dans l'état <archivé par le SAE>\n";
 
+$index = 0;
 foreach($allEnvelopes as $envelopeInfo){
+    checkBatchStop();
+    $index++;
 	$actesEnvelope->deleteFiles($envelopeInfo['file_path']);
 	
 	$msg = "Les fichiers de l'envelope {$envelopeInfo['id']} ont été détruits";
@@ -16,15 +29,18 @@ foreach($allEnvelopes as $envelopeInfo){
 	$actesTransactionsSQL->updateStatus($envelopeInfo['transaction_id'],16,$msg);
 	Log::newEntry(LOG_ISSUER_NAME, $msg, 1, false, 'USER', "actes", false,$envelopeInfo['user_id']);
 	
-	echo $msg."\n";	
+	echo "Actes ($index/$count) - $msg.\n";	
 }
 
 $heliosTransactionsSQL = new HeliosTransactionsSQL($sqlQuery);
 $allTransaction = $heliosTransactionsSQL->getTransactionToDelete();
 $heliosFile = new HeliosFiles(HELIOS_FILES_UPLOAD_ROOT); 
 
-echo count($allTransaction) . " transactions Helios trouvées dans l'état <archivé par le SAE>\n";
+$count = count($allTransaction);
+echo $count . " transactions Helios trouvées dans l'état <archivé par le SAE>\n";
 foreach($allTransaction as $transactionInfo){
+    checkBatchStop();
+    $index++;
 	$heliosFile->deleteFiles($transactionInfo);
 	
 	$msg = "Les fichiers de la transaction {$transactionInfo['id']} ont été détruits";
@@ -32,5 +48,5 @@ foreach($allTransaction as $transactionInfo){
 	$heliosTransactionsSQL->updateStatus($transactionInfo['id'],12,$msg);
 	Log::newEntry(LOG_ISSUER_NAME, $msg, 1, false, 'USER', "helios", false,$transactionInfo['user_id']);
 	
-	echo $msg."\n";	
+	echo "Helios ($index/$count) - $msg.\n";	
 }
