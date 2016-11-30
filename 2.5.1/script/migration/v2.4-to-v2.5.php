d174e0dca16c322da7cb30450462e625f9506515
Pascal Bonifacj <pascal.bonifacj@berger-levrault.com>
2016-11-30 15:53:13 +0000

    - les lignes reprises sont calculées par rapport à la date; cette solution s'adapte au contexte et évite de modifier le script pour y préciser l'id de départ, qui change d'une plateforme à l'autre.

--- 2.5.1/script/migration/v2.4-to-v2.5.php
+++ 2.5.1/script/migration/v2.4-to-v2.5.php
@@ -9,16 +9,7 @@
  *
  */
 
-
-exit;
-
-//Il est nécessaire de spécifier l'identifiant de la table log a partir de laquelle on va vider les lignes dans
-//la table log_historique.
-//Il faut être au plus pret des 6 mois.
 //Le mieux est d'indexé la table logs de de calculer l'id.
-
-//Coupe la table en deux à partir du id_cut
-$id_cut_journal = 0;
 
 //A FAIRE AVANT : mise à jour de la base de données
 
@@ -32,10 +23,11 @@ migration_log("Passage au nouveau système de notification : déporter à une date 
 //$sqlQuery->query($sql);
 migration_log("[PASS]");
 
+$date_purge = date("Y-m-d H:i:s",strtotime("-" . KEEP_NB_MONTHS_IN_LOGS . " month"));
 
 migration_log("Copie de la table logs vers logs_historique");
-$sql = "CREATE TABLE logs_historique AS SELECT * FROM logs;";
-$sqlQuery->query($sql);
+$sql = "CREATE TABLE logs_historique AS SELECT * FROM logs WHERE logs.date<?;";
+$sqlQuery->query($sql, $date_purge);
 migration_log("[DONE]");
 
 migration_log("Ajout de la clé primaire sur la table logs_historique");
@@ -43,15 +35,9 @@ $sql = "ALTER TABLE logs_historique ADD PRIMARY KEY (id);";
 $sqlQuery->query($sql);
 migration_log("[DONE]");
 
-migration_log("Suppression des lignes en trop dans logs_historique");
-$sql = "DELETE FROM logs_historique WHERE id > ?";
-$sqlQuery->query($sql,$id_cut_journal);
-migration_log("[DONE]");
-
-
 migration_log("Suppression des lignes en trop dans logs");
-$sql = "DELETE FROM logs WHERE id <= ?";
-$sqlQuery->query($sql,$id_cut_journal);
+$sql = "DELETE FROM logs WHERE logs.date < ?";
+$sqlQuery->query($sql,$date_purge);
 migration_log("[DONE]");
 
 
