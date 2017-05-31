593acb763dd67f220d54596e8e86c617d346381e
Eric Pommateau <eric.pommateau@adullact-projet.coop>
2017-05-31 17:02:23 +0200

    Correction fichier de config avec des valeurs par défaut

--- docker-resources/generate-config.sh
+++ docker-resources/generate-config.sh
@@ -6,12 +6,12 @@ set -e
 cat <<EOF
 <?php
 
-define("WEBSITE","${S2LOW_WEBSITE}");
-define("WEBSITE_SSL","${S2LOW_WEBSITE_SSL}");
-define('DB_HOST', "${POSTGRES_HOST}");
-define('DB_USER', "${POSTGRES_USER}");
+define("WEBSITE","${S2LOW_WEBSITE:-http://localhost/}");
+define("WEBSITE_SSL","${S2LOW_WEBSITE_SSL:-https://localhost}");
+define('DB_HOST', "${POSTGRES_HOST:-localhost}");
+define('DB_USER', "${POSTGRES_USER:-tedetis}");
 define('DB_PASSWORD', "${POSTGRES_PASSWORD}");
-define('DB_DATABASE', "${POSTGRES_DB}");
+define('DB_DATABASE', "${POSTGRES_DB}:-tedetis");
 
 define('TRACE_FILE_PATH','/tmp/slow.log');
 define('HELIOS_FILES_ROOT', '/data/tdt-workspace/helios/');
@@ -21,7 +21,6 @@ define('HELIOS_RESPONSES_ERROR_PATH', '/data/tdt-workspace/helios/response_error
 define('HELIOS_FILES_UPLOAD_TMP', '/data/tdt-workspace/helios/sending-tmp/');
 define('HELIOS_COUNTER_FILE',"/data/tdt-workspace/helios/counter.txt");
 define('HELIOS_FTP_RESPONSE_TMP_LOCAL_PATH','/data/tdt-workspace/helios/response_tmp/');
-define('HELIOS_RESPONSES_ERROR_PATH', '/data/tdt-workspace/helios/response_error/');
 
 
 define('TIMESTAMPING_CERT', '/etc/apache2/ssl/s2low_timestamp_cert.pem');
@@ -33,11 +32,11 @@ define("EXTENDED_VALIDCA_PATH","/etc/s2low/ssl/validca/");
 define("OPENSSL_PATH","/usr/bin/openssl");
 
 
-define("HELIOS_FTP_SERVER","${HELIOS_FTP_SERVER}");
-define("HELIOS_FTP_PORT","${HELIOS_FTP_PORT}");
-define("HELIOS_FTP_PASSIVE_MODE", ${HELIOS_FTP_PASSIVE_MODE});
-define("HELIOS_SENDING_DESTINATION","${HELIOS_SENDING_DESTINATION}");
-define("HELIOS_FTP_RESPONSE_SERVER_PATH","${HELIOS_FTP_RESPONSE_SERVER_PATH}");
+define("HELIOS_FTP_SERVER","${HELIOS_FTP_SERVER}:-localhost");
+define("HELIOS_FTP_PORT","${HELIOS_FTP_PORT}:-21");
+define("HELIOS_FTP_PASSIVE_MODE", ${HELIOS_FTP_PASSIVE_MODE:-false});
+define("HELIOS_SENDING_DESTINATION","${HELIOS_SENDING_DESTINATION:-/entree}");
+define("HELIOS_FTP_RESPONSE_SERVER_PATH","${HELIOS_FTP_RESPONSE_SERVER_PATH:-/sortie}");
 define('HELIOS_FTP_LOGIN',"${HELIOS_FTP_LOGIN}");
 define('HELIOS_FTP_PASSWORD',"${HELIOS_FTP_PASSWORD}");
 
