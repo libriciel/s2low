83bdc5e40f9ce58c7daf5024cf49640bc359f519
Eric Pommateau <eric.pommateau@adullact-projet.coop>
2017-05-31 17:36:31 +0200

    Bon... c'est un peu le bazard...

--- docker-resources/init-docker.php
+++ docker-resources/init-docker.php
@@ -3,9 +3,9 @@
 require_once __DIR__."/../init/init.php";
 require_once __DIR__."/S2lowBootstrap.class.php";
 
-echo "Database hostname : ".DB_HOST."\n";
-echo "Database name : ".DB_DATABASE."\n";
-echo "Database name : ".DB_USER."\n";
+echo "Database hostname : ".DB_HOST_TEST."\n";
+echo "Database name : ".DB_DATABASE_TEST."\n";
+echo "Database name : ".DB_USER_TEST."\n";
 
 
 $slowBootrap = new S2lowBootstrap();
