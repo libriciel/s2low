4d919bb9a626a5433acddd635f8a2ff0f6637961
Eric Pommateau <eric.pommateau@adullact-projet.coop>
2017-05-31 18:03:37 +0200

    Ménage

--- docker-resources/init-docker.php
+++ docker-resources/init-docker.php
@@ -3,10 +3,5 @@
 require_once __DIR__."/../init/init.php";
 require_once __DIR__."/S2lowBootstrap.class.php";
 
-echo "Database hostname : ".DB_HOST_TEST."\n";
-echo "Database name : ".DB_DATABASE_TEST."\n";
-echo "Database name : ".DB_USER_TEST."\n";
-
-
 $slowBootrap = new S2lowBootstrap();
 $slowBootrap->bootstrap($sqlQuery);
