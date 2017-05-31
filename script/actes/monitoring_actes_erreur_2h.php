0ad771f9a6a3a2e75dd515e6a0cf6399931d73c9
Pierre-Emmanuel VIVER <pierre-emmanuel.viver@libriciel.coop>
2017-05-31 10:34:11 +0200

    ajout des scripts de monitoring actes

---
+++ script/actes/monitoring_actes_erreur_2h.php
@@ -0,0 +1,39 @@
+<?php
+
+//Script charge de verifier que le nombre d'acte revenu en erreur ne dépasse pas un seuil
+//sur une tranche horaire
+
+//RETOURNE 0 si tout va bien
+//RETOURNE 2 si tout va mal
+require_once( __DIR__."/../../init/init.php");
+
+$email="s2low@libriciel.coop";
+$subject="Transaction actes a l etat erreur";
+
+$retour=0;
+$message="OK";
+$limit=75;
+$last_status="-1";
+$timestamp=time();
+$timestampmax=time()-(2*60*60);
+$sql="SELECT count(*) ".
+     "FROM actes_envelopes INNER JOIN actes_transactions ON actes_envelopes.id = actes_transactions.envelope_id ".
+     "WHERE actes_transactions.last_status_id = '".$last_status."' ".
+     "AND DATE_TRUNC('minute',actes_envelopes.submission_date) < DATE_TRUNC('minute',TIMESTAMP '".date("Y-m-d H:i:s",$timestamp)."') ".
+     "AND DATE_TRUNC('minute',actes_envelopes.submission_date) > DATE_TRUNC('minute',TIMESTAMP '".date("Y-m-d H:i:s",$timestampmax)."') ".
+     "AND (actes_transactions.type like '1' OR actes_transactions.type like '6') "  ;
+
+#echo "$sql \n";
+
+$nb_transac=$sqlQuery->queryOne($sql);
+
+#echo "$nb_transac \n";
+
+if( $nb_transac > $limit){
+        $message="CRITICAL";
+        $retour=2;
+        mail($email,$subject,"ATTENTION : $nb_transac transactions sont en erreur sur S2LOW sur une periode de 2h. La limite est a $limit actes en erreur.");
+}
+
+echo "$message - $nb_transac en erreur sur une periode de 2h\n";
+exit($retour);
---
+++ script/actes/monitoring_actes_poste_sup30min.php
@@ -0,0 +1,37 @@
+<?php
+
+//Script charge de verifier que le plus vieil acte à l'état poste
+//n'a pas plus de 30 minutes
+
+//RETOURNE 0 si tout va bien
+//RETOURNE 2 si le plus vieil acte à l'état posté à plus d'une heure
+require_once( __DIR__."/../../init/init.php");
+
+$email="s2low@libriciel.coop";
+$subject="Transaction actes a l etat poste";
+
+$retour=0;
+$message="OK";
+$limit=10;
+$last_status="1";
+$timestamp=time()-(30*60);
+$sql="SELECT count(*) ".
+     "FROM actes_envelopes INNER JOIN actes_transactions ON actes_envelopes.id = actes_transactions.envelope_id ".
+     "WHERE actes_transactions.last_status_id = '".$last_status."' ".
+     "AND DATE_TRUNC('minute',actes_envelopes.submission_date) < DATE_TRUNC('minute',TIMESTAMP '".date("Y-m-d H:i:s",$timestamp)."') ".
+     "AND (actes_transactions.type like '1' OR actes_transactions.type like '6') "  ;
+
+#echo "$sql \n";
+
+$nb_transac=$sqlQuery->queryOne($sql);
+
+#echo "$nb_transac \n";
+
+if( $nb_transac > $limit){
+        $message="CRITICAL";
+	$retour=2;
+        mail($email,$subject,"ATTENTION : $nb_transac transactions a etat poste sur S2LOW depuis plus de 30 minutes. La limite est a $limit actes a etat poste.");
+}
+
+echo "$message - $nb_transac etat poste de plus de 30 min\n";
+exit($retour);
