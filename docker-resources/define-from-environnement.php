bbd1f9e1b5e93c5a930cbd4e94ecdd3cf9daf2dc
Eric Pommateau <eric.pommateau@adullact-projet.coop>
2017-06-01 07:49:01 +0200

    Fonctionnement PHPUnit avec PHPStorm

---
+++ docker-resources/define-from-environnement.php
@@ -0,0 +1,25 @@
+<?php
+
+//Utilisé dans le cadre de PHPStorm qui ne permet pas de lancer des scripts après le démarrage du Docker
+//et qui ne lance pas l'entrypoint
+//Uniquement utilisé pour PHPUnit et Codeception donc
+
+//Et bien sur, ca interagi mal avec gitlab-ci...
+
+if (! file_exists("/etc/s2low/DockerSettings.php")) {
+    echo "DockerSettings n'existe pas : création à partir des variables d'environnement\n";
+	$script = __DIR__ . "/generate-config.sh";
+
+	`/bin/bash $script > /tmp/DockerSettings.php`;
+
+	require_once "/tmp/DockerSettings.php";
+
+	// Toujours sur phpstorm, y a un bug avec ça...
+	//https://www.quora.com/How-do-I-fix-Class-PHPUnit_Util_Configuration-not-found-error-in-PHPUNIT-2
+	if (!defined('PHPUNIT_COMPOSER_INSTALL')) {
+		define('PHPUNIT_COMPOSER_INSTALL', __DIR__ . '/../vendor/autoload.php');
+	}
+
+	exec("/bin/bash ". __DIR__."/create-directory-structure.sh");
+
+}
--- test/PHPUnit/bootstrap.php
+++ test/PHPUnit/bootstrap.php
@@ -6,11 +6,11 @@ define('HELIOS_FILES_UPLOAD_ROOT', "vfs://test/helios/");
 
 set_include_path(__DIR__."/../../ext/" . PATH_SEPARATOR .   get_include_path());
 
+require_once __DIR__."/../../docker-resources/define-from-environnement.php";
 
 
 require_once(__DIR__."/../../init/init.php");
 
-//require_once(PHP_UNIT_AUTOLOADER);
 
 require_once(__DIR__."/S2lowTestCase.class.php");
 
