111297e71b68748ed75beab13d9d4dd6d74b2ebd
Eric Pommateau <eric.pommateau@libriciel.coop>
2017-06-06 16:53:18 +0200

    Hotfix récupération SAE

--- cron/pastell-verif-transfert-sae-actes.php
+++ cron/pastell-verif-transfert-sae-actes.php
@@ -2,7 +2,10 @@
 require_once( __DIR__ . "/../init/init.php");
 
 $actesTransactionsSQL = new ActesTransactionsSQL($sqlQuery);
-$allTransactions = $actesTransactionsSQL->getArchiveFromStatus(12);
+
+$date = date("Y-m-d",strtotime("-30 days"));
+
+$allTransactions = $actesTransactionsSQL->getLastArchiveFromStatus(12,$date);
 
 echo count($allTransactions). " transactions ACTES trouvees dans l'etat <envoye au SAE>\n";
 
