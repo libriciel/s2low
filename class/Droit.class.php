ed48c8c99d3b7c3fb197b5e6b7206ac8902325a4
Eric Pommateau <eric.pommateau@libriciel.coop>
2017-07-11 14:24:45 +0200

    Couverture de la classe droit

--- class/Droit.class.php
+++ class/Droit.class.php
@@ -2,7 +2,6 @@
 
 class Droit {
 
-	//hasDroit($user_id,$authority_id,$module_id,$droit)
 
 	public function canAccess($moduleInfo,$userInfo,$authorityInfo,$groupeInfo,$droitModuleInfo,$permUser, $droit_specific = array()){	
 		
