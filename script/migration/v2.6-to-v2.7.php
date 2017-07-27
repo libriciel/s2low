df6bb2328d41943fb5b4466d9dd3df2bca6a9e44
Eric Pommateau <eric.pommateau@libriciel.coop>
2017-07-27 17:14:56 +0200

    Orthographe dans les status #212

--- script/migration/v2.6-to-v2.7.php
+++ script/migration/v2.6-to-v2.7.php
@@ -7,5 +7,8 @@ require_once( __DIR__."/../../init/init.php");
 
 $sql = "UPDATE actes_status SET name='Acquittement de document reçu' WHERE id=11";
 $sqlQuery->query($sql);
-
 echo "Mise à jour du libellé du status 11 - Aquittement de document reçu -> Acquittement de document reçu\n";
+
+$sql = "UPDATE actes_status SET name=? WHERE id=?";
+$sqlQuery->query($sql,"En attente d'être signé",18);
+echo "Mise à jour du libellé du status 18 - En attente d'être signée -> En attente d'être signé\n";
