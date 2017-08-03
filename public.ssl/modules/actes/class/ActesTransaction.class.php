35e6fcc1e51f4f76ca3cb688670a227524693562
Eric Pommateau <eric.pommateau@libriciel.coop>
2017-08-03 16:13:40 +0200

    Les transactions de type envoi de la préfecture sont affichés pour tout le monde  #223 ~Actes

--- public.ssl/modules/actes/class/ActesTransaction.class.php
+++ public.ssl/modules/actes/class/ActesTransaction.class.php
@@ -1462,7 +1462,8 @@ class ActesTransaction extends DataObject {
     if ($new && $this->get('authority_id')){
     	$sql_verif = "SELECT actes_transactions.id FROM actes_transactions ".
     			" WHERE actes_transactions.number='" . pg_escape_string($this->get('number')) . "' AND authority_id=" . $this->get('authority_id');
-    	if ($this->db->getOneValue($sql_verif)){
+
+    	if ($this->type == 1 && $this->db->getOneValue($sql_verif)){
     		$this->errorMsg = "Une transaction avec le même numéro existe déjà dans la base.";
     		$this->db->rollback();
     		return false;
