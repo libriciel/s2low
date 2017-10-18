488c3b59cff753899ccb5546d4174f7a5a134c5f
Eric Pommateau <eric.pommateau@libriciel.coop>
2017-10-18 15:36:58 +0200

    Script de grand ménage helios

---
+++ cron/helios-grand-menage.php
@@ -0,0 +1,54 @@
+<?php
+
+require_once(__DIR__ . "/../init/init.php");
+
+// supprime les fichier de plus de nb jours avec nb passé en parametre
+// mettre la chaine de caractère ok en second parametre pour faire la suppression
+
+$openStackSwiftWrapper = $objectInstancier->get('OpenStackSwiftWrapper');
+
+if (isset($argv[1])){
+    $nb_days = $argv[1];
+} else {
+    $nb_days = 99999;
+}
+if (intval($nb_days) == 0){
+    $nb_days = 99999;
+}
+
+if (empty($argv[2]) || $argv[2] != 'ok'){
+    $confirm = false;
+} else {
+    $confirm = true;
+}
+
+echo "Suppression des fichiers de plus de $nb_days jours\n";
+
+$submission_date = date("Y-m-d",strtotime("-$nb_days days"));
+
+$sql = "SELECT id,sha1,filename,submission_date FROM helios_transactions WHERE is_in_cloud=TRUE AND submission_date<? ORDER BY id ASC";
+
+$all_pes = $sqlQuery->query($sql,$submission_date);
+
+echo "Il y a ".count($all_pes)." PES ALLER à analyser\n";
+
+foreach($all_pes as $pes) {
+    echo "Analyse du fichier {$pes['sha1']} - {$pes['id']} - {$pes['submission_date']}\n";
+    $filename = HELIOS_FILES_UPLOAD_ROOT."/{$pes['sha1']}";
+    if (! file_exists($filename)){
+        echo "Le fichier n'existe pas... [PASS]\n";
+        continue;
+    }
+    if ($openStackSwiftWrapper->fileExistsOnCloud(PesAllerStorage::CONTAINER_NAME,$pes['sha1'])){
+        echo "Le fichier existe sur le cloud, supression...\n";
+        if($confirm){
+            unlink($filename);
+            echo "Fichier $filename supprimé [OK] \n";
+        } else {
+            echo "Le fichier $filename aurait été supprimé [PASS]\n";
+        }
+    }
+
+}
+
+
