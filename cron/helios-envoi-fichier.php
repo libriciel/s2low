740f3aebf4e2739827158c99b79de85c4b8a0c28
Eric Pommateau <eric.pommateau@libriciel.coop>
2017-11-13 17:19:01 +0100

    Supervisord pour prod

--- cron/helios-envoi-fichier.php
+++ cron/helios-envoi-fichier.php
@@ -1,5 +1,11 @@
 #! /usr/bin/php
 <?php
+
+/* ATTENTION - NE PAS UTILISER EN PRODUCTION */
+/* le serveur FTP de la DGFIP ne supporte pas l'envoi et la réception simultané */
+/* Il faut utiliser helios-reception-envoi à la place */
+
+
 require_once( __DIR__ . "/../init/init.php");
 
 $start = time();
---
+++ cron/helios-reception-envoi.php
@@ -0,0 +1,29 @@
+#! /usr/bin/php
+<?php
+require_once (__DIR__."/../config/config.php");
+
+
+require_once (__DIR__."/../class/FTP.class.php");
+
+$ftp = new FTP();
+$ftp->setConnexionInfo(HELIOS_FTP_SERVER, HELIOS_FTP_PORT, HELIOS_FTP_LOGIN, HELIOS_FTP_PASSWORD);
+if (HELIOS_SENDING_MODE_DEMO){
+    $ftp->setDeleteFileAfterDownload();
+}
+try {
+    /* Réception des fichiers*/
+    $ftp->recupAll(HELIOS_FTP_RESPONSE_SERVER_PATH, HELIOS_FTP_RESPONSE_TMP_LOCAL_PATH);
+    echo "Récupération terminée\n";
+} catch (Exception $e){
+    echo "Problème lors de la récupération des enveloppes : " . $e->getMessage() ." \n";
+    exit;
+}
+
+sleep(10);
+
+/* Envois des fichiers*/
+/** @var HeliosEnvoiControler $heliosEnvoiControler */
+$heliosEnvoiControler = $objectInstancier->get("HeliosEnvoiControler");
+$heliosEnvoiControler->sendAllTransactions();
+
+sleep(60);
