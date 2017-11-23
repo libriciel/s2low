bb7877f9ee58ef0c1ad174cd5d2fd75e52cf552b
Eric Pommateau <eric.pommateau@libriciel.coop>
2017-11-23 11:52:36 +0100

    typo

--- script/installation/check-macro.php
+++ script/installation/check-macro.php
@@ -5,7 +5,7 @@ require_once( __DIR__ . "/../../config/LoadLocalSettings.php");
 echo "récupération des constantes surchargées";
 $constantes_surchargee = get_defined_constants();
 
-require_once( __DIR__ . "/../../../config/config.php");
+require_once( __DIR__ . "/../../config/config.php");
 
 echo "récupération de toutes les constantes";
 $constantes_globales = get_defined_constants();
