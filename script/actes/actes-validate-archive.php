2dff278728164be4ab44ca6745aff753b6f55871
Unknown <eric@sigmalis.com>
2018-01-24 15:39:16 +0100

    script permettant la validation d'une archive actes ~Actes

---
+++ script/actes/actes-validate-archive.php
@@ -0,0 +1,24 @@
+<?php
+
+require_once __DIR__."/../../init/init.php";
+
+if ($argc < 1){
+    echo "Usage {$argv[0]} archive_path\n";
+    echo "{$argv[0]} : permet d'afficher la validation d'une enveloppe tel que s2low le ferait";
+    exit(-1);
+}
+
+$archive_path = $argv[1];
+
+$archive = new \Libriciel\LibActes\ArchiveValidator($objectInstancier->get('actes_appli_trigramme'));
+
+try {
+    $archive->validate($archive_path);
+} catch (Exception $e){
+    echo "L'archive n'est pas valide : " . $e->getMessage();
+    return false;
+}
+
+echo "L'archive est valide";
+
+
