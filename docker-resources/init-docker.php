6fbc998ea2b6d74398da25d0e7d50871ab213a70
Pierre-Emmanuel VIVER <pierre-emmanuel.viver@libriciel.coop>
2018-03-01 16:04:25 +0100

    on cree la bdd de test uniquement si necessaire

--- docker-resources/init-docker.php
+++ docker-resources/init-docker.php
@@ -10,21 +10,3 @@ $slowBootrap = $objectInstancier->get('S2lowBootstrap');
 
 
 $slowBootrap->bootstrap();
-
-
-if (DB_DATABASE_TEST) {
-
-	$objectInstancierTest = new ObjectInstancier();
-	$sqlQueryTest = new SQLQuery(DB_DATABASE_TEST);
-	$sqlQueryTest->setCredential(DB_USER_TEST, DB_PASSWORD_TEST);
-	$sqlQueryTest->setDatabaseHost(DB_HOST_TEST);
-	$sqlQueryTest->setClientEncoding(DB_CLIENT_ENCODING);
-	$objectInstancierTest->set('SQLQuery',$sqlQueryTest);
-	$objectInstancierTest->set('database_json_definition_filepath',__DIR__."/../db/s2low.sql.json");
-	$objectInstancierTest->set('database_sql_definition_filepath',__DIR__."/../db/s2low.sql");
-
-	/** @var PostgreSQLController $postgreSQLController */
-	$postgreSQLController = $objectInstancierTest->get('PostgreSQLController');
-	$postgreSQLController->alterDatabase(function($m){echo "[Mise à jour base de données de test]".$m."\n";});
-}
-
