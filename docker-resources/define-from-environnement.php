e1d06cf9d54c345d08f5faf658306a89a26074a7
Unknown <eric@sigmalis.com>
2018-03-08 09:14:17 +0100

    Correction test unitaire

--- docker-resources/define-from-environnement.php
+++ docker-resources/define-from-environnement.php
@@ -21,7 +21,7 @@ if (! file_exists("/etc/s2low/DockerSettings.php")) {
 	}
 
 	exec("/bin/bash ". __DIR__."/create-directory-structure.sh");
-
+	return;
 }
 
 $contenu=file_get_contents("/etc/s2low/DockerSettings.php");
