3b8489fdc3e20da55d4777670db238ec60fb6ffe
Unknown <eric@sigmalis.com>
2018-04-13 11:32:49 +0200

    Ajout d'un script de test

---
+++ test/actes/test-actes-enveloppe-in-directory.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once(__DIR__."/../../init/init.php");
+
+
+$archive = new \Libriciel\LibActes\Archive();
+
+$archive_info  = $archive->getArchiveDataFromFolder($argv[1]);
+
+print_r($archive_info);
