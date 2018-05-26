e48d186d3f432651300c7c39a7c7c6e8015866eb
Unknown <eric@sigmalis.com>
2018-05-26 06:50:09 +0200

    Correction gros bug

--- class/Antivirus.class.php
+++ class/Antivirus.class.php
@@ -1,6 +1,5 @@
 <?php
-class Antivirus
-{
+class Antivirus {
 	public $errorMsg;
 
     /**
@@ -26,9 +25,8 @@ class Antivirus
 		Trace::wrap_exec("chmod 644 $new_file",$output, $ret);
 	 	
 		Trace::wrap_exec(ANTIVIRUS_COMMAND . " $new_file", $output, $ret);
-		Trace::wrap_exec("rm $new_file",$output, $ret);
-
-
+		Trace::wrap_exec("rm $new_file",$output1,$ret1);
+		
 		switch ($ret) {
 		  case 0:
 				$returnValue= true;
