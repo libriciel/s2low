4bfdc4fbfde3873990873a7611b647a039756831
Unknown <eric@sigmalis.com>
2018-06-25 11:34:01 +0200

    ajout des fonction de l'API : admin/services/add-service-user.php, admin/services/list-service.php, admin/users/add-user-to-service.php  #350

--- public.ssl/admin/services/add-service-user.php
+++ public.ssl/admin/services/add-service-user.php
@@ -1,29 +1,4 @@
-<?php 
+<?php
 
-include("init.php");
-
-$name =  Helpers::getVarFromPost('name');
-if(!$name){
-	header('Location: admin_services.php');
-	exit;
-}
-
-$authority_id =  Helpers::getVarFromPost('authority_id');
-if ($authority_id){
-
-	$authorities = $me->getAllPossibleAuthority();
-	if (! in_array($authority_id,array_keys($authorities))){
-		header("Location: admin_services.php");
-		exit;
-	}
-} else {
-	$authority_id = $me->get('authority_id');
-}
-
-$result = $serviceUser->add($name,$authority_id);
-
-if (! $result){
-	$_SESSION['error'] = "Ce groupe existe déjà !";
-}
-	
-header('Location: admin_services.php?authority_id='.$authority_id);
+require_once( __DIR__ . "/../../../init/init.php");
+$frontController->go("AdminService","add");
