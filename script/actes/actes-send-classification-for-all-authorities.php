1fc30968c74b5117180fa8c5193b821ae6fffb64
Pierre-Emmanuel VIVER <pierre-emmanuel.viver@libriciel.coop>
2019-04-09 11:16:36 +0200

    Correctif sur le contrôle du dépt ds l'envoi des demande de classif

--- script/actes/actes-send-classification-for-all-authorities.php
+++ script/actes/actes-send-classification-for-all-authorities.php
@@ -2,18 +2,18 @@
 declare(ticks = 1);
 
 /*
- * Ce script permet d'envoyer une demande de classification pour TOUTES les collectivités utilisant le module actes.
- * Il est aussi possible de spécifier un département. cela enverra une demande de classification pour TOUTES
- * les collectivités présentes dans ce département.
+ * Ce script permet d'envoyer une demande de classification pour TOUTES les collectivites utilisant le module actes.
+ * Il est aussi possible de specifier un departement. cela enverra une demande de classification pour TOUTES
+ * les collectivites presentes dans ce departement.
  */
 
 require_once __DIR__."/../../init/init.php";
 
 if ($argc == 2 && ((int)$argv[1] == 0) ){
     echo "Usage {$argv[0]} [departement]\n";
-    echo "{$argv[0]} : permet d'envoyer une demande de classification à toutes les collectivités utilisant le module actes.\n
-    Si le departement est passé en arguement (optionnel), cela enverra une demande de classificaiton pour toutes les collectivité
-    utilisant le module acte et présente dans ce département.\n";
+    echo "{$argv[0]} : permet d'envoyer une demande de classification a toutes les collectivites utilisant le module actes.\n
+    Si le departement est passe en arguement (optionnel), cela enverra une demande de classificaiton pour toutes les collectivite
+    utilisant le module acte et presente dans ce departement.\n";
     exit(-1);
 }
 
@@ -25,11 +25,13 @@ if ($argc == 2
         )
     )
 {
-    echo "Le département ".(int)$argv[1]." doit être compris entre 0 et 1000\n";
-    exit(-1);
-}elseif($argc == 2 && (!((int)$argv[1] > 0)) && (!((int)$argv[1] < 1000))){
-    $departement=(int)$argv[1];
-}
+  echo "Le departement ".(int)$argv[1]." doit etre compris entre 0 et 1000\n";
+  exit(-1);
+}elseif($argc == 2
+        &&
+        ((int)$argv[1] > 0) && ((int)$argv[1] < 1000)
+){
+
 
 $actesClassificationCreation = new ActesClassificationCreation();
 $actesClassificationCreation->sendToAllAuthorities($departement);
