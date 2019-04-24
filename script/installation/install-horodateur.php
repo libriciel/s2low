811f6522c26f4446c6384572ea84a75f8728aaf2
Eric Pommateau <eric.pommateau@libriciel.coop>
2019-04-24 17:06:23 +0200

    ajout de script

--- script/installation/install-horodateur.php
+++ script/installation/install-horodateur.php
@@ -1,8 +1,9 @@
 <?php
 
+
 require_once(__DIR__ . "/../../init/init.php");
 require_once (__DIR__."/../../docker-resources/S2lowBootstrap.class.php");
 
 ObjectInstancierFactory::getObjetInstancier()->get(S2lowLogger::class)->enableStdOut(true);
 $s2lowBootstrap = ObjectInstancierFactory::getObjetInstancier()->get(S2lowBootstrap::class);
-$s2lowBootstrap->majLibersign();
+$s2lowBootstrap->installHorodateur();
