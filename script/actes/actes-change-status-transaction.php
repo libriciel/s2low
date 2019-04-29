22fbbb36ccb5e0f73fa5bce7ba00d8ff21cd450f
Pierre-Emmanuel VIVER <pierre-emmanuel.viver@libriciel.coop>
2019-04-29 15:15:01 +0200

    Script permettant de changer le statut d'une transaction

---
+++ script/actes/actes-change-status-transaction.php
@@ -0,0 +1,14 @@
+<?php
+
+/** Change le status d'une transaction donnée en paramètre
+ */
+
+require_once ( __DIR__."/../../init/init.php");
+$idtransaction = 1;
+$status = 16;
+$message = "Changement de statut realise manuellement par P. VIVER";
+$objectInstancier = ObjectInstancierFactory::getObjetInstancier();
+
+$actesTransactions = $objectInstancier->get(ActesTransactionsSQL::class);
+
+$actesTransactions->updateStatus($idtransaction,$status,$message);
