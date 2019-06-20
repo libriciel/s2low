36843f4cfb6551b93b269172ec5584544acbd3aa
Eric Pommateau <eric.pommateau@libriciel.coop>
2019-06-20 15:52:30 +0200

    Ajout de log sur la console pour le script helios-store-pes-aller-in-cloud

--- cron/helios-store-pes-aller-in-cloud.php
+++ cron/helios-store-pes-aller-in-cloud.php
@@ -2,6 +2,7 @@
 
 declare(ticks = 1);
 
+
 require_once(__DIR__ . "/../init/init.php");
 
 /** @var ScriptSleeping $scriptSleeping */
@@ -9,8 +10,16 @@ $scriptSleeping = $objectInstancier->get("ScriptSleeping");
 
 $scriptSleeping->debut("helios-store-pes-aller-in-cloud");
 
+$logger  = $objectInstancier->get("Monolog\Logger");
+$logger->pushHandler(new  Monolog\Handler\StreamHandler('php://stdout'));
+
+$logger->info("Starting helios-store-envelope-in-cloud");
+
+
 /** @var PesAllerStorage $pesAllerStorage */
 $pesAllerStorage = $objectInstancier->get("PesAllerStorage");
 $pesAllerStorage->storeAll();
-
+$logger->info("Ending actes-store-envelope-in-cloud");
 $scriptSleeping->fin();
+exit(0);
+
