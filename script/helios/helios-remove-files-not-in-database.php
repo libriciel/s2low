0f33c451b64b115059322decff0b1504c1aed78f
Eric Pommateau <eric.pommateau@libriciel.coop>
2019-10-09 15:39:45 +0200

    Supression des fichiers helios pas dans la base

---
+++ script/helios/helios-remove-files-not-in-database.php
@@ -0,0 +1,26 @@
+<?php
+require_once(__DIR__ . "/../../init/init.php");
+
+
+$dh = opendir(HELIOS_FILES_UPLOAD_ROOT);
+
+
+$heliosTransactionsSQL = $objectInstancier->get(HeliosTransactionsSQL::class);
+
+while (($file = readdir($dh)) !== false) {
+
+	if (in_array($file,array('.','..'))){
+		continue;
+	}
+
+	$nb_files = $heliosTransactionsSQL->isDuplicate($file);
+	echo "Fichier $file : $nb_files trouvé(s)\n";
+
+	if ($nb_files == 0){
+		echo "Destruction du fichier\n";
+		//unlink(HELIOS_FILES_UPLOAD_ROOT."/$file");
+	}
+
+
+}
+closedir($dh);
