0f290f381da03fd0f3af633e555dfb797645a7e4
Eric Pommateau <eric.pommateau@libriciel.coop>
2019-11-12 15:19:10 +0100

    Improve sql request for postgresql analyser

---
+++ test/PHPUnit/class/helios/HeliosTransactionsListeTest.php
@@ -0,0 +1,14 @@
+<?php
+
+class HeliosTransactionsListeTest extends S2lowTestCase {
+
+	use HeliosUtilitiesTestTrait;
+
+	public function testGetAll(){
+		$this->createTransaction();
+		$heliosTransactionsListe  = $this->getObjectInstancier()->get(HeliosTransactionsListe::class);
+		$all = $heliosTransactionsListe->getAll();
+		$this->assertEquals('toto.txt',$all[0]['filename']);
+	}
+
+}
