1c35fd22fa653d333d791c51fef2b12c826cf094
Julien Le Gall <juli1legall@gmail.com>
2019-11-13 15:53:27 +0100

    Vérification qu'un message est bien passé à l'état transmis dans les dernières minutes, envoi d'un mail sinon.

--- script/actes/monitoring_actes_attente_sup30min.php
+++ script/actes/monitoring_actes_attente_sup30min.php
@@ -15,22 +15,23 @@ $message="OK";
 $limit=10;
 $last_status="2";
 $timestamp=time()-(30*60);
-$sql="SELECT count(*) ".
-     "FROM actes_envelopes INNER JOIN actes_transactions ON actes_envelopes.id = actes_transactions.envelope_id ".
-     "WHERE actes_transactions.last_status_id = '".$last_status."' ".
-     "AND DATE_TRUNC('minute',actes_envelopes.submission_date) < DATE_TRUNC('minute',TIMESTAMP '".date("Y-m-d H:i:s",$timestamp)."') ".
-     "AND (actes_transactions.type like '1' OR actes_transactions.type like '6') "  ;
 
-#echo "$sql \n";
+$sql_alert_Last_reception="SELECT NOW()-MAX(date) > INTERVAL '10 minutes' FROM actes_transactions_workflow WHERE status_id =3";
+
+$alert_last_transmission=$sqlQuery->queryOne($sql_alert_Last_reception);
+
+$sql="SELECT count(*) ".
+    "FROM actes_envelopes INNER JOIN actes_transactions ON actes_envelopes.id = actes_transactions.envelope_id ".
+    "WHERE actes_transactions.last_status_id = '".$last_status."' ".
+    "AND DATE_TRUNC('minute',actes_envelopes.submission_date) < DATE_TRUNC('minute',TIMESTAMP '".date("Y-m-d H:i:s",$timestamp)."') ".
+    "AND (actes_transactions.type like '1' OR actes_transactions.type like '6') "  ;
 
 $nb_transac=$sqlQuery->queryOne($sql);
 
-#echo "$nb_transac \n";
-
-if( $nb_transac > $limit){
-        $message="CRITICAL";
-	$retour=2;
-        mail($email,$subject,"ATTENTION : $nb_transac transactions a etat en attente sur S2LOW depuis plus de 30 minutes. La limite est a $limit actes a etat en attente.");
+if(($nb_transac > 0) && $alert_last_transmission) {
+    $message = "CRITICAL";
+    $retour = 2;
+    mail($email, $subject, "ATTENTION : Aucun message transmis depuis 10 minutes.\n $nb_transac transactions a etat en attente sur S2LOW depuis plus de 30 minutes. La limite est a $limit actes a etat en attente.");
 }
 
 echo "$message - $nb_transac etat en attente de plus de 30 min\n";
