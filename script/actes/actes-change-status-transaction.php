d156d31ee34a9a1ac8ba9dd300303b8ad309f4d0
Julien Le Gall <juli1legall@gmail.com>
2019-11-13 17:15:01 +0100

    Généralisation de l'administration

--- script/actes/actes-change-status-transaction.php
+++ script/actes/actes-change-status-transaction.php
@@ -6,7 +6,7 @@
 require_once ( __DIR__."/../../init/init.php");
 $idtransaction = 1;
 $status = 16;
-$message = "Changement de statut realise manuellement par P. VIVER";
+$message = "Changement de statut realise manuellement par l'administrateur";
 $objectInstancier = ObjectInstancierFactory::getObjetInstancier();
 
 $actesTransactions = $objectInstancier->get(ActesTransactionsSQL::class);
