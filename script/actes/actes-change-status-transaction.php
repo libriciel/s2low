2a81e763314387602496fb3ae248df945d7b45ca
Julien Le Gall <juli1legall@gmail.com>
2019-11-20 16:41:08 +0100

    Corrections Eric

--- script/actes/actes-change-status-transaction.php
+++ script/actes/actes-change-status-transaction.php
@@ -6,17 +6,18 @@
 require_once ( __DIR__."/../../init/init.php");
 
 $nbParams = count($argv) - 1;
-if(!($nbParams==2)){
-    echo "Nombre de paramètres incorrect. ($nbParams fourni(s), 2 attendus)\nUsage : php actes-change-status-transaction.php idtransation status\n";
-    return 1;
+
+if($nbParams!=2){
+    echo "Nombre de paramètres incorrect. ($nbParams fourni(s), 2 attendus)\nUsage : php $argv[0] idtransation status\n";
+    return -1;
 }
 
-$idtransaction = (int) $argv[1];
+$idTransaction = (int) $argv[1];
 $status = (int) $argv[2];
 
-if(!(strval($idtransaction) == $argv[1] ) || !(strval($status) == $argv[2]) ){
+if(!(strval($idTransaction) == $argv[1] ) || !(strval($status) == $argv[2]) ){
     echo "Les paramètres doivent être des entiers\n";
-    return 1;
+    return -2;
 }
 
 $message = "Changement de statut realise manuellement par l'administrateur";
@@ -24,4 +25,6 @@ $objectInstancier = ObjectInstancierFactory::getObjetInstancier();
 
 $actesTransactions = $objectInstancier->get(ActesTransactionsSQL::class);
 
-$actesTransactions->updateStatus($idtransaction,$status,$message);
+$actesTransactions->updateStatus($idTransaction,$status,$message);
+
+return 0;
