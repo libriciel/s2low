d9ce788cdb8a84772810ccb785256ef68d99880f
jlegall <julien.legall@adullact-projet.coop>
2019-12-26 11:26:45 +0100

    Vérification paramètres entrée

--- docker-resources/certificate/recup_ac_ministere.sh
+++ docker-resources/certificate/recup_ac_ministere.sh
@@ -1,27 +1,38 @@
 #!/bin/bash
 
-DIR1="/etc/apache2/ssl"
-DIR2="/etc/ssl/certs/"
-
-if [ -e $1 ]; then
-	DIR1=$1
+if [ -z "$1" ] || [ -z "$2" ];
+then
+	echo "Syntaxe : ./recup_ac_ministere.sh CertPath CAPath";
+	exit 1;
 fi
 
-if [ -e $2 ]; then
-	DIR2=$2
+
+if [[ $1 != /* ]] || [[ $1 != /* ]];
+then
+	echo "Les répertoires doivent être absolus";
+	exit 2;
 fi
 
-cd $DIR1
+if [ ! -d "$1" ] || [ ! -d "$2" ];
+then
+	echo "Les deux répertoires doivent exister";
+	exit 3;
+fi
 
-echo "Copie des certificats dans "$DIR1
+DIR1=$1
+DIR2=$2
+
+cd "$DIR1" || exit 4;
+
+echo "Copie des certificats dans $DIR1"
 
 wget https://ressources.libriciel.fr/s2low/certificat-mi-chaine.pem
 wget https://ressources.libriciel.fr/s2low/ac-racine-mi.pem
 wget https://ressources.libriciel.fr/s2low/serveur-1e.pem
 
-cd $DIR2
+cd "$DIR2" || exit 5;
 
-echo "Creation des liens symboliques dans "$DIR2
-ln -sfn $DIR1/ac-racine-mi.pem
-ln -sfn $DIR1/serveur-1e.pem
+echo "Creation des liens symboliques dans $DIR2"
+ln -sfn "$DIR1"/ac-racine-mi.pem
+ln -sfn "$DIR1"/serveur-1e.pem
 c_rehash .
