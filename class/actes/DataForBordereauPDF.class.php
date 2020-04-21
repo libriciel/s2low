3239bf2089a6ffdf99be4b5d393027c578c8789e
Julien LE GALL <julien.legall@libriciel.coop>
2020-04-21 16:29:26 +0200

    Refacto

--- class/actes/DataForBordereauPDF.class.php
+++ class/actes/DataForBordereauPDF.class.php
@@ -7,12 +7,29 @@ class DataForBordereauPDF
     private $texteCollectivite;
     /** @var string  */
     private $texteUtilisateur;
-    /** @var array */
-    private $contenuTableau;
+
     /** @var array */
     private $fichier_table;
 
     private $cycle_table;
+    /**
+     * @var mixed
+     */
+    private $nature_description;
+    private $broadcasted;
+    /**
+     * @var mixed
+     */
+    private $broadcastEmails;
+    private $addEmailNotificationField;
+    private $classification;
+    private $arch_url;
+    private $typeDeTransaction;
+    private $numeroActe;
+    private $dateDecision;
+    private $objet;
+    private $presenceDocPapier;
+    private $idUnique;
 
     /**
      * @return string
@@ -35,7 +52,18 @@ class DataForBordereauPDF
      */
     public function getContenuTableau()
     {
-        return $this->contenuTableau;
+        return [
+            ["Type de transaction:",$this->typeDeTransaction],
+            ["Nature de l'acte:",$this->nature_description],
+            ["Numéro de l'acte:",$this->numeroActe],
+            ["Date de la décision:",$this->dateDecision],
+            ["Objet:",$this->objet],
+            ["Documents papiers complémentaires:",$this->presenceDocPapier],
+            ["Classification matières/sous-matières:",$this->classification],
+            ["Identifiant unique:",$this->idUnique],
+            ["URL d'archivage:",$this->arch_url],
+            ["Notification:",$this->getNotifieA()]
+        ];
     }
 
     /**
@@ -54,28 +82,93 @@ class DataForBordereauPDF
         return $this->cycle_table;
     }
 
-    public function setTexteCollectivite( string $nameCollectivite)
+    public function setIncludedFiles(array $files)
     {
-        $this->texteCollectivite = $nameCollectivite;
+        $filesTemp = [];
+        foreach ($files as $file) {
+            $fileTemp=[];
+            $written = false;
+            if ($file["posted_filename"])
+            {
+
+                $fileTemp[]=["Nom original :",$file["posted_filename"],$file["filetype"],$file["filesize"]];
+                $written = true;
+
+            }
+            if ($file["filename"])
+            {
+                list($size,$mimetype) = ['',''];
+                if(!$written){
+                    $size = $file["filesize"];
+                    $mimetype = $file["filetype"];
+                }
+                $fileTemp[]=["Nom métier:",$file["filename"], $mimetype, $size];
+            }
+            $filesTemp[]= $fileTemp;
+        }
+        $this->fichier_table = $filesTemp;
     }
 
-    public function setTexteUtilisateur($userName, $userGivenName)
+    public function setCycleTable($workflow,$status)
     {
-        $this->texteUtilisateur = $userName." ".$userGivenName;
+        //traiter des requêtes
+        $cycle_table=[];
+        foreach ($workflow as $stage) {
+            $cycle_table[] = [
+                $status[$stage["status_id"]],
+                Helpers :: getDateFromBDDDate($stage["date"], true),
+                $stage["message"]
+            ];
+        }
+        $this->cycle_table = $cycle_table;
     }
 
-    public function setContenuTableau(array $initDataTable)
-    {
-        $this->contenuTableau = $initDataTable;
+    public function setAddEmailNotificationField($addEmailNotificationField){
+        $this->addEmailNotificationField = $addEmailNotificationField;
     }
 
-    public function setFichierTable(array $initDatafichier_table)
-    {
-        $this->fichier_table = $initDatafichier_table;
+    /**
+     * @return string
+     */
+    private function getNotifieA(){
+        if (($this->broadcasted == 't') || ($this->addEmailNotificationField && $this->broadcastEmails) ) {
+            return "Notifiée à " . $this->broadcastEmails;
+        }
+        return "Non notifiée";
     }
 
-    public function setCycleTable(array $initcycle_table)
-    {
-        $this->cycle_table = $initcycle_table;
+    public function setDonneesTransaction(array $transaction, array $transactionComplement){
+        $this->texteCollectivite = $transaction[0]["authority_name"];
+        $this->texteUtilisateur = $transaction[0]["name"]." ".$transaction[0]["givenname"];
+
+        //traiter des requêtes
+        if(isset($transaction[0]["nature_descr"])){
+            $this->nature_description = $transaction[0]["nature_descr"];
+        } else {
+            $this->nature_description = "n/a";
+        }
+
+        $this->broadcasted = $transactionComplement["broadcasted"];
+        $this->broadcastEmails = $transactionComplement["broadcast_emails"];
+
+        $this->classification = $transactionComplement["classification"];
+        $classification_string = $transactionComplement["classification_string"];
+
+        if ($classification_string) {
+            $this->classification .= " - $classification_string";
+        }
+
+        $this->arch_url = $transaction[0]["archive_url"];
+        if (empty($arch_url))
+            $this->arch_url= "Non définie";
+
+        $this->typeDeTransaction = $transaction[0]["type_str"];
+
+        $this->numeroActe = $transaction[0]["number"];
+        $this->dateDecision = $transactionComplement["decision_date"];
+        $this->objet = $transaction[0]["subject"];
+        $this->presenceDocPapier = $transactionComplement["document_papier"]?"OUI":"NON";
+        $this->idUnique = $transactionComplement["unique_id"];
     }
+
 }
--- class/actes/ExtractDataForBordereauPDF.class.php
+++ class/actes/ExtractDataForBordereauPDF.class.php
@@ -24,107 +24,31 @@ class ExtractDataForBordereauPDF{
         $this->actesStatusSQL = $actesStatusSQL;
     }
 
+    /**
+     * @param $transactionId
+     * @param bool $addEmailNotificationField
+     * @return DataForBordereauPDF
+     * @throws Exception
+     */
+
     public function extract($transactionId,$addEmailNotificationField=false){
+        $data = new DataForBordereauPDF();
 
         $this->transactionSQL->setTransmissionId($transactionId);
         $transaction = $this->transactionSQL->getAll();
         $transactionComplement = $this->transactionSQL->getComplement($transactionId);
+
+        $data->setDonneesTransaction($transaction, $transactionComplement);
+        $data->setAddEmailNotificationField($addEmailNotificationField);
+
+        $includedFiles = $this->actesIncludedFileSQL->getAll($transactionId);
+        $data->setIncludedFiles($includedFiles);
+
         $workflow = $this->transactionSQL->fetchWorkflow($transactionId);
         $status = $this->actesStatusSQL->getAllStatus();
-        $includedFiles = $this->actesIncludedFileSQL->getAll($transactionId);
+        $data->setCycleTable($workflow,$status);
 
-        $data = new DataForBordereauPDF();
-
-        $data->setTexteCollectivite($transaction[0]["authority_name"]);
-        $data->setTexteUtilisateur($transaction[0]["name"],$transaction[0]["givenname"]);
-        $data->setContenuTableau($this->initDataTable($transaction, $transactionComplement, $addEmailNotificationField));
-        $data->setFichierTable($this->initDatafichier_table($includedFiles));
-        $data->setCycleTable($this->initcycle_table($workflow,$status));
-
+        //$data->setCycleVieTransaction($workflow,$status);
         return $data;
-    }
-
-    private function getNotifieA(array $transactionComplement,bool $addEmailNotificationField){
-        if ($transactionComplement["broadcasted"] == 't' ) {
-            return "Notifiée à " . $transactionComplement["broadcast_emails"];
-        }
-        if ($addEmailNotificationField && $transactionComplement["broadcast_emails"]){
-            return "Notifiée à " . $transactionComplement["broadcast_emails"];
-        }
-        return "Non notifiée";
-    }
-
-    public function initDataTable(array $transaction, array $transactionComplement, bool $addEmailNotificationField){
-        //traiter des requêtes
-        if(isset($transaction[0]["nature_descr"])){
-            $nature_description = $transaction[0]["nature_descr"];
-        } else {
-            $nature_description = "n/a";
-        }
-
-        $notification = $this->getNotifieA($transactionComplement,$addEmailNotificationField);
-
-        $classification = $transactionComplement["classification"];
-        $classification_string = $transactionComplement["classification_string"];
-
-        if ($classification_string) {
-            $classification .= " - $classification_string";
-        }
-
-        $arch_url = $transaction[0]["archive_url"];
-        if (empty($arch_url))
-            $arch_url= "Non définie";
-
-        return [
-            ["Type de transaction:",$transaction[0]["type_str"]],
-            ["Nature de l'acte:",$nature_description],
-            ["Numéro de l'acte:",$transaction[0]["number"]],
-            ["Date de la décision:",$transactionComplement["decision_date"]],
-            ["Objet:",$transaction[0]["subject"]],
-            ["Documents papiers complémentaires:",$transactionComplement["document_papier"]?"OUI":"NON"],
-            ["Classification matières/sous-matières:",$classification],
-            ["Identifiant unique:",$transactionComplement["unique_id"]],
-            ["URL d'archivage:",$arch_url],
-            ["Notification:",$notification]
-        ];
-    }
-
-    public function initDatafichier_table($files){
-        $filesTemp = [];
-        foreach ($files as $file) {
-            $fileTemp=[];
-            $written = false;
-            if ($file["posted_filename"])
-            {
-
-                $fileTemp[]=["Nom original :",$file["posted_filename"],$file["filetype"],$file["filesize"]];
-                $written = true;
-
-            }
-            if ($file["filename"])
-            {
-                list($size,$mimetype) = ['',''];
-                if(!$written){
-                    $size = $file["filesize"];
-                    $mimetype = $file["filetype"];
-                }
-                $fileTemp[]=["Nom métier:",$file["filename"], $mimetype, $size];
-            }
-            $filesTemp[]= $fileTemp;
-        }
-        return $filesTemp;
-    }
-
-    public function initcycle_table(array $workflow,array $status){
-        //traiter des requêtes
-        $cycle_table=[];
-		foreach ($workflow as $stage) {
-            $cycle_table[] = [
-                $status[$stage["status_id"]],
-                Helpers :: getDateFromBDDDate($stage["date"], true),
-                $stage["message"]
-            ];
-        }
-		return $cycle_table;
     }
 }
