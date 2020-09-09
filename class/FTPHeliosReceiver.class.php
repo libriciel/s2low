3edb95c5744da88bc3a9f2ce6ca26f0a4986a8ec
Julien LE GALL <julien.legall@libriciel.coop>
2020-09-09 16:37:38 +0200

    Utilisation de s2LowLogger

--- class/FTPHeliosReceiver.class.php
+++ class/FTPHeliosReceiver.class.php
@@ -9,9 +9,16 @@ class FTPHeliosReceiver implements Iterator {
     private $localPath;
     private $filesToProcess = [];
     private $index = 0;
+    private $s2lowLogger;
 
-    public function __construct(FTPService $FTPService, $helios_ftp_response_server_path, $helios_ftp_response_tmp_local_path)
+    public function __construct(
+        S2lowLogger $s2lowLogger,
+        FTPService $FTPService,
+        $helios_ftp_response_server_path,
+        $helios_ftp_response_tmp_local_path
+    )
     {
+        $this->s2lowLogger = $s2lowLogger;
         $this->FTPService = $FTPService;
         $this->remotePath = $helios_ftp_response_server_path;
         $this->localPath = $helios_ftp_response_tmp_local_path;
@@ -47,7 +54,7 @@ class FTPHeliosReceiver implements Iterator {
 	private function isPesAller($filename){
         $isPesAller = preg_match("#^PESALR2_#",basename($filename));
         if($isPesAller){
-            echo "$filename : PES ALLER ignoré\n";
+            $this->s2lowLogger->info("$filename : PES ALLER ignoré");
         }
         return $isPesAller;
     }
@@ -55,7 +62,7 @@ class FTPHeliosReceiver implements Iterator {
     public function retrieveNames(){
         $this->FTPService->connect();
 
-        echo "Remote_path : $this->remotePath\n";
+        $this->s2lowLogger->info("Remote_path : $this->remotePath");
 
         $all_file = $this->FTPService->getFileNames($this->remotePath);
         $this->filesToProcess = [];
@@ -75,7 +82,7 @@ class FTPHeliosReceiver implements Iterator {
     private function recupOneFile($file, $i): void
     {
         $ftp_get_result = $this->FTPService->retrieveFile($file, $this->localPath);
-        echo $i . " : " . $file . " récupéré : " . ($ftp_get_result ? "SUCCES" : "ECHEC") . "\n";
+        $this->s2lowLogger->info($i . " : " . $file . " récupéré : " . ($ftp_get_result ? "SUCCES" : "ECHEC")) ;
     }
 
     public function finTraitement()
