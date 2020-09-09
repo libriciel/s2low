fe2e1a0d9640a8b559fac20b8633463297742a96
Julien LE GALL <julien.legall@libriciel.coop>
2020-09-09 11:24:07 +0200

    Refacto

--- class/FTPHeliosSender.class.php
+++ class/FTPHeliosSender.class.php
@@ -2,14 +2,23 @@
 class FTPHeliosSender {
     /** @var FTPService  */
     private $FTPConnection;
-    private $pstMode;
+    private $isPstMode;
     private $pAppli;
+    private $destinationDirectory;
+    private $isPassiveMode;
 
-    public function __construct(FTPService $ftptemp, $helios_ftp_pst_mode,$helios_ftp_p_appli)
-    {
+    public function __construct(
+        FTPService $ftptemp,
+        $helios_ftp_pst_mode,
+        $helios_ftp_p_appli,
+        $helios_sending_destination,
+        $helios_ftp_passive_mode
+    ) {
         $this->FTPConnection = $ftptemp;
-        $this->pstMode = $helios_ftp_pst_mode;
+        $this->isPstMode = $helios_ftp_pst_mode;
         $this->pAppli = $helios_ftp_p_appli;
+        $this->destinationDirectory = $helios_sending_destination;
+        $this->isPassiveMode = $helios_ftp_passive_mode;
     }
 
     /**
@@ -21,18 +30,26 @@ class FTPHeliosSender {
     public function sendFile(string $p_dest, string $p_msg, string $file_to_send): void
     {
         $this->FTPConnection->connect();
-        $this->FTPConnection->setPassiveMode(HELIOS_FTP_PASSIVE_MODE);
-        if($this->pstMode){
-            $command = "site meta P_DEST={$p_dest};P_APPLI={$this->pAppli};P_MSG=$p_msg";          // TODO : self::P_APPLI=THELPES2 const ??
-            echo "$command\n";
-            $this->FTPConnection->sendRawCommand($command, false/*HELIOS_SENDING_MODE_DEMO*/);
-        } else {
-            $this->FTPConnection->sendRawCommand("site P_DEST {$p_dest}",HELIOS_SENDING_MODE_DEMO);
-            $this->FTPConnection->sendRawCommand("site P_APPLI {$this->pAppli}",HELIOS_SENDING_MODE_DEMO);  // TODO : self::P_APPLI=THELPES2 const ??
-            $this->FTPConnection->sendRawCommand("site P_MSG $p_msg",HELIOS_SENDING_MODE_DEMO);
-
-        }
-        $this->FTPConnection->sendOneFile("depot/"/*HELIOS_SENDING_DESTINATION*/, $file_to_send);
+        $this->FTPConnection->setPassiveMode($this->isPassiveMode);
+        $this->configureFileProperties($p_dest, $p_msg);
+        $this->FTPConnection->sendOneFile($this->destinationDirectory, $file_to_send);
         $this->FTPConnection->disconnect();
+    }
+
+    /**
+     * @param bool $p_dest Code destinataire
+     * @param string $p_msg de la forme CodFich#CodColl#IdPost#CodBud
+     * @throws Exception
+     */
+    private function configureFileProperties(string $p_dest, string $p_msg): void
+    {
+        if ($this->isPstMode) {
+            $commands = ["site meta P_DEST={$p_dest};P_APPLI={$this->pAppli};P_MSG=$p_msg"];
+        } else {
+            $commands=["site P_DEST {$p_dest}","site P_APPLI {$this->pAppli}","site P_MSG $p_msg"];
+        }
+        foreach ($commands as $command){
+            $this->FTPConnection->sendRawCommand($command);
+        }
     }
 }
