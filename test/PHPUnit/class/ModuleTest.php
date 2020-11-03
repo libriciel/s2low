e9d0c79d87afb224a6a5529ad5a7ff31b0bcf4f4
Eric Pommateau <eric.pommateau@libriciel.coop>
2020-11-03 16:08:13 +0100

    fix unit test

--- test/PHPUnit/class/ModuleTest.php
+++ test/PHPUnit/class/ModuleTest.php
@@ -5,7 +5,7 @@ class ModuleTest extends S2lowTestCase
     public function testSecurityHole()
     {
         $this->expectException(Exception::class);
-        $this->expectExceptionMessage("invalid input syntax for integer");
+        $this->expectExceptionMessage("SQLSTATE[22P02]");
         $module = new Module("0' UNION SELECT 'you','have been','hacked','0");
         $module->init();
     }
