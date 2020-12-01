37c04df476004bfd543599e135ee820445d8d893
Eric Pommateau <eric.pommateau@libriciel.coop>
2020-12-01 14:16:49 +0100

    Après test sur la prod

--- script/bug/delete-empty-folder.php
+++ script/bug/delete-empty-folder.php
@@ -2,24 +2,39 @@
 
 use Symfony\Component\Finder\Finder;
 
-require_once __DIR__."/../init/init.php";
+require_once __DIR__."/../../init/init.php";
 
-$is_empty = function (\SplFileInfo $dirname)  {
-    return count(scandir($dirname)) == 2;
-};
+$i=0;
 
-$finder = new Finder();
-$finder
-    ->in(ACTES_FILES_UPLOAD_ROOT)
-    ->directories()
-    ->filter(
-        function (\SplFileInfo $dirname)  {
-            return count(scandir($dirname)) == 2;
+while(true) {
+    $finder = new Finder();
+    $finder
+        ->in(ACTES_FILES_UPLOAD_ROOT)
+        ->directories()
+        ->filter(
+            function (\SplFileInfo $dirname) {
+                return count(scandir($dirname)) == 2;
+            }
+        );
+    if (!$finder->hasResults()) {
+        exit;
+    }
+    $all = [];
+    $nb_file=0;
+
+    /*
+     * On ne peux pas parcourir l'itérateur et supprimer un repertoire en même temps...
+     * Du coup, on prends les 100 premiers truc et on recommence
+     */
+
+    foreach ($finder as $directory) {
+        $all[] = $directory->getRealPath();
+        if (++$nb_file>=100){
+            break;
         }
-    )
-;
-
-foreach(iterator_to_array($finder) as $directory){
-    echo "remove $directory\n";
-    //rmdir($directory->getRealPath());
+    }
+    foreach($all as $rep){
+        echo ++$i . ". remove $rep\n";
+        rmdir($rep);
+    }
 }
