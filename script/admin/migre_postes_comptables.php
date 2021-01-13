1a618a370609a42a940e16177b4c351f5475f41d
Julien LE GALL <julien.legall@libriciel.coop>
2021-01-13 18:04:27 +0100

    refacto

--- script/admin/migre_postes_comptables.php
+++ script/admin/migre_postes_comptables.php
@@ -1,7 +1,26 @@
 <?php
 
+//CONSTANTES------------------------------------------------------------------------------------------------------------
+const CORRESPONDANCEPOSTECOMPTABLEFTP = [
+    "SL1V" => "VHPCE11",
+    "SL2V" => "VHPCE21",
+    "SL3V" => "VHPCE31",
+    "SL5V" => "VHPCE51",
+    "SL1M" => "MHPCE11",
+    "SL2M" => "MHPCE21",
+    "SL3M" => "MHPCE31",
+    "SL4M" => "MHPCE41",
+    "SL5M" => "MHPCE51"
+];
+
+const COL_DATE = 2;
+const COL_SIREN = 9;
+const COL_SL_SOURCE = 3;
+const COL_SL_CIBLE = 11;
+const COL_CHT_SL = 16;
 require_once( __DIR__."/../../init/init.php");
 
+//FONCTIONS-------------------------------------------------------------------------------------------------------------
 /**
  * @param $nameFile
  * @param $date
@@ -9,70 +28,62 @@ require_once( __DIR__."/../../init/init.php");
  */
 function extractDataFromFile($nameFile, $date): array
 {
-    // STRUCTURE DU FICHIER
-    $colDate=2;
-    $colSiren=9;
-    $colLibelle=5;
-    $colSlSource=3;
-    $colSlCible=11;
-    $colChangementSl=16;
-
-    $correspondancePosteComptableFTP = [
-        "SL1V"=>"VHPCE11",
-        "SL2V"=>"VHPCE21",
-        "SL3V"=>"VHPCE31",
-        "SL5V"=>"VHPCE51",
-        "SL1M"=>"MHPCE11",
-        "SL2M"=>"MHPCE21",
-        "SL3M"=>"MHPCE31",
-        "SL4M"=>"MHPCE41",
-        "SL5M"=>"MHPCE51"
-    ];
-
     $collectivitesATraiter = [];
     if (($handle = fopen($nameFile, "r")) !== FALSE) {
         while (($data = fgetcsv($handle, 1000, ";")) !== FALSE) {
-            if ($data[$colDate] === $date) {
-                // ECRIRE LA PARTIE GENERIQUE DU MESSAGE
-                $message = $data[$colSiren] . " : " . $data[$colSlSource] . "=>" . $data[$colSlCible];
-
-                // TESTER QUE LES FTPS SOURCES ET CIBLE EXISTENT BIEN
-                if (
-                    !isset($correspondancePosteComptableFTP[$data[$colSlSource]])
-                    ||
-                    !isset($correspondancePosteComptableFTP[$data[$colSlCible]])
-                ) {
-                    $messageErreur = "SL inconnu";
-                    echo $message . " : KO : " . $messageErreur . "\n";
-                    continue;
+            try {
+                $collectivitesATraiter = traiteLigne($data, $date, $collectivitesATraiter);
+            }
+            catch (Exception $e){
+                echo $e->getMessage() . "\n";
+                if(is_a($e,DomainException::class)){
+                    break;
                 }
-
-                // TESTER SI SL EST MODIFIE
-                if ($data[$colChangementSl] != "OUI") {
-                    //$messageErreur = "Sl non modifié";
-                    //echo $message . " : KO : " . $messageErreur . "\n";
-                    continue;
-                }
-
-                //VERIFIER QUE CE N'EST PAS UN DOUBLON
-                if (in_array($data[$colSiren], array_keys($collectivitesATraiter))) {
-                    if (
-                        $collectivitesATraiter[$data[$colSiren]]["SlSource"] != $correspondancePosteComptableFTP[$data[$colSlSource]]
-                        ||
-                        $collectivitesATraiter[$data[$colSiren]]["SlCible"] != $correspondancePosteComptableFTP[$data[$colSlCible]]
-                    ) {
-                        $messageErreur = "PB : fichier incohérent";
-                        echo $message . " : KO : " . $messageErreur . "\n";
-                        break;
-                    }
-                    continue;
-                }
-                $collectivitesATraiter[$data[$colSiren]]["SlSource"] = $correspondancePosteComptableFTP[$data[$colSlSource]];
-                $collectivitesATraiter[$data[$colSiren]]["SlCible"] = $correspondancePosteComptableFTP[$data[$colSlCible]];
+                continue;
             }
         }
         fclose($handle);
     }
+    return $collectivitesATraiter;
+}
+
+/**
+ * @param array $data
+ * @param $date
+ * @param array $collectivitesATraiter
+ * @return array
+ * @throws Exception
+ */
+function traiteLigne(array $data, $date, array $collectivitesATraiter): array
+{
+    if ($data[COL_DATE] != $date || $data[COL_CHT_SL] != "OUI") {
+        return $collectivitesATraiter;
+    }
+
+    $message = $data[COL_SIREN] . " : " . $data[COL_SL_SOURCE] . "=>" . $data[COL_SL_CIBLE];
+
+    if (
+        !isset(CORRESPONDANCEPOSTECOMPTABLEFTP[$data[COL_SL_SOURCE]])
+        ||
+        !isset(CORRESPONDANCEPOSTECOMPTABLEFTP[$data[COL_SL_CIBLE]])
+    ) {
+        $messageErreur = "SL inconnu";
+        throw new Exception($message . " : KO : " . $messageErreur);
+    }
+
+    if (in_array($data[COL_SIREN], array_keys($collectivitesATraiter))) {
+        if (
+            $collectivitesATraiter[$data[COL_SIREN]]["SlSource"] != CORRESPONDANCEPOSTECOMPTABLEFTP[$data[COL_SL_SOURCE]]
+            ||
+            $collectivitesATraiter[$data[COL_SIREN]]["SlCible"] != CORRESPONDANCEPOSTECOMPTABLEFTP[$data[COL_SL_CIBLE]]
+        ) {
+            $messageErreur = "PB : fichier incohérent";
+            throw new DomainException($message . " : KO : " . $messageErreur);
+        }
+        return $collectivitesATraiter;
+    }
+    $collectivitesATraiter[$data[COL_SIREN]]["SlSource"] = CORRESPONDANCEPOSTECOMPTABLEFTP[$data[COL_SL_SOURCE]];
+    $collectivitesATraiter[$data[COL_SIREN]]["SlCible"] = CORRESPONDANCEPOSTECOMPTABLEFTP[$data[COL_SL_CIBLE]];
     return $collectivitesATraiter;
 }
 
@@ -103,13 +114,13 @@ function getAuthority(object $sqlQuery, int $siren, string $message): array
  */
 function checkSlSource($helios_ftp_dest, $helios_ftp_dest_source, string $message): void
 {
-// TESTER QUE LE REPERTOIRE D'ORIGINE EST CORRECT
     if ($helios_ftp_dest != $helios_ftp_dest_source) {
         $messageErreur = "Le ftp_dest actuel " . $helios_ftp_dest . " ne correspond pas à celui spécifié " . $helios_ftp_dest_source;
         throw new Exception($message . " : KO : " . $messageErreur);
     }
 }
 
+//PROGRAMME-------------------------------------------------------------------------------------------------------------
 // TRAITEMENT DES PARAMETRES
 if(!in_array($argc,[3,4])){
     var_dump($argc);
@@ -136,15 +147,13 @@ $collectivitesATraiter = extractDataFromFile($nameFile, $date);
 foreach ($collectivitesATraiter as $siren=> $collectivite){
     try {
         $message = $siren . " : " . $collectivite["SlSource"] . "=>" . $collectivite["SlCible"];
-        // RECUPERER LA COLLECTIVITE PAR SON SIREN
 
         $infoAuthority = getAuthority($sqlQuery, $siren, $message);
         checkSlSource($infoAuthority["helios_ftp_dest"], $collectivite["SlSource"], $message);
 
-        // TOUT EST OK, ON EXPLICITE L'ACTION A FAIRE
         $action = $infoAuthority["name"] . " ( " . $infoAuthority["id"] . " ) " . $infoAuthority["helios_ftp_dest"] . "=>" . $collectivite["SlCible"];
         echo $message . " : OK : " . $action . "\n";
-        // ON EXECUTE SI NECESSAIRE
+
         if ($execute) {
             $sqlQuery->query(
                 "UPDATE authorities SET helios_ftp_dest=? WHERE id=?",
