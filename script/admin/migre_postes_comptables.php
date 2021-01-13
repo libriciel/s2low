3d3a9c66b56353d584a011d69e4972aafb73dd9b
Julien LE GALL <julien.legall@libriciel.coop>
2021-01-13 14:31:48 +0100

    refacto

--- script/admin/migre_postes_comptables.php
+++ script/admin/migre_postes_comptables.php
@@ -14,6 +14,15 @@ $correspondancePosteComptableFTP = [
     "SL5M"=>"MHPCE51"
     ];
 
+// STRUCTURE DU FICHIER
+$colDate=2;
+$colSiren=9;
+$colLibelle=5;
+$colSlOrigine=3;
+$colSlCible=11;
+$colChangementSl=16;
+
+// TRAITEMENT DES PARAMETRES
 if(!in_array($argc,[3,4])){
     var_dump($argc);
     echo "Usage : ".$argv[0]." nomFichier date [confirmExecution]\n";
@@ -34,20 +43,13 @@ if(isset($argv[3])&&$argv[3]==="execute"){
     $execute = true;
 }
 
-$colDate=2;
-$colSiren=9;
-$colLibelle=5;
-$colSlOrigine=3;
-$colSlCible=11;
-$colChangementSl=16;
-
 if (($handle = fopen($nameFile, "r")) !== FALSE) {
     while (($data = fgetcsv($handle, 1000, ";")) !== FALSE) {
         if($data[$colDate] === $date){
-            //ECRIRE LA PARTIE GENERIQUE DU MESSAGE
+            // ECRIRE LA PARTIE GENERIQUE DU MESSAGE
             $message = $data[$colLibelle]." ( ".$data[$colSiren]." ) : ".$data[$colSlOrigine]."=>".$data[$colSlCible];
 
-            //TESTER QUE LES FTPS SOURCES ET CIBLE EXISTENT BIEN
+            // TESTER QUE LES FTPS SOURCES ET CIBLE EXISTENT BIEN
             if(
                 !isset($correspondancePosteComptableFTP[$data[$colSlOrigine]])
                 ||
@@ -58,32 +60,33 @@ if (($handle = fopen($nameFile, "r")) !== FALSE) {
               continue;
             }
 
-            //TESTER SI SL EST MODIFIE
+            // TESTER SI SL EST MODIFIE
             if($data[$colChangementSl]!="OUI"){
                 $messageErreur = "Sl non modifié";
                 echo $message." : KO : ".$messageErreur."\n";
                 continue;
             }
 
-            //RECUPERER LA COLLECTIVITE PAR SON SIREN
+            // RECUPERER LA COLLECTIVITE PAR SON SIREN
             $infoAuthority= $sqlQuery->queryOne("SELECT id,name,helios_ftp_dest FROM authorities where siren=?",(int) $data[$colSiren]);
 
-            //TESTER QUE LA COLLECTIVITE EXISTE BIEN
+            // TESTER QUE LA COLLECTIVITE EXISTE BIEN
             if(empty($infoAuthority)){
                 $messageErreur = "Collectivité inconnue";
                 echo $message." : KO : ".$messageErreur."\n";
                 continue;
             }
 
-            //TESTER QUE LE REPERTOIRE D'ORIGINE EST CORRECT
+            // TESTER QUE LE REPERTOIRE D'ORIGINE EST CORRECT
             if($infoAuthority["helios_ftp_dest"] != $correspondancePosteComptableFTP[$data[$colSlOrigine]]) {
                 $messageErreur = "Le ftp_dest actuel ".$infoAuthority["helios_ftp_dest"]." ne correspond pas à celui spécifié ".$correspondancePosteComptableFTP[$data[$colSlOrigine]];
                 echo $message . " : KO : " . $messageErreur."\n";
                 continue;
             }
+            // TOUT EST OK, ON EXPLICITE L'ACTION A FAIRE
             $action = $infoAuthority["name"]." ( ".$infoAuthority["id"]." ) ".$infoAuthority["helios_ftp_dest"]."=>".$correspondancePosteComptableFTP[$data[$colSlCible]];
             echo $message." : OK : ".$action."\n";
-            // TOUT EST OK, ON EXECUTE SI NECESSAIRE
+            // ON EXECUTE SI NECESSAIRE
             if($execute){
                 $sqlQuery->query(
                         "UPDATE authorities SET helios_ftp_dest=? WHERE id=?",
