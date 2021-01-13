b7c8b55431240ed36b16dfcb5e6b21ed1c65aad6
Julien LE GALL <julien.legall@libriciel.coop>
2021-01-13 11:11:25 +0100

    passage de paramètres en ligne de commande

--- script/admin/migre_postes_comptables.php
+++ script/admin/migre_postes_comptables.php
@@ -14,14 +14,25 @@ $correspondancePosteComptableFTP = [
     "SL5M"=>"MHPCE51"
     ];
 
-$nameFile = $argv[1];
-$date = $argv[2];
-$execute = true; // TODO Récupérer depuis la ligne de commande
+if(!in_array($argc,[3,4])){
+    var_dump($argc);
+    echo "Usage : ".$argv[0]." nomFichier date [confirmExecution]\n";
+    echo "confirmExecution (optionnel) les modifs en BDD sont réalisée ssi ce paramètre vaut execute\n ";
+    return -1;
+}
 
-// Options :
-//    $1 : nom du fichier
-//    $2 : date
-//    $3 : dry run ou pas ( à rajouter)
+$nameFile = $argv[1];
+
+if(!is_file($nameFile)){
+    echo "$nameFile doit être un nom de fichier\n";
+    return -2;
+}
+$date = $argv[2];
+
+$execute = false;
+if(isset($argv[3])&&$argv[3]==="execute"){
+    $execute = true;
+}
 
 $colDate=2;
 $colSiren=9;
@@ -30,7 +41,6 @@ $colSlOrigine=3;
 $colSlCible=11;
 $colChangementSl=16;
 
-$row = 1;
 if (($handle = fopen($nameFile, "r")) !== FALSE) {
     while (($data = fgetcsv($handle, 1000, ";")) !== FALSE) {
         if($data[$colDate] === $date){
@@ -44,14 +54,14 @@ if (($handle = fopen($nameFile, "r")) !== FALSE) {
                 !isset($correspondancePosteComptableFTP[$data[$colSlCible]])
             ){
               $messageErreur = "SL inconnu";
-              echo $message." : ".$messageErreur."\n";
+              echo $message." : KO : ".$messageErreur."\n";
               continue;
             }
 
             //TESTER SI SL EST MODIFIE
             if($data[$colChangementSl]!="OUI"){
                 $messageErreur = "Sl non modifié";
-                echo $message." : ".$messageErreur."\n";
+                echo $message." : KO : ".$messageErreur."\n";
                 continue;
             }
 
@@ -61,37 +71,27 @@ if (($handle = fopen($nameFile, "r")) !== FALSE) {
             //TESTER QUE LA COLLECTIVITE EXISTE BIEN
             if(empty($infoAuthority)){
                 $messageErreur = "Collectivité inconnue";
-                echo $message." : ".$messageErreur."\n";
+                echo $message." : KO : ".$messageErreur."\n";
                 continue;
             }
 
             //TESTER QUE LE REPERTOIRE D'ORIGINE EST CORRECT
             if($infoAuthority["helios_ftp_dest"] != $correspondancePosteComptableFTP[$data[$colSlOrigine]]) {
                 $messageErreur = "Le ftp_dest actuel ".$infoAuthority["helios_ftp_dest"]." ne correspond pas à celui spécifié ".$correspondancePosteComptableFTP[$data[$colSlOrigine]];
-                echo $message . " : " . $messageErreur."\n";
+                echo $message . " : KO : " . $messageErreur."\n";
                 continue;
             }
-             
+            $action = $infoAuthority["name"]." ( ".$infoAuthority["id"]." ) ".$infoAuthority["helios_ftp_dest"]."=>".$correspondancePosteComptableFTP[$data[$colSlCible]];
+            echo $message." : OK : ".$action."\n";
             // TOUT EST OK, ON EXECUTE SI NECESSAIRE
             if($execute){
-                    $action = $infoAuthority["name"]." ( ".$infoAuthority["id"]." ) ".$infoAuthority["helios_ftp_dest"]."=>".$correspondancePosteComptableFTP[$data[$colSlCible]];
-                    echo $message." : ".$action."\n";
-                    $a=$sqlQuery->query(
+                $sqlQuery->query(
                         "UPDATE authorities SET helios_ftp_dest=? WHERE id=?",
                         $correspondancePosteComptableFTP[$data[$colSlCible]],
                         (int) $infoAuthority["id"]
-                    );
+                );
             }
         }
     }
     fclose($handle);
 }
-//Charger un fichier csv
-//Pour chaque ligne
-//-> vérifier que la date est ok (comparer col 2 à la date)
-//-> prendre le SIRET (col 8 )
-//-> rechercher l'id de l'autorité correspondante
-//      $sql = "SELECT id FROM authorities where dia_siret=?";
-//      return $this->queryOne($sql,$siret);
-//-> modifier le poste comptable
-//      helios_ftp_dest dans la table authorities
