832c9632375ce3bfd5e57d26b6ca01558473daa9
Julien LE GALL <julien.legall@libriciel.coop>
2021-03-10 22:02:38 +0100

    correction configuration msmtp

--- docker-resources/generate-msmtp.conf.sh
+++ docker-resources/generate-msmtp.conf.sh
@@ -11,6 +11,5 @@ port            ${SMTP_PORT:-25}
 from		        default@${SITE_HOST_NAME-default.fr}
 tls_trust_file  /etc/ssl/certs/ca-certificates.crt
 logfile         /var/log/msmtp.log
-FromLineOverride=YES
 
 EOF
