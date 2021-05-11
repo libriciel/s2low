deec35ac8f35a4684faf444143f6fa8534e69e81
Julien LE GALL <julien.legall@libriciel.coop>
2021-05-11 15:40:35 +0200

    Refactor Tests

--- test/PHPUnit/class/VerifyPadesSignatureTest.php
+++ test/PHPUnit/class/VerifyPadesSignatureTest.php
@@ -6,7 +6,6 @@
 
 class VerifyPadesSignatureTest extends S2lowTestCase
 {
-
     /**
      * @var \PHPUnit\Framework\MockObject\MockObject|\VerifyPemCertificate
      */
@@ -14,8 +13,20 @@ class VerifyPadesSignatureTest extends S2lowTestCase
     /** @var \VerifyPadesSignature  */
     private $verifyPadesSignature;
 
-    private function extractSignatureFromPadesValidJson($string){
-        return json_decode($string)->signatures[0];
+    private function getSignature(
+        bool $valid=true,
+        string $signingCert="certificat",
+        string $signatureDate="1502268600000"
+    ){
+        $signature = $this->getMockBuilder(stdClass::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $signature->valid = $valid;
+        $signature->signingCert = $signingCert;
+        $signature->signatureDate =$signatureDate;
+
+        return $signature;
     }
 
     protected function setUp() :void {
@@ -34,88 +45,74 @@ class VerifyPadesSignatureTest extends S2lowTestCase
             $verifyPemCertificateFactory
         );
     }
+    // checkNecessaryFields
 
     /**
-     * @throws Exception
+     * @dataProvider missingNecessaryFieldsProvider
      */
-    public function testValidateSigned(){
-        $rgs_validca_path = __DIR__ . "/../lib/fixtures/validca/";
-        $verifyPadesSignature = new VerifyPadesSignature(
-            $rgs_validca_path,
-            new VerifyPemCertificateFactory()
-        );
 
-        $signature = $this->extractSignatureFromPadesValidJson(
-            file_get_contents(__DIR__."/fixtures/signature-pades/return-courrier-signe.json")
-        );
+    public function testMissingNecessaryFields($signature, $exceptionMessage){
 
-        $exceptionThrown = false;
-        try{
-            $verifyPadesSignature->validateSignature($signature);
-        } catch (Exception $exception){
-            $exceptionThrown = true;
-        }
-        $this->assertFalse($exceptionThrown);
+        $this->expectException(Exception::class);
+        $this->expectExceptionMessage($exceptionMessage);
+        $this->verifyPadesSignature->validateSignature($signature);
+    }
+
+    public function missingNecessaryFieldsProvider(){
+        return [
+            [
+                $this->getSignature(false,"",""),
+                "Au moins une signature n'est pas valide"
+            ],
+            [
+                $this->getSignature(true,"",""),
+                "Impossible de récupérer le certificat de signature"
+            ],
+            [
+                $this->getSignature(true,"certificat",""),
+                "Impossible de determiner la date de la signature"
+            ]
+        ];
+
+    }
+
+    // checkCertificateWasValidAtSignatureTime
+
+    /**
+     * @doesNotPerformAssertions
+     */
+    public function testCertificateWasValidOnSignature(){
+        $this->verifyPemCertificate
+            ->method("parsePemCertificate")
+            ->willReturn([
+                "validFrom_time_t"=>1502268599,
+                "validTo_time_t"=>1502268601
+            ]);
+
+        $this->verifyPadesSignature->validateSignature($this->getSignature());
     }
 
     /**
      * @throws Exception
      */
-    public function testNotValidateSigned(){
-        $rgs_validca_path = __DIR__ . "/../lib/fixtures/validca/";
-        $verifyPadesSignature = new VerifyPadesSignature(
-            $rgs_validca_path,
-            new VerifyPemCertificateFactory()
-        );
+    public function testCertificateWasInvalidOnSignature(){
+        $this->verifyPemCertificate
+            ->method("parsePemCertificate")
+            ->willReturn([
+                "validFrom_time_t"=>0,
+                "validTo_time_t"=>0
+            ]);
 
-        $signature = $this->extractSignatureFromPadesValidJson(
-            file_get_contents(__DIR__."/fixtures/signature-pades/return-courrier-alter.json")
-        );
-
-        $exceptionThrown = false;
-        try{
-            $verifyPadesSignature->validateSignature($signature);
-        } catch (Exception $exception){
-            $exceptionThrown = true;
-            $this->assertEquals(
-                "Au moins une signature n'est pas valide",
-                $exception->getMessage()
-            );
-        }
-
-        $this->assertTrue($exceptionThrown);
+        $this->expectException(Exception::class);
+        $this->expectExceptionMessage("La date de la signature 1502268600000 n'entre pas dans la date de validité du certitficat 0 - 0");
+        $this->verifyPadesSignature->validateSignature($this->getSignature());
     }
 
-    /**
-     * @throws Exception
-     */
-    public function testValidateSignedNoCertificatCheking(){
-        $rgs_validca_path = __DIR__ . "/../lib/fixtures/validca/";
-        $verifyPadesSignature = new VerifyPadesSignature(
-            $rgs_validca_path,
-            new VerifyPemCertificateFactory()
-        );
+    // Check checkCertificateWithoutCheckingCertificateChain est
+    //  - appelé par validateSignature
+    //  - pas appelé par validateSignatureWithoutCertificateChecking
 
-        $signature = $this->extractSignatureFromPadesValidJson(
-            file_get_contents(__DIR__."/fixtures/signature-pades/return-courrier-signe.json")
-        );
-        $exceptionThrown = false;
-        try{
-        $verifyPadesSignature->validateSignatureWithoutCertificateChecking($signature);
-        } catch (Exception $exception){
-            $exceptionThrown = true;
-        }
-        $this->assertFalse($exceptionThrown);
-    }
-
-    /**
-     * @throws Exception
-     */
-    public function testValidateSignedBadCertificate(){
-        $signature = $this->extractSignatureFromPadesValidJson(
-            file_get_contents(__DIR__."/fixtures/signature-pades/return-courrier-signe.json")
-        );
-
+    public function testcheckCertificateWithoutCheckingCertificateChainIsCalled(){
         $this->verifyPemCertificate
             ->method("parsePemCertificate")
             ->willReturn([
@@ -124,110 +121,43 @@ class VerifyPadesSignatureTest extends S2lowTestCase
             ]);
 
         $this->verifyPemCertificate
-            ->method('checkCertificateWithoutCheckingCertificateChain')
-            ->willThrowException(new Exception("problème"));
+            ->expects($this->once())
+            ->method("checkCertificateWithoutCheckingCertificateChain");
 
-        $exceptionThrown = false;
-        try{
-            $this->verifyPadesSignature->validateSignature($signature);
-        } catch (Exception $exception){
-            $exceptionThrown = true;
-            $this->assertEquals(
-                "problème",
-                $exception->getMessage()
-            );
-        }
-        $this->assertTrue($exceptionThrown);
+        $this->verifyPadesSignature->validateSignature($this->getSignature());
     }
 
-    /**
-     * @throws Exception
-     */
-    public function testValidateSignedOutdatedCertificate(){
-        $signature = $this->extractSignatureFromPadesValidJson(
-            file_get_contents(__DIR__."/fixtures/signature-pades/return-courrier-signe.json")
-        );
-
+    public function testcheckCertificateWithoutCheckingCertificateChainIsNotCalled(){
         $this->verifyPemCertificate
             ->method("parsePemCertificate")
             ->willReturn([
-                "validFrom_time_t"=>0,
-                "validTo_time_t"=>0
+                "validFrom_time_t"=>1502268599,
+                "validTo_time_t"=>1502268601
             ]);
 
-        $exceptionThrown = false;
-        try{
-            $this->verifyPadesSignature->validateSignature($signature);
-        } catch (Exception $exception){
-            $exceptionThrown = true;
-            $this->assertEquals(
-                "La date de la signature 1502268600000 n'entre pas dans la date de validité du certitficat 0 - 0",
-                $exception->getMessage()
-            );
-        }
-        $this->assertTrue($exceptionThrown);
+        $this->verifyPemCertificate
+            ->expects($this->never())
+            ->method("checkCertificateWithoutCheckingCertificateChain");
+
+        $this->verifyPadesSignature->validateSignatureWithoutCertificateChecking($this->getSignature());
     }
 
-    public function testMissingNecessaryFields(){
-        $signature = $this->getMockBuilder(stdClass::class)
-            ->disableOriginalConstructor()
-            ->getMock();
+    // Test que l'exception lancée par checkCertificateWithoutCheckingCertificateChain passe le cas échéant
 
-        $signature->valid = false;
+    public function testcheckCertificateWithoutCheckingCertificateChainExceptionGoesThrough(){
+        $this->verifyPemCertificate
+            ->method("parsePemCertificate")
+            ->willReturn([
+                "validFrom_time_t"=>1502268599,
+                "validTo_time_t"=>1502268601
+            ]);
 
-        $exceptionThrown = false;
-        try{
-            $this->verifyPadesSignature->validateSignature($signature);
-        } catch (Exception $exception){
-            $exceptionThrown = true;
-            $this->assertEquals(
-                "Au moins une signature n'est pas valide",
-                $exception->getMessage()
-            );
-        }
-        $this->assertTrue($exceptionThrown);
-    }
+        $this->verifyPemCertificate
+            ->method("checkCertificateWithoutCheckingCertificateChain")
+            ->willThrowException(new Exception("Exception de test LahgnjCM"));
 
-    public function testMissingNecessaryFields2(){
-        $signature = $this->getMockBuilder(stdClass::class)
-            ->disableOriginalConstructor()
-            ->getMock();
-
-        $signature->valid = true;
-        $signature->signingCert = "";
-
-        $exceptionThrown = false;
-        try{
-            $this->verifyPadesSignature->validateSignature($signature);
-        } catch (Exception $exception){
-            $exceptionThrown = true;
-            $this->assertEquals(
-                "Impossible de récupérer le certificat de signature",
-                $exception->getMessage()
-            );
-        }
-        $this->assertTrue($exceptionThrown);
-    }
-
-    public function testMissingNecessaryFields3(){
-        $signature = $this->getMockBuilder(stdClass::class)
-            ->disableOriginalConstructor()
-            ->getMock();
-
-        $signature->valid = true;
-        $signature->signingCert = "certificat";
-        $signature->signatureDate ="";
-
-        $exceptionThrown = false;
-        try{
-            $this->verifyPadesSignature->validateSignature($signature);
-        } catch (Exception $exception){
-            $exceptionThrown = true;
-            $this->assertEquals(
-                "Impossible de determiner la date de la signature",
-                $exception->getMessage()
-            );
-        }
-        $this->assertTrue($exceptionThrown);
+        $this->expectException(Exception::class);
+        $this->expectExceptionMessage("Exception de test LahgnjCM");
+        $this->verifyPadesSignature->validateSignature($this->getSignature());
     }
 }
