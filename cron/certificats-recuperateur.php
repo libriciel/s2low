a1924e5819d8e24cb2eb39ee730a271435880a7f
Julien LE GALL <julien.legall@libriciel.coop>
2021-11-09 11:41:05 +0100

    Corrections mineures

--- cron/certificats-recuperateur.php
+++ cron/certificats-recuperateur.php
@@ -3,5 +3,5 @@ require_once( __DIR__ . "/../init/init.php");
 
 
 $workerScript = $objectInstancier->get(WorkerScript::class);
-$workerScript->setMinExecutionTimeInSeconds(300);
+$workerScript->setMinExecutionTimeInSeconds(600);
 $workerScript->scriptByClassName(AcCertificatesRetrieverWorker::class,true,true);
