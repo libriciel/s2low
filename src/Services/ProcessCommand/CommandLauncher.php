0d55436dc6a814b3979a8913ef367570027bab85
Julien LE GALL <julien.legall@libriciel.coop>
2022-05-24 09:24:48 +0200

    correction retour EP

--- src/Services/ProcessCommand/CommandLauncher.php
+++ src/Services/ProcessCommand/CommandLauncher.php
@@ -6,7 +6,7 @@ use Symfony\Component\Process\Process;
 
 class CommandLauncher
 {
-   public function launch($commmand, ICommandOutputTranslator $outputTranslator): string
+   public function launch(string $commmand, ICommandOutputTranslator $outputTranslator): string
    {
        $process = new Process($commmand);
        try{
