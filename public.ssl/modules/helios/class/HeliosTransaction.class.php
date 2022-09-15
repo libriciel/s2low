a018277841a2eff701c4fd4f438ff5c61d9ac77b
Julien LE GALL <julien.legall@libriciel.coop>
2022-09-15 15:14:42 +0200

    Corrections

--- public.ssl/modules/helios/class/HeliosTransaction.class.php
+++ public.ssl/modules/helios/class/HeliosTransaction.class.php
@@ -353,9 +353,9 @@ class HeliosTransaction extends DataObject
     public function getCurrentStatus()
     {
         if (isset($this->id) && !empty($this->id)) {
-            $sql = "SELECT status_id, message FROM helios_transactions_workflow atw WHERE date = ( SELECT MAX(date) FROM helios_transactions_workflow atw2 WHERE atw2.transaction_id = atw.transaction_id) AND transaction_id = " . $this->id . " ORDER BY atw.id DESC LIMIT 1";
+            $sql = "SELECT status_id, message FROM helios_transactions_workflow atw WHERE date = ( SELECT MAX(date) FROM helios_transactions_workflow atw2 WHERE atw2.transaction_id = atw.transaction_id) AND transaction_id = ? ORDER BY atw.id DESC LIMIT 1";
 
-            $result = $this->db->select($sql);
+            $result = $this->db->select($sql,[ $this->id ]);
 
             if (!$result->isError()) {
                 $row = $result->get_next_row();
