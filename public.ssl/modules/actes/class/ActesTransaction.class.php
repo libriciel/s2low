67c96d478245f2358989775f307f00f418432ef5
Julien LE GALL <julien.legall@libriciel.coop>
2022-09-15 15:14:42 +0200

    Corrections

--- public.ssl/modules/actes/class/ActesTransaction.class.php
+++ public.ssl/modules/actes/class/ActesTransaction.class.php
@@ -382,9 +382,9 @@ class ActesTransaction extends DataObject {
    */
   public function getCurrentStatus() {
     if (isset ($this->id) && !empty ($this->id)) {
-      $sql = "SELECT status_id FROM actes_transactions_workflow atw WHERE date = ( SELECT MAX(date) FROM actes_transactions_workflow atw2 WHERE atw2.transaction_id = atw.transaction_id) AND transaction_id = " . $this->id . " ORDER BY atw.id DESC LIMIT 1";
+      $sql = "SELECT status_id FROM actes_transactions_workflow atw WHERE date = ( SELECT MAX(date) FROM actes_transactions_workflow atw2 WHERE atw2.transaction_id = atw.transaction_id) AND transaction_id = ? ORDER BY atw.id DESC LIMIT 1";
 
-      $result = $this->db->select($sql);
+      $result = $this->db->select($sql,[$this->id]);
 
       if (!$result->isError()) {
         $row = $result->get_next_row();
