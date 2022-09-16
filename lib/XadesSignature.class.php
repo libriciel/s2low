92f44187e137e5ea481d8ed4bb23868d94f3ab5a
Julien LE GALL <julien.legall@libriciel.coop>
2022-09-16 18:13:32 +0200

    Seconde version

--- lib/XadesSignature.class.php
+++ lib/XadesSignature.class.php
@@ -216,17 +216,7 @@ class XadesSignature {
 			if (!$id) {
                 throw new Exception("Impossible d'extraire l'attribut Id de l'entité \<Signature\>");
 			}
-            $signature_node_URI = $this->getSignature_node_URI($signatureNode);
-            if(empty($signature_node_URI)){
-                throw new Exception("Impossible d'extraire l'URI de l'entité \<signature\> d'Id $id");
-            }
-            $xpath = "//*[@Id='$signature_node_URI']";
-			$element = $xml->xpath($xpath);
-			if (count($element) != 1) {
-                throw new Exception("Impossible d'extraire l'entité \<SignedProperties\> d'Id $signature_node_URI");
-			}
-			$element = $element[0];
-			$name = $element->getName();
+            $signedElementRootName = $this->getSignedElementRootName($signatureNode, $xml);
 
             $signingTime = $this->xadesSignatureParser->extractXadesSigningTime($xml,strval($id));
 
@@ -256,7 +246,7 @@ class XadesSignature {
                 unlink($file);
             }
 
-            if (!$this->verifyIntern($xml_file_signed, $name, $id, $signingTime)) {
+            if (!$this->verifyIntern($xml_file_signed, $signedElementRootName, $id, $signingTime)) {
                 throw new Exception("Impossible d'affirmer que la signature correspond au fichier");
             }
 		}
@@ -303,6 +293,33 @@ class XadesSignature {
             }
         }
         return "";
+    }
+
+    /**
+     * @param $signatureNode
+     * @param $id
+     * @param $xml
+     * @return mixed
+     * @throws \Exception
+     */
+    private function getSignedElementRootName($signatureNode, $xml)
+    {
+        $signature_node_URI =
+            ltrim(
+                strval($signatureNode->children(self::NS_DS_URI)->SignedInfo->Reference->attributes()->URI),
+                "#");
+
+        if (empty($signature_node_URI)) {           // Si l'URI n'est pas spécifiée dans le premier noeud référence,
+            return $xml->getName();                 // on prend en compte l'entité racine du XML
+        }
+        $xpath = "//*[@Id='$signature_node_URI']";
+        $element = $xml->xpath($xpath);
+        if (count($element) != 1) {
+            throw new Exception("Impossible d'extraire l'entité \<SignedProperties\> d'Id $signature_node_URI");
+        }
+        $element = $element[0];
+        $name = $element->getName();
+        return $name;
     }
 }
 
