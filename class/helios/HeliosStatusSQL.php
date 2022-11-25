8b525530b1f720696b2717a7812999a4a729b08c
Julien LE GALL <julien.legall@libriciel.coop>
2022-11-25 10:15:46 +0100

    correction encodage

--- class/helios/HeliosStatusSQL.php
+++ class/helios/HeliosStatusSQL.php
@@ -26,8 +26,8 @@ class HeliosStatusSQL
         $status_libelle_list = [
             19 => "En attente de transmission au SAE",
             20 => "Erreur lors de l'envoi au SAE",
-            9 => "Envoyï¿½ au SAE",
-            10 => "Acceptï¿½ par le SAE",
+            9 => "Envoyé au SAE",
+            10 => "Accepté par le SAE",
         ];
 
         return $status_libelle_list[$status_id] ?? $status_id;
