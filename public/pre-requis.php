20ce84f83cd312e03659b74114f721a5bd6ba129
Julien LE GALL <julien.legall@libriciel.coop>
2022-12-23 12:22:47 +0100

    corriger la page des pré-requis

--- public/pre-requis.php
+++ public/pre-requis.php
@@ -2,6 +2,9 @@
 
 use S2lowLegacy\Class\HTMLLayout;
 
+require_once("../init/init.php");
+\S2lowLegacy\Class\LegacyObjectsManager::setLegacyObjectInstancier();
+
 $doc = new HTMLLayout('xhtml_prerequis.tpl.php');
 
 
