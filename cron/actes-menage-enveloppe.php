10c9fad9ee8d186b35aa7cfa3f20ca1546cb5978
Julien LE GALL <julien.legall@libriciel.coop>
2023-07-18 20:43:24 +0200

    corriger le worker actes-menage-enveloppe.php

--- cron/actes-menage-enveloppe.php
+++ cron/actes-menage-enveloppe.php
@@ -3,14 +3,16 @@
 use S2lowLegacy\Class\actes\ActesMenageEnveloppeWorker;
 use S2lowLegacy\Class\LegacyObjectsManager;
 use S2lowLegacy\Class\WorkerRunnerBuilder;
+use S2lowLegacy\Class\WorkerRunnerWithDataFromDB;
 
-require_once(__DIR__ . "/../init/init.php");
+require_once(__DIR__ . '/../init/init.php');
 /** @var WorkerRunnerBuilder $workerBuilder */
+/** @var ActesMenageEnveloppeWorker $worker */
 [$workerBuilder, $worker] = LegacyObjectsManager::getLegacyObjectInstancier()
     ->getArray([WorkerRunnerBuilder::class,ActesMenageEnveloppeWorker::class]);
 
 $workerBuilder->scriptWithLogs(
     $worker,
     true,
-    \S2lowLegacy\Class\WorkerRunnerWithDataFromDB::class
-);
+    WorkerRunnerWithDataFromDB::class
+)->work();
