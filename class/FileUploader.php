c4e903c381cc22b87400ffe846f042c6fff7ee58
Julien LE GALL <julien.legall@libriciel.coop>
2024-05-22 17:58:37 +0200

    ajout de tests unitaires

--- class/FileUploader.php
+++ class/FileUploader.php
@@ -6,29 +6,31 @@ class FileUploader
 {
     private const MAX_LINE_LENGTH = 1024;
 
-    private $forbidenExtension = array("asp","aspx","asax","asa","jsp","cer","cdx","asa","htr","php","php3","exe","cgi");
+    private array $forbidenExtension = [
+        'asp', 'aspx', 'asax', 'asa', 'jsp', 'cer', 'cdx', 'asa', 'htr', 'php', 'php3', 'exe', 'cgi'
+    ];
 
-    private $destinationDirectory;
-    private $lastError;
-    private $fileName;
-    private $fileSize;
-    private $extension;
+    private string $destinationDirectory = '';
+    private string $lastError = '';
+    private string $fileName;
+    private string $fileSize;
+    private string $extension;
 
     private $fileHandler;
 
 
-    public function setDestinationDirectory($directory)
+    public function setDestinationDirectory($directory): void
     {
         $this->destinationDirectory = $directory;
     }
 
-    public function disableForbidenExtension()
+    public function disableForbidenExtension(): void
     {
-        $this->forbidenExtension = array();
+        $this->forbidenExtension = [];
     }
 
 
-    public function verifOKAll($formFileName): bool
+    public function verifOKAll(string $formFileName): bool
     {
         if (! isset($_FILES[$formFileName]) || ! $_FILES[$formFileName]) {
             $this->lastError = "Il n'y a pas de fichier à charger sur le serveur";
@@ -49,7 +51,7 @@ class FileUploader
         return true;
     }
 
-    public function verifOK($formFileName): bool
+    public function verifOK(string $formFileName): bool
     {
         if (! isset($_FILES[$formFileName]) || ! $_FILES[$formFileName]) {
             $this->lastError = "Il n'y a pas de fichier à charger sur le serveur";
@@ -65,43 +67,45 @@ class FileUploader
         $this->fileName = $_FILES[$formFileName]['name'];
         $this->fileSize = $_FILES[$formFileName]['size'];
         if ($this->fileSize <= 0) {
-            $this->lastError = "Le fichier {$this->fileName} semble vide";
+            $this->lastError = "Le fichier $this->fileName semble vide";
             return false;
         }
         return true;
     }
 
-    public function upload($formFileName)
+    public function upload(string $formFileName): bool
     {
-
         if (! $this->verifOK($formFileName)) {
             return false;
         }
 
-        $this->extension = mb_strtolower(mb_substr($this->fileName, mb_strrpos($this->fileName, ".") + 1));
+        $this->extension = mb_strtolower(mb_substr($this->fileName, mb_strrpos($this->fileName, '.') + 1));
 
         if (in_array($this->extension, $this->forbidenExtension)) {
-            $this->lastError = "Le fichier " . $this->fileName . " contient une extension interdite";
+            $this->lastError = 'Le fichier ' . $this->fileName . ' contient une extension interdite';
             return false;
         }
 
         if (file_exists($this->destinationDirectory . $this->fileName)) {
-            $this->lastError = "Le fichier " . $this->fileName . " existe déjà sur le serveur";
+            $this->lastError = 'Le fichier ' . $this->fileName . ' existe déjà sur le serveur';
             return false;
         }
         if ($this->destinationDirectory) {
-            $res = move_uploaded_file($_FILES[$formFileName]['tmp_name'], $this->destinationDirectory . $this->fileName);
-            if ($res == false) {
-                $this->lastError = "Impossible de recopier le fichier sur le serveur ";
+            $res = move_uploaded_file(
+                $_FILES[$formFileName]['tmp_name'],
+                $this->destinationDirectory . $this->fileName
+            );
+            if (!$res) {
+                $this->lastError = 'Impossible de recopier le fichier sur le serveur ';
                 return false;
             }
         } else {
-            $this->fileHandler = fopen($_FILES[$formFileName]['tmp_name'], "r");
+            $this->fileHandler = fopen($_FILES[$formFileName]['tmp_name'], 'r');
         }
         return true;
     }
 
-    public function getLigne()
+    public function getLigne(): bool|string
     {
         if (feof($this->fileHandler)) {
             return false;
@@ -109,53 +113,38 @@ class FileUploader
         return fgets($this->fileHandler, self::MAX_LINE_LENGTH);
     }
 
-    private function setErrorMessage($code)
+    private function setErrorMessage(int $code): void
     {
-        switch ($code) {
-            case UPLOAD_ERR_INI_SIZE:
-                $message = "La taille du fichier excède la taille maximum (" . ini_get('upload_max_filesize') . ")";
-                break;
-            case UPLOAD_ERR_FORM_SIZE:
-                $message = "Le fichier dépasse la taille limite autorisée par le formulaire";
-                break;
-            case UPLOAD_ERR_PARTIAL:
-                $message = "Le fichier n'a été que partiellement reçu";
-                break;
-            case UPLOAD_ERR_NO_FILE:
-                $message = "Aucun fichier n'a été présenté";
-                break;
-            case UPLOAD_ERR_NO_TMP_DIR:
-                $message = "Erreur de configuration : le répertoire temporaire n'existe pas";
-                break;
-            case UPLOAD_ERR_CANT_WRITE:
-                $message = "Erreur de configuration : Impossible d'écrire dans le répertoire temporaire";
-                break;
-            case UPLOAD_ERR_EXTENSION:
-                $message = "Une extension PHP empeche l'upload du fichier!";
-                break;
-            default:
-                $message = "Erreur lors du chargement du fichier (code $code)";
-                break;
-        }
+        $message = match ($code) {
+            UPLOAD_ERR_INI_SIZE =>
+                'La taille du fichier excède la taille maximum (' . ini_get('upload_max_filesize') . ')',
+            UPLOAD_ERR_FORM_SIZE => 'Le fichier dépasse la taille limite autorisée par le formulaire',
+            UPLOAD_ERR_PARTIAL => "Le fichier n'a été que partiellement reçu",
+            UPLOAD_ERR_NO_FILE => "Aucun fichier n'a été présenté",
+            UPLOAD_ERR_NO_TMP_DIR => "Erreur de configuration : le répertoire temporaire n'existe pas",
+            UPLOAD_ERR_CANT_WRITE => "Erreur de configuration : Impossible d'écrire dans le répertoire temporaire",
+            UPLOAD_ERR_EXTENSION => "Une extension PHP empeche l'upload du fichier!",
+            default => "Erreur lors du chargement du fichier (code $code)",
+        };
         $this->lastError = $message;
     }
 
-    public function getFileName()
+    public function getFileName(): string
     {
         return $this->fileName;
     }
 
-    public function getFileSize()
+    public function getFileSize(): string
     {
         return $this->fileSize;
     }
 
-    public function getLastError()
+    public function getLastError(): string
     {
         return $this->lastError;
     }
 
-    public function getExtension()
+    public function getExtension(): string
     {
         return $this->extension;
     }
--- test/PHPUnit/class/FileUploaderTest.php
+++ test/PHPUnit/class/FileUploaderTest.php
@@ -4,14 +4,20 @@ declare(strict_types=1);
 
 namespace PHPUnit\class;
 
+use Exception;
 use S2lowLegacy\Class\FileUploader;
+use S2lowLegacy\Class\TmpFolder;
 use S2lowTestCase;
 
 class FileUploaderTest extends S2lowTestCase
 {
-    public function setUp(): void
+    private TmpFolder $tmpFolder;
+
+    protected function setUp(): void
     {
+        parent::setUp();
         $_FILES = [];
+        $this->tmpFolder = new TmpFolder();
     }
     public function testUploadInexistingFile(): void
     {
@@ -25,7 +31,7 @@ class FileUploaderTest extends S2lowTestCase
 
     public function testUploadOkFile(): void
     {
-        $_FILES['yep']['name'] = 'toto.txt';
+        $_FILES['yep']['name'] = 'titi.txt';
         $_FILES['yep']['tmp_name'] = __DIR__ . '/fixtures/toto.txt';
         $_FILES['yep']['error'] = UPLOAD_ERR_OK;
         $_FILES['yep']['size'] = '1024';
@@ -33,6 +39,60 @@ class FileUploaderTest extends S2lowTestCase
         $fileUploader = new FileUploader();
 
         static::assertTrue($fileUploader->upload('yep'));
+        static::assertSame('titi.txt', $fileUploader->getFileName());
+        // C'est le contenu de $_FILES['yep']['size'] = '1024', non la taille réelle du fichier ...
+        static::assertSame('1024', $fileUploader->getFileSize());
+        static::assertSame('contenuToto', $fileUploader->getLigne());
+        static::assertFalse($fileUploader->getLigne());
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function testUploadExistingFile(): void
+    {
+        $_FILES['yep']['name'] = 'toto.txt';
+        $_FILES['yep']['tmp_name'] = __DIR__ . '/fixtures/toto.txt';
+        $_FILES['yep']['error'] = UPLOAD_ERR_OK;
+        $_FILES['yep']['size'] = '1024';
+
+        $testDirectory = $this->tmpFolder->create();
+        copy(__DIR__ . '/fixtures/toto.txt', "$testDirectory/toto.txt");
+
+        $fileUploader = new FileUploader();
+        $fileUploader->setDestinationDirectory("$testDirectory/");
+
+        static::assertFalse($fileUploader->upload('yep'));
+        static::assertSame(
+            'Le fichier toto.txt existe déjà sur le serveur',
+            $fileUploader->getLastError()
+        );
+
+        $this->tmpFolder->delete($testDirectory);
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function testUploadWrongDirectory(): void
+    {
+        $_FILES['yep']['name'] = 'toto.txt';
+        $_FILES['yep']['tmp_name'] = __DIR__ . '/fixtures/toto.txt';
+        $_FILES['yep']['error'] = UPLOAD_ERR_OK;
+        $_FILES['yep']['size'] = '1024';
+
+        $testDirectory = $this->tmpFolder->create();
+
+        $fileUploader = new FileUploader();
+        $fileUploader->setDestinationDirectory("$testDirectory/nochancethisisadir");
+
+        static::assertFalse($fileUploader->upload('yep'));
+        static::assertSame(
+            'Impossible de recopier le fichier sur le serveur ',
+            $fileUploader->getLastError()
+        );
+
+        $this->tmpFolder->delete($testDirectory);
     }
 
     public function testUploadExtension(): void
@@ -85,6 +145,19 @@ class FileUploaderTest extends S2lowTestCase
             $fileUploader->getLastError()
         );
     }
+
+    public function testUploadDisabledForbiddenExtension(): void
+    {
+        $_FILES['yep']['name'] = 'toto.asp';
+        $_FILES['yep']['tmp_name'] = __DIR__ . '/fixtures/toto.txt';
+        $_FILES['yep']['error'] = UPLOAD_ERR_OK;
+        $_FILES['yep']['size'] = '1024';
+
+        $fileUploader = new FileUploader();
+        $fileUploader->disableForbidenExtension();
+
+        static::assertTrue($fileUploader->upload('yep'));
+    }
     public function testVerifOkNoFiles(): void
     {
         $_FILES = [];
@@ -120,7 +193,7 @@ class FileUploaderTest extends S2lowTestCase
 
         $fileUploader = new FileUploader();
 
-        static::assertFalse($fileUploader->verifOK("form"));
+        static::assertFalse($fileUploader->verifOK('form'));
         static::assertEquals(
             'Le fichier vide.pdf semble vide',
             $fileUploader->getLastError()
@@ -158,7 +231,7 @@ class FileUploaderTest extends S2lowTestCase
 
         $fileUploader = new FileUploader();
 
-        static::assertFalse($fileUploader->verifOKAll("form"));
+        static::assertFalse($fileUploader->verifOKAll('form'));
         static::assertEquals(
             "Aucun fichier n'a été présenté",
             $fileUploader->getLastError()
@@ -191,5 +264,34 @@ class FileUploaderTest extends S2lowTestCase
         $fileUploader = new FileUploader();
 
         static::assertTrue($fileUploader->verifOKAll('form'));
+    }
+
+    /**
+     * @dataProvider errors
+     */
+    public function testErrorMessages(int $error, string $message): void
+    {
+        $_FILES['form']['name'][] = 'toto.txt';
+        $_FILES['form']['tmp_name'][] = __DIR__ . '/fixtures/toto.txt';
+        $_FILES['form']['error'][] = $error;
+        $_FILES['form']['size'][] = '1024';
+
+        $fileUploader = new FileUploader();
+
+        static::assertFalse($fileUploader->verifOKAll('form'));
+        static::assertEquals($message, $fileUploader->getLastError());
+    }
+
+    public function errors(): iterable
+    {
+        yield [ UPLOAD_ERR_INI_SIZE,
+            'La taille du fichier excède la taille maximum (' . ini_get('upload_max_filesize') . ')'];
+        yield [ UPLOAD_ERR_FORM_SIZE, 'Le fichier dépasse la taille limite autorisée par le formulaire'];
+        yield [ UPLOAD_ERR_PARTIAL, "Le fichier n'a été que partiellement reçu"];
+        yield [ UPLOAD_ERR_NO_FILE, "Aucun fichier n'a été présenté"];
+        yield [ UPLOAD_ERR_NO_TMP_DIR, "Erreur de configuration : le répertoire temporaire n'existe pas"];
+        yield [UPLOAD_ERR_CANT_WRITE, "Erreur de configuration : Impossible d'écrire dans le répertoire temporaire"];
+        yield [UPLOAD_ERR_EXTENSION, "Une extension PHP empeche l'upload du fichier!"];
+        yield [ 256678, 'Erreur lors du chargement du fichier (code 256678)'];
     }
 }
