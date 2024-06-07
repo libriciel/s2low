91a0bd7ada7da3e4ee8d34e6fbbb0612ebc6fd51
Julien LE GALL <julien.legall@libriciel.coop>
2024-06-07 16:31:51 +0200

    Extraction de s2lowIntegrationTest

--- integration_tests/AdminAuthorityEditHandlerTest.php
+++ integration_tests/AdminAuthorityEditHandlerTest.php
@@ -2,92 +2,16 @@
 
 declare(strict_types=1);
 
+namespace IntegrationTests;
+
+use S2lowLegacy\Class\Authority;
 use S2lowLegacy\Class\LegacyObjectsManager;
-use S2lowLegacy\Lib\Environnement;
-use S2lowLegacy\Lib\ObjectInstancier;
-use S2lowLegacy\Lib\ObjectInstancierFactory;
-use S2lowLegacy\Lib\PemCertificateFactory;
-use S2lowLegacy\Lib\SQLQuery;
-use Symfony\Bundle\FrameworkBundle\KernelBrowser;
-use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 /**
  *
  */
-class AdminAuthorityEditHandlerTest extends WebTestCase
+class AdminAuthorityEditHandlerTest extends S2lowIntegrationTest
 {
-    /** @var SQLQuery */
-    private SQLQuery $sqlQuery;
-    private PemCertificateFactory $pemCertificateFactory;
-
-    /**
-     * @param int|string $dataName
-     *
-     * @internal This method is not covered by the backward compatibility promise for PHPUnit
-     */
-    public function __construct(?string $name = null, array $data = [], $dataName = '')
-    {
-        parent::__construct($name, $data, $dataName);
-    }
-
-    /**
-     * This method is called before each test.
-     */
-    protected function setUp(): void
-    {
-        parent::setUp();
-        LegacyObjectsManager::resetObjectInstancier();
-        $_SESSION = [];
-        ObjectInstancierFactory::setObjectInstancier(new ObjectInstancier());    //DatabasePool utilise ObjectInstancier
-        $this->sqlQuery = new SQLQuery(DB_DATABASE_TEST);            // On en crée un le temps de MàJ la BDD
-        $this->sqlQuery->setCredential(DB_USER_TEST, DB_PASSWORD_TEST); // On le ressettera ensuite
-        $this->sqlQuery->setDatabaseHost(DB_HOST_TEST);
-        ObjectInstancierFactory::getObjetInstancier()->set(SQLQuery::class, $this->sqlQuery);
-        $this->pemCertificateFactory = new PemCertificateFactory();
-        $this->sqlQuery->exec(file_get_contents(__DIR__ . '/fixtures/s2low-test-init.sql'));
-        ObjectInstancierFactory::resetObjectInstancier();    //DatabasePool utilise ObjectInstancier
-    }
-
-    /**
-     * @throws \Exception
-     */
-    public function setUpUser(string $certificatPem, string $certificatHash): void
-    {
-        $sql = "INSERT INTO users VALUES (1, 'eric@sigmalis.com', 'test_subject', 'test_issuer', 'Pommateau', 'Eric', NULL, 'SADM', 1, 1, ?, NULL, NULL, NULL, 1, NULL, NULL, ?, ?)";
-        $this->sqlQuery->query($sql, [$certificatPem, $certificatPem, $certificatHash]);
-
-        $sql1 = "INSERT INTO users_perms VALUES (64395, 1, 1, 'RW'); -- Permission RW sur le module Actes";
-        $this->sqlQuery->query($sql1);
-        $sql2 = "INSERT INTO users_perms VALUES (64396, 2, 1, 'RW'); -- Permission RW sur le module Helios";
-        $this->sqlQuery->query($sql2);
-        $sql3 = "INSERT INTO users_perms VALUES (64397, 3, 1, 'RW'); -- Permission RW sur le module Mail";
-        $this->sqlQuery->query($sql3);
-    }
-
-    /**
-     * @param string $certificatPem
-     * @param string $certificatSansBegin
-     * @return \Symfony\Bundle\FrameworkBundle\KernelBrowser
-     */
-    private function setUpClient(string $certificatPem, string $certificatSansBegin): KernelBrowser
-    {
-        $serverVariables = [
-            'SSL_CLIENT_VERIFY' => 'ssl_client_verify',
-            'SSL_CLIENT_S_DN' => 'subject_dn',
-            'SSL_CLIENT_I_DN' => 'issuer_dn',
-            'SSL_CLIENT_CERT' => $certificatPem,
-            'HTTP_ORG_S2LOW_FORWARD_X509_IDENTIFICATION' => $certificatSansBegin
-
-        ];
-        foreach ($serverVariables as $key => $value) {
-            $_SERVER[$key] = $value;         // Le client Symfony ne set pas la session, utilisée par l'appli...
-        }
-        return static::createClient(
-            [],
-            $serverVariables
-        );
-    }
-
     /**
      * @throws \Exception
      */
@@ -139,28 +63,29 @@ class AdminAuthorityEditHandlerTest extends WebTestCase
             $crawler->html()
         );
 
-        $authority = new \S2lowLegacy\Class\Authority(1);
+        $authority = new Authority(1);
         $authority->init();
 
-        $this->assertEquals('le nom', $authority->get('name'));
-        $this->assertEquals('111', $authority->get('siren'));
-            $this->assertEquals(1, $authority->get('authority_group_id'));
-            $this->assertEquals('', $authority->get('agreement'));
-            $this->assertEquals('test@test.ts', $authority->get('email'));
-            $this->assertEquals('test@test.ts', $authority->get('default_broadcast_email'));
-            $this->assertEquals('test@test.ts', $authority->get('broadcast_email'));
-            $this->assertEquals(1, $authority->get('status'));
-            $this->assertEquals(1, $authority->get('authority_type_id'));
-            $this->assertEquals('te', $authority->get('address'));
-            $this->assertEquals('29620               ', $authority->get('postal_code')); //WTF ??! Il y a des espaces en plus
-            $this->assertEquals('SAN FRANCISCO', $authority->get('city'));
-            $this->assertEquals('001', $authority->get('department'));
-            $this->assertEquals('1', $authority->get('district'));
-            $this->assertEquals('0000000000', $authority->get('telephone'));
-            $this->assertEquals('0000000000', $authority->get('fax'));
-            $this->assertEquals('test', $authority->get('helios_ftp_dest'));
-            $this->assertEquals('fds@fds.r', $authority->get('email_mail_securise'));
-        $this->assertEquals('fds', $authority->get('descr_mail_securise'));
+        static::assertEquals('le nom', $authority->get('name'));
+        static::assertEquals('111', $authority->get('siren'));
+        static::assertEquals(1, $authority->get('authority_group_id'));
+        static::assertEquals('', $authority->get('agreement'));
+        static::assertEquals('test@test.ts', $authority->get('email'));
+        static::assertEquals('test@test.ts', $authority->get('default_broadcast_email'));
+        static::assertEquals('test@test.ts', $authority->get('broadcast_email'));
+        static::assertEquals(1, $authority->get('status'));
+        static::assertEquals(1, $authority->get('authority_type_id'));
+        static::assertEquals('te', $authority->get('address'));
+        //WTF ??! Il y a des espaces en plus
+        static::assertEquals('29620               ', $authority->get('postal_code'));
+        static::assertEquals('SAN FRANCISCO', $authority->get('city'));
+        static::assertEquals('001', $authority->get('department'));
+        static::assertEquals('1', $authority->get('district'));
+        static::assertEquals('0000000000', $authority->get('telephone'));
+        static::assertEquals('0000000000', $authority->get('fax'));
+        static::assertEquals('test', $authority->get('helios_ftp_dest'));
+        static::assertEquals('fds@fds.r', $authority->get('email_mail_securise'));
+        static::assertEquals('fds', $authority->get('descr_mail_securise'));
     }
 
     /**
@@ -217,27 +142,27 @@ class AdminAuthorityEditHandlerTest extends WebTestCase
         preg_match('#{.*}#', $crawler->html(), $matches);
         $id = json_decode($matches[0])->id;
 
-        $authority = new \S2lowLegacy\Class\Authority($id);
+        $authority = new Authority($id);
         $authority->init();
 
-        $this->assertEquals('le nom', $authority->get('name'));
-        $this->assertEquals('111', $authority->get('siren'));
-        $this->assertEquals(1, $authority->get('authority_group_id'));
-        $this->assertEquals('', $authority->get('agreement'));
-        $this->assertEquals('test@test.ts', $authority->get('email'));
-        $this->assertEquals('test@test.ts', $authority->get('default_broadcast_email'));
-        $this->assertEquals('test@test.ts', $authority->get('broadcast_email'));
-        $this->assertEquals(1, $authority->get('status'));
-        $this->assertEquals(1, $authority->get('authority_type_id'));
-        $this->assertEquals('te', $authority->get('address'));
-        $this->assertEquals('29620               ', $authority->get('postal_code')); //WTF ??! Il y a des espaces en plus
-        $this->assertEquals('SAN FRANCISCO', $authority->get('city'));
-        $this->assertEquals('001', $authority->get('department'));
-        $this->assertEquals('1', $authority->get('district'));
-        $this->assertEquals('0000000000', $authority->get('telephone'));
-        $this->assertEquals('0000000000', $authority->get('fax'));
-        $this->assertEquals('test', $authority->get('helios_ftp_dest'));
-        $this->assertEquals('fds@fds.r', $authority->get('email_mail_securise'));
-        $this->assertEquals('fds', $authority->get('descr_mail_securise'));
+        static::assertEquals('le nom', $authority->get('name'));
+        static::assertEquals('111', $authority->get('siren'));
+        static::assertEquals(1, $authority->get('authority_group_id'));
+        static::assertEquals('', $authority->get('agreement'));
+        static::assertEquals('test@test.ts', $authority->get('email'));
+        static::assertEquals('test@test.ts', $authority->get('default_broadcast_email'));
+        static::assertEquals('test@test.ts', $authority->get('broadcast_email'));
+        static::assertEquals(1, $authority->get('status'));
+        static::assertEquals(1, $authority->get('authority_type_id'));
+        static::assertEquals('te', $authority->get('address'));
+        static::assertEquals('29620               ', $authority->get('postal_code'));
+        static::assertEquals('SAN FRANCISCO', $authority->get('city'));
+        static::assertEquals('001', $authority->get('department'));
+        static::assertEquals('1', $authority->get('district'));
+        static::assertEquals('0000000000', $authority->get('telephone'));
+        static::assertEquals('0000000000', $authority->get('fax'));
+        static::assertEquals('test', $authority->get('helios_ftp_dest'));
+        static::assertEquals('fds@fds.r', $authority->get('email_mail_securise'));
+        static::assertEquals('fds', $authority->get('descr_mail_securise'));
     }
 }
---
+++ integration_tests/S2lowIntegrationTest.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace IntegrationTests;
+
+use S2lowLegacy\Class\LegacyObjectsManager;
+use S2lowLegacy\Lib\ObjectInstancier;
+use S2lowLegacy\Lib\ObjectInstancierFactory;
+use S2lowLegacy\Lib\PemCertificateFactory;
+use S2lowLegacy\Lib\SQLQuery;
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class S2lowIntegrationTest extends WebTestCase
+{
+    /** @var SQLQuery */
+    protected SQLQuery $sqlQuery;
+    protected PemCertificateFactory $pemCertificateFactory;
+
+    /**
+     * @param int|string $dataName
+     *
+     * @internal This method is not covered by the backward compatibility promise for PHPUnit
+     */
+    public function __construct(?string $name = null, array $data = [], $dataName = '')
+    {
+        parent::__construct($name, $data, $dataName);
+    }
+
+    /**
+     * This method is called before each test.
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+        LegacyObjectsManager::resetObjectInstancier();
+        $_SESSION = [];
+        ObjectInstancierFactory::setObjectInstancier(new ObjectInstancier());    //DatabasePool utilise ObjectInstancier
+        $this->sqlQuery = new SQLQuery(DB_DATABASE_TEST);            // On en crée un le temps de MàJ la BDD
+        $this->sqlQuery->setCredential(DB_USER_TEST, DB_PASSWORD_TEST); // On le ressettera ensuite
+        $this->sqlQuery->setDatabaseHost(DB_HOST_TEST);
+        ObjectInstancierFactory::getObjetInstancier()->set(SQLQuery::class, $this->sqlQuery);
+        $this->pemCertificateFactory = new PemCertificateFactory();
+        $this->sqlQuery->exec(file_get_contents(__DIR__ . '/fixtures/s2low-test-init.sql'));
+        ObjectInstancierFactory::resetObjectInstancier();    //DatabasePool utilise ObjectInstancier
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function setUpUser(string $certificatPem, string $certificatHash): void
+    {
+        $sql = "INSERT INTO users VALUES (1, 'eric@sigmalis.com', 'test_subject', 'test_issuer', 'Pommateau', 'Eric', NULL, 'SADM', 1, 1, ?, NULL, NULL, NULL, 1, NULL, NULL, ?, ?)";
+        $this->sqlQuery->query($sql, [$certificatPem, $certificatPem, $certificatHash]);
+
+        $sql1 = "INSERT INTO users_perms VALUES (64395, 1, 1, 'RW'); -- Permission RW sur le module Actes";
+        $this->sqlQuery->query($sql1);
+        $sql2 = "INSERT INTO users_perms VALUES (64396, 2, 1, 'RW'); -- Permission RW sur le module Helios";
+        $this->sqlQuery->query($sql2);
+        $sql3 = "INSERT INTO users_perms VALUES (64397, 3, 1, 'RW'); -- Permission RW sur le module Mail";
+        $this->sqlQuery->query($sql3);
+    }
+
+    /**
+     * @param string $certificatPem
+     * @param string $certificatSansBegin
+     * @return \Symfony\Bundle\FrameworkBundle\KernelBrowser
+     */
+    protected function setUpClient(string $certificatPem, string $certificatSansBegin): KernelBrowser
+    {
+        $serverVariables = [
+            'SSL_CLIENT_VERIFY' => 'ssl_client_verify',
+            'SSL_CLIENT_S_DN' => 'subject_dn',
+            'SSL_CLIENT_I_DN' => 'issuer_dn',
+            'SSL_CLIENT_CERT' => $certificatPem,
+            'HTTP_ORG_S2LOW_FORWARD_X509_IDENTIFICATION' => $certificatSansBegin
+
+        ];
+        foreach ($serverVariables as $key => $value) {
+            $_SERVER[$key] = $value;         // Le client Symfony ne set pas la session, utilisée par l'appli...
+        }
+        return static::createClient(
+            [],
+            $serverVariables
+        );
+    }
+}
