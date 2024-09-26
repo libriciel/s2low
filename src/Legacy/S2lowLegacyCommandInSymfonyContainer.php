42809eeddeb6a5ea70533855259a33776c761521
Julien LE GALL <julien.legall@libriciel.coop>
2024-09-26 10:16:04 +0200

    correction des liens dans admin_authorities.php

--- src/Legacy/S2lowLegacyCommandInSymfonyContainer.php
+++ src/Legacy/S2lowLegacyCommandInSymfonyContainer.php
@@ -6,8 +6,10 @@ use Symfony\Component\HttpFoundation\Response;
 
 abstract class S2lowLegacyCommandInSymfonyContainer
 {
-    public function generateResponse(): Response
+    public function generateResponse(string $requestPath): Response
     {
+        $_SERVER['PHP_SELF'] = $requestPath;
+        $_SERVER['SCRIPT_NAME'] = $requestPath;
         ob_start();
         $this->launchLegacyCommand();
         $output = ob_get_clean();
