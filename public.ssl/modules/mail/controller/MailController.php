3fee8b4b727c80ad4a4629569c59a5a1d3b0d88b
Maxime REYROLLE <maxime.reyrolle@libriciel.coop>
2024-11-14 15:53:27 +0100

    review

--- public.ssl/modules/mail/controller/MailController.php
+++ public.ssl/modules/mail/controller/MailController.php
@@ -85,9 +85,17 @@ class MailController
 
      //---delete l'enregistment choisi.
      //FIXME : ca n'a rien à foutre là: faire un script intermédiaire
+        $db = DatabasePool::getInstance();
+        $mailList = new MailList($db, $this->me->getId());
+
         if ($deleteId != null) {
             foreach ($deleteId as $transId) {
-                MailPeer::DeleteMailTransation($transId);
+                $detail = $mailList->getDetail($transId);
+                if ($detail) {
+                    MailPeer::DeleteMailTransation($transId);
+                } else {
+                    $_SESSION['last_error'] = "ERROR: cette transaction n'existe pas";
+                }
             }
         }
      //----delete fini
