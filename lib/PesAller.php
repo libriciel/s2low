67caa7024de3cbb24bf59989d816d2d0ee6a0e25
Julien LE GALL <julien.legall@libriciel.coop>
2025-03-10 15:45:28 +0100

    permettre l'analyse des PES RETOUR ALLER

--- lib/PesAller.php
+++ lib/PesAller.php
@@ -6,15 +6,23 @@ use Exception;
 
 class PesAller
 {
-    public function getP_MSG($pes_aller_path)
+    /**
+     * @throws Exception
+     */
+    public function getP_MSG($pes_aller_path): string
     {
         $pes_xml = simplexml_load_file($pes_aller_path, 'SimpleXMLElement', LIBXML_PARSEHUGE);
-        if (is_null($pes_xml) || is_null($pes_xml->EnTetePES) || is_null($pes_xml->EnTetePES->CodCol)) {    //Quickfix php8
-            throw new Exception("La balise EnTetePES/CodCol n'est pas présente ou est vide");
+        if (!$pes_xml || empty($pes_xml->EnTetePES)) {
+            throw new Exception("La balise EnTetePES n'est pas présente ou est vide");
         }
-        $cod_col = $pes_xml->EnTetePES->CodCol['V'];
+        if ($pes_xml->getName() === 'PES_ACQUIT_RETOUR') {
+            $cod_col = $pes_xml->EnTetePES->CodColl['V'];
+        } else {
+            $cod_col = $pes_xml->EnTetePES->CodCol['V'];
+        }
+
         if (! $cod_col) {
-            throw new Exception("La balise EnTetePES/CodCol n'est pas présente ou est vide");
+            throw new Exception('La balise EnTetePES/CodCol ou EnTetePES/CodColl n\'est pas présente ou est vide');
         }
         $id_post = $pes_xml->EnTetePES->IdPost['V'];
         if (! $id_post) {
@@ -35,6 +43,6 @@ class PesAller
      */
     public function getP_MSGFromParameters(string $cod_col, string $id_post, string $cod_bud): string
     {
-        return "PES#" . $cod_col . "#" . $id_post . "#" . $cod_bud;
+        return 'PES#' . $cod_col . '#' . $id_post . '#' . $cod_bud;
     }
 }
--- test/PHPUnit/lib/PesAllerTest.php
+++ test/PHPUnit/lib/PesAllerTest.php
@@ -1,10 +1,16 @@
 <?php
 
+declare(strict_types=1);
+
+namespace PHPUnit\lib;
+
+use Exception;
+use PHPUnit\Framework\TestCase;
 use S2lowLegacy\Lib\PesAller;
 
-class PesAllerTest extends PHPUnit_Framework_TestCase
+class PesAllerTest extends TestCase
 {
-    private $pesAller;
+    private PesAller $pesAller;
 
     protected function setUp(): void
     {
@@ -12,16 +18,40 @@ class PesAllerTest extends PHPUnit_Framework_TestCase
         $this->pesAller = new PesAller();
     }
 
-    public function testGetPmsg()
+    /**
+     * @throws Exception
+     * @dataProvider bonsPesAllers
+     */
+    public function testGetPmsg(string $path, string $P_MSG): void
     {
-        $pes_aller_path = __DIR__ . "/fixtures/HELIOS_SIMU_ALR2_1444811220_681372666.xml";
-        $this->assertEquals("PES#123#034000#12", $this->pesAller->getP_MSG($pes_aller_path));
+        static::assertEquals($P_MSG, $this->pesAller->getP_MSG($path));
+    }
+    public function bonsPesAllers(): array
+    {
+        return [
+            [ __DIR__ . '/fixtures/HELIOS_SIMU_ALR2_1444811220_681372666.xml','PES#123#034000#12'],
+            [__DIR__ . '/fixtures/PES_ACQUIT_RETOUR.xml', 'PES#007#123456#12']
+        ];
     }
 
-    public function testGetPmsgBadPesAller()
+    /**
+     * @throws Exception
+     * @dataProvider mauvaisPesAllers
+     */
+    public function testGetPmsgBadPesAller(string $path, string $message)
     {
-        $pes_aller_path = __DIR__ . "/fixtures/test.xml";
-        $this->setExpectedException("Exception", "La balise EnTetePES/CodCol n'est pas présente ou est vide");
-        $this->pesAller->getP_MSG($pes_aller_path);
+        self::expectException(Exception::class);
+        self::expectExceptionMessage($message);
+        $this->pesAller->getP_MSG($path);
+    }
+
+    public function mauvaisPesAllers(): array
+    {
+        return [
+            [__DIR__ . '/fixtures/test.xml', 'La balise EnTetePES n\'est pas présente ou est vide'],
+            [__DIR__ . '/fixtures/HELIOS_SIMU_ALR2_NoCodBud.xml','La balise EnTetePES/CodBud n\'est pas présente ou est vide' ],
+            [__DIR__ . '/fixtures/HELIOS_SIMU_ALR2_NoCodColl.xml', 'La balise EnTetePES/CodCol ou EnTetePES/CodColl n\'est pas présente ou est vide'],
+            [__DIR__ . '/fixtures/HELIOS_SIMU_ALR2_NoIdPost.xml', 'La balise EnTetePES/IdPost n\'est pas présente ou est vide']
+        ];
     }
 }
