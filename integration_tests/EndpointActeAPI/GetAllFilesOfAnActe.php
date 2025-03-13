c5863247a6daa2011d54987b907fe1b1782e4360
cdutheil <cdutheil@libriciel.coop>
2025-03-13 13:37:08 +0100

    1247(test):
    - Ajout d'un test pour le endpoint : actes_transac_get_files_list

---
+++ integration_tests/EndpointActeAPI/GetAllFilesOfAnActe.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace IntegrationTests\EndpointActeAPI;
+
+use IntegrationTests\S2lowIntegrationTestCase;
+use PHPUnit\ActesUtilitiesTestTrait;
+use S2low\Enum\UserRole;
+use S2lowLegacy\Class\actes\ActesStatusSQL;
+use S2lowLegacy\Class\actes\ActesTransactionsSQL;
+use S2lowLegacy\Lib\ObjectInstancierFactory;
+
+class GetAllFilesOfAnActe extends S2lowIntegrationTestCase
+{
+    use ActesUtilitiesTestTrait;
+
+    private ?ActesTransactionsSQL $actesTransactionsSQL;
+
+    protected function setUp(): void
+    {
+        $this->setUpWithoutDeletingObjectInstancier();
+        $this->actesTransactionsSQL = ObjectInstancierFactory::getObjetInstancier()->get(ActesTransactionsSQL::class);
+        ObjectInstancierFactory::resetObjectInstancier();
+    }
+
+    protected function getActesTransactionsSQL(): ActesTransactionsSQL
+    {
+        return $this->actesTransactionsSQL;
+    }
+
+    public function testShouldReturnOk(): void
+    {
+        $client = $this->getAuthenticatedClientWithUserLoggedAs(UserRole::Utilisateur);
+
+        $transactionId = $this->createTransaction(ActesStatusSQL::STATUS_ACQUITTEMENT_RECU);
+        $this->createActeIncludedFiles($transactionId);
+
+        $_GET['transaction'] = $transactionId;
+
+        $client->request(
+            'GET',
+            '/modules/actes/actes_transac_get_files_list.php',
+            [
+                'transaction' => $transactionId,
+
+            ],
+        );
+
+        $response = $client->getResponse();
+        $content = explode("\n", trim($response->getContent()));
+
+        static::assertNotSame('KO', $content[0]);
+        static::assertJson($content[0]);
+
+        $files = json_decode($response->getContent(), true);
+
+        foreach ($files as $file) {
+            static::assertArrayHasKey('id', $file);
+            static::assertArrayHasKey('name', $file);
+            static::assertArrayHasKey('posted_filename', $file);
+            static::assertArrayHasKey('mimetype', $file);
+            static::assertArrayHasKey('size', $file);
+            static::assertArrayHasKey('sign', $file);
+            static::assertArrayHasKey('code_pj', $file);
+        }
+    }
+}
