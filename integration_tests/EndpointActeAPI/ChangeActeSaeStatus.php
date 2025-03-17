d02de56708cbe36ceb722cc0f3c43d8f75a1d95a
cdutheil <cdutheil@libriciel.coop>
2025-03-17 16:11:22 +0100

    1247(test):
    - Ajout un test pour le endpoint : actes_sae_status

---
+++ integration_tests/EndpointActeAPI/ChangeActeSaeStatus.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace IntegrationTests\EndpointActeAPI;
+
+use IntegrationTests\S2lowIntegrationTestCase;
+use PHPUnit\ActesUtilitiesTestTrait;
+use S2low\Enum\UserRole;
+use S2lowLegacy\Class\actes\ActesStatusSQL;
+use S2lowLegacy\Class\actes\ActesTransactionsSQL;
+use S2lowLegacy\Lib\ObjectInstancierFactory;
+
+class ChangeActeSaeStatus extends S2lowIntegrationTestCase
+{
+    use ActesUtilitiesTestTrait;
+
+    private ?ActesTransactionsSQL $actesTransactionsSQL;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->actesTransactionsSQL = new ActesTransactionsSQL($this->sqlQuery);
+        ObjectInstancierFactory::resetObjectInstancier();
+    }
+
+    protected function getActesTransactionsSQL(): ActesTransactionsSQL
+    {
+        return $this->actesTransactionsSQL;
+    }
+
+    public function changeStatusProvider(): array
+    {
+        return [
+            [
+                UserRole::Archiviste,
+                ActesStatusSQL::STATUS_ACQUITTEMENT_RECU,
+                ActesStatusSQL::STATUS_ENVOYE_AU_SAE,
+                'status',
+                'ok',
+                200
+            ],
+            [
+                UserRole::Archiviste,
+                ActesStatusSQL::STATUS_ACQUITTEMENT_RECU,
+                ActesStatusSQL::STATUS_ERREUR_LORS_DE_L_ARCHIVAGE,
+                'status',
+                'ok',
+                200
+            ],
+            [
+                UserRole::Archiviste,
+                ActesStatusSQL::STATUS_VALIDE,
+                ActesStatusSQL::STATUS_ENVOYE_AU_SAE,
+                'status',
+                'ok',
+                200
+            ],
+            [
+                UserRole::Archiviste,
+                ActesStatusSQL::STATUS_ENVOYE_AU_SAE,
+                ActesStatusSQL::STATUS_ENVOYE_AU_SAE,
+                'error',
+                'Transition entre deux status identiques (12) impossible',
+                400
+            ],
+            [
+                UserRole::Utilisateur,
+                ActesStatusSQL::STATUS_ACQUITTEMENT_RECU,
+                ActesStatusSQL::STATUS_ENVOYE_AU_SAE,
+                'error',
+                'L\'utilisateur n\'est pas archiviste',
+                400
+            ],
+            [
+                UserRole::Archiviste,
+                ActesStatusSQL::STATUS_VALIDE,
+                ActesStatusSQL::STATUS_ACQUITTEMENT_RECU,
+                'error',
+                'Transition vers le statut 4 impossible',
+                400
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider changeStatusProvider
+     */
+    public function testActeStatusTransition(
+        UserRole $userRole,
+        int $acteStatusDepart,
+        int $acteStatusDestination,
+        string $arrayKeyAnswer,
+        string $arrayValueAnswer,
+        int $codeResponse
+    ): void {
+        $client = $this->getAuthenticatedClientWithUserLoggedAs($userRole);
+
+        $transactionId = $this->createTransaction($acteStatusDepart);
+
+        $statusId = $acteStatusDestination;
+
+        $_POST['transaction_id'] = $transactionId;
+        $_POST['status_id'] = $statusId;
+
+        $client->request('POST', '/modules/actes/api/actes_sae_status.php', [
+            'transaction_id' => $transactionId,
+            'status_id' => $statusId,
+        ]);
+
+        $response = $client->getResponse();
+
+        $content = explode("\n", trim($response->getContent()));
+
+        static::assertJson($content[0]);
+        $contentAsArray = json_decode($content[0], true);
+        static::assertArrayHasKey($arrayKeyAnswer, $contentAsArray);
+        static::assertEquals($arrayValueAnswer, $contentAsArray[$arrayKeyAnswer]);
+        static::assertResponseStatusCodeSame($codeResponse);
+    }
+}
