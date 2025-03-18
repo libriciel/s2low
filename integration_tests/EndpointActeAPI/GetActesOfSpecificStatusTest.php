4abf61d27213c05c54762ec2787a7a0a089bd780
cdutheil <cdutheil@libriciel.coop>
2025-03-18 16:12:11 +0100

    1247(test):
    - ajout quelques cas de test au endpoint list_actes

--- integration_tests/EndpointActeAPI/GetActesOfSpecificStatusTest.php
+++ integration_tests/EndpointActeAPI/GetActesOfSpecificStatusTest.php
@@ -25,29 +25,79 @@ class GetActesOfSpecificStatusTest extends S2lowIntegrationTestCase
         return $this->actesTransactionsSQL;
     }
 
-    public function testShouldReturnOk(): void
+    protected function dataProvider(): array
+    {
+        return [
+            [
+                [
+                    'offset' => 0,
+                    'limit' => 5,
+                    'minDate' => (new \DateTimeImmutable('2000-01-01'))->format('Y-m-d'),
+                    'maxDate' => date('Y-m-d'),
+                    'nbTransactionToCreate' => 1,
+                    'nbTransactionToReturn' => 1
+                ]
+            ],
+            [
+                [
+                    'offset' => 1,
+                    'limit' => 5,
+                    'minDate' => (new \DateTimeImmutable('2000-01-01'))->format('Y-m-d'),
+                    'maxDate' => date('Y-m-d'),
+                    'nbTransactionToCreate' => 5,
+                    'nbTransactionToReturn' => 4
+                ]
+            ],
+            [
+                [
+                    'offset' => 0,
+                    'limit' => 5,
+                    'minDate' => (new \DateTimeImmutable('2100-01-01'))->format('Y-m-d'),
+                    'maxDate' => date('Y-m-d'),
+                    'nbTransactionToCreate' => 5,
+                    'nbTransactionToReturn' => 0
+                ]
+            ],
+            [
+                [
+                    'offset' => 0,
+                    'limit' => 5,
+                    'minDate' => (new \DateTimeImmutable('2000-01-01'))->format('Y-m-d'),
+                    'maxDate' => (new \DateTimeImmutable('2001-01-01'))->format('Y-m-d'),
+                    'nbTransactionToCreate' => 5,
+                    'nbTransactionToReturn' => 0
+                ]
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider dataProvider
+     */
+    public function testShouldReturnOk($data): void
     {
         $client = $this->getAuthenticatedClientWithUserLoggedAs(UserRole::SuperAdministrateur);
-        $this->createTransaction(ActesStatusSQL::STATUS_ACQUITTEMENT_RECU);
+
+        $this->createTransaction(ActesStatusSQL::STATUS_VALIDE);
+
+        for ($i = $data['nbTransactionToCreate']; $i > 0; $i--) {
+            $this->createTransaction(ActesStatusSQL::STATUS_ACQUITTEMENT_RECU);
+        }
 
         $statusId = ActesStatusSQL::STATUS_ACQUITTEMENT_RECU;
-        $offset = 0;
-        $limit = 5;
-        $minDate = (new \DateTimeImmutable('2000-01-01'))->format('Y-m-d');
-        $maxDate = date('Y-m-d');
 
         $_GET['status_id'] = $statusId;
-        $_GET['offset'] = $offset;
-        $_GET['limit'] = $limit;
-        $_GET['min_date'] = $minDate;
-        $_GET['max_date'] = $maxDate;
+        $_GET['offset'] = $data['offset'];;
+        $_GET['limit'] = $data['limit'];
+        $_GET['min_date'] = $data['minDate'];
+        $_GET['max_date'] = $data['maxDate'];
 
         $client->request('GET', '/modules/actes/api/list_actes.php', [
             'status_id' => $statusId,
-            'offset' => $offset,
-            'limit' => $limit,
-            'min_date' => $minDate,
-            'max_date' => $maxDate,
+            'offset' => $data['offset'],
+            'limit' => $data['limit'],
+            'min_date' => $data['minDate'],
+            'max_date' => $data['maxDate'],
         ]);
 
         $response = $client->getResponse();
@@ -57,12 +107,15 @@ class GetActesOfSpecificStatusTest extends S2lowIntegrationTestCase
         static::assertJson($content[0]);
 
         $contentAsArray = json_decode($content[0], true);
-
         static::assertArrayHasKey('status_id', $contentAsArray);
         static::assertArrayHasKey('authority_id', $contentAsArray);
         static::assertArrayHasKey('offset', $contentAsArray);
         static::assertArrayHasKey('limit', $contentAsArray);
         static::assertArrayHasKey('transactions', $contentAsArray);
-        static::assertCount(1, $contentAsArray['transactions']);
+        static::assertCount($data['nbTransactionToReturn'], $contentAsArray['transactions']);
+
+        static::assertEquals($statusId, $contentAsArray['status_id']);
+        static::assertEquals($data['offset'], $contentAsArray['offset']);
+        static::assertEquals($data['limit'], $contentAsArray['limit']);
     }
 }
