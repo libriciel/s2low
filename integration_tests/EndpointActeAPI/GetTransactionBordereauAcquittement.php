1c8bd4322bf31ddb4b34d3ad0e5f32aaf157de3f
cdutheil <cdutheil@libriciel.coop>
2025-03-18 14:45:17 +0100

    1247(test):
    - ajout du test pour le endpoint actes_create_pdf. ajout des wrapper dans le code

---
+++ integration_tests/EndpointActeAPI/GetTransactionBordereauAcquittement.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace IntegrationTests\EndpointActeAPI;
+
+use IntegrationTests\S2lowIntegrationTestCase;
+use PHPUnit\ActesUtilitiesTestTrait;
+use S2low\Enum\UserRole;
+use S2lowLegacy\Class\actes\ActesStatusSQL;
+use S2lowLegacy\Class\actes\ActesTransactionsSQL;
+use S2lowLegacy\Lib\ObjectInstancierFactory;
+
+class GetTransactionBordereauAcquittement extends S2lowIntegrationTestCase
+{
+    use ActesUtilitiesTestTrait;
+
+    private ?ActesTransactionsSQL $actesTransactionsSQL;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->actesTransactionsSQL = new ActesTransactionsSQL($this->sqlQuery);
+        ObjectInstancierFactory::resetObjectInstancier();
+    }
+
+    protected function getActesTransactionsSQL(): ActesTransactionsSQL
+    {
+        return $this->actesTransactionsSQL;
+    }
+
+    public function testShouldReturnOk(): void
+    {
+        $client = $this->getAuthenticatedClientWithUserLoggedAs(UserRole::Utilisateur);
+
+        $transactionId = $this->createTransaction(ActesStatusSQL::STATUS_ACQUITTEMENT_RECU);
+
+        $_GET['trans_id'] = $transactionId;
+
+        $client->request('GET', '/modules/actes/actes_create_pdf.php', [
+            'trans_id' => $transactionId
+        ]);
+
+        $response = $client->getResponse();
+
+        var_dump($response->getContent());
+        static::assertStringContainsString(
+            "%PDF-1.3
+3 0 obj
+<</Type /Page
+/Parent 1 0 R
+/Resources 2 0 R
+/Contents 4 0 R>>",
+            $response->getContent()
+        );
+    }
+
+}
