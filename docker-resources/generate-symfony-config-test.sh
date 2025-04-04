8c7e769609d8a005e5abdc7055f69cdfa38b9c80
Julien LE GALL <julien.legall@libriciel.coop>
2025-04-04 15:41:35 +0200

    configuration de la BDD de test

---
+++ docker-resources/generate-symfony-config-test.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+set -e
+
+# Utiliser pour creer un fichier de settings symfony en fonction des variables d'environnement (envoye par Docker)
+#TODO : integrer les variables WEB_HTTP_PORT et WEB_HTTPS_PORT
+
+cat <<EOF
+
+DB_HOST=${POSTGRES_HOST_TEST:-dbtest}
+DB_USER=${POSTGRES_USER_TEST:-s2lowusertest}
+DB_PASSWORD=${POSTGRES_PASSWORD_TEST:-s2lowpasswordtest}
+DB_DATABASE=${POSTGRES_DATABASE_TEST:-s2lowdbtest}
+
+EOF
