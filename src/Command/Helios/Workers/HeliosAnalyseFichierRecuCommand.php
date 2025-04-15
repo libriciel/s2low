6fe23ea22e0dce118f440882c94d3c2a253ac269
cdutheil <cdutheil@libriciel.coop>
2025-04-15 09:31:19 +0200

    1265(revert):
    - suppresion de la methode configure de la commande et utilisation de l'attribut AsCommand a la place
    - utilisation de la promotion des proprietes dans le constructeur plutot que des attributs habituel + ajout du readonly

--- src/Command/Helios/Workers/HeliosAnalyseFichierRecuCommand.php
+++ src/Command/Helios/Workers/HeliosAnalyseFichierRecuCommand.php
@@ -4,31 +4,22 @@ namespace S2low\Command\Helios\Workers;
 
 use S2lowLegacy\Class\helios\HeliosAnalyseFichierRecuWorker;
 use S2lowLegacy\Class\WorkerRunnerBuilder;
+use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
+#[AsCommand(
+    name: 'worker:helios-analyse-fichier-recu',
+    description: 'Analyse les fichiers recu de la DGFIP'
+)]
 class HeliosAnalyseFichierRecuCommand extends Command
 {
-    private WorkerRunnerBuilder $workerBuilder;
-    private HeliosAnalyseFichierRecuWorker $worker;
-
     public function __construct(
-        WorkerRunnerBuilder $workerRunnerBuilder,
-        HeliosAnalyseFichierRecuWorker $worker
+        private readonly WorkerRunnerBuilder $workerBuilder,
+        private readonly HeliosAnalyseFichierRecuWorker $worker
     ) {
-        $this->workerBuilder = $workerRunnerBuilder;
-        $this->worker = $worker;
         parent::__construct();
-    }
-
-    protected function configure(): void
-    {
-        $this
-            ->setName('helios:analyse-fichier-recu')
-            ->setDescription(
-                "Analyse les fichiers recu de la DGFIP"
-            );
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
