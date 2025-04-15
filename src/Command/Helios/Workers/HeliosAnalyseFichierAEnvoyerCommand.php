6da7d88d86296311c0f87777f11f33847a0e57c7
cdutheil <cdutheil@libriciel.coop>
2025-04-15 09:30:08 +0200

    1265(revert):
    - suppresion de la methode configure de la commande et utilisation de l'attribut AsCommand a la place

--- src/Command/Helios/Workers/HeliosAnalyseFichierAEnvoyerCommand.php
+++ src/Command/Helios/Workers/HeliosAnalyseFichierAEnvoyerCommand.php
@@ -4,10 +4,15 @@ namespace S2low\Command\Helios\Workers;
 
 use S2low\Services\Helios\HeliosAnalyseFichierAEnvoyerWorker;
 use S2lowLegacy\Class\WorkerRunnerBuilder;
+use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use  Symfony\Component\Console\Output\OutputInterface;
 
+#[AsCommand(
+    name: 'worker:helios-analyse-fichier-a-envoyer',
+    description: 'Analyse les flux à envoyer à la DGFiP'
+)]
 class HeliosAnalyseFichierAEnvoyerCommand extends Command
 {
     private WorkerRunnerBuilder $workerRunnerBuilder;
@@ -20,15 +25,6 @@ class HeliosAnalyseFichierAEnvoyerCommand extends Command
         $this->workerRunnerBuilder = $workerRunnerBuilder;
         $this->heliosAnalyseFichierAEnvoyer = $analyseFichierAEnvoyerWorkerFactory;
         parent::__construct();
-    }
-
-    protected function configure(): void
-    {
-        $this
-            ->setName('helios:analyse-fichier-a-envoyer')
-            ->setDescription(
-                "Analyse des flux à envoyé à la DGFiP"
-            );
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
