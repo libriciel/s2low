06d7439cecde775a8dd27734b3216f2a9fd0dfb7
Julien LE GALL <julien.legall@libriciel.coop>
2025-05-12 16:48:14 +0200

    ajout de test

--- test/PHPUnit/class/actes/ActesFileSenderTest.php
+++ test/PHPUnit/class/actes/ActesFileSenderTest.php
@@ -25,6 +25,7 @@ class ActesFileSenderTest extends TestCase
             ->disableOriginalConstructor()->getMock();
 
         $this->setProperties = [];
+        parent::setUp();
     }
     /**
      * @throws Exception
@@ -108,6 +109,115 @@ class ActesFileSenderTest extends TestCase
 
         self::expectException(Exception::class);
         self::expectExceptionMessage('Last Error');
+        static::assertTrue($actesFileSender->send('/path/to/filetosend.tar.gz'));
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function testSendMinistereWrongCertificate(): void
+    {
+        $actesMinistereProperties = new ActesMinistereProperties(
+            'https://128.127.126.125:9456/path/to/upload.php',
+            ActesMinistereProperties::AUTHENTICATION_POST,
+            'login',
+            'password',
+            'client_certificate',
+            'client_certificate_key',
+            'client_certificate_password',
+            false,
+            __DIR__ . '/../fixtures/toto.txt',
+        );
+
+        $actesFileSender = new ActesFileSender(
+            $actesMinistereProperties,
+            '/path/to/truststore',
+            $this->curlWrapperFactoryMock,
+            $this->x509CertificateMock
+        );
+
+        $this->curlWrapperMock->method('getHTTPCode')->willReturn(200);
+        $this->x509CertificateMock->method('getBase64Hash')
+            ->willReturnOnConsecutiveCalls('A', 'B');
+
+        self::expectException(Exception::class);
+        self::expectExceptionMessage('Le certificat recu (A) ne correspond pas à celui attendu (B)');
+        static::assertTrue($actesFileSender->send('/path/to/filetosend.tar.gz'));
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function testAdaptProtocol(): void
+    {
+        $actesMinistereProperties = new ActesMinistereProperties(
+            'https://128.127.126.125:9456/path/to/upload.php',
+            ActesMinistereProperties::AUTHENTICATION_POST,
+            'login',
+            'password',
+            'client_certificate',
+            'client_certificate_key',
+            'client_certificate_password',
+            true,
+            __DIR__ . '/../fixtures/toto.txt',
+        );
+
+        $actesFileSender = new ActesFileSender(
+            $actesMinistereProperties,
+            '/path/to/truststore',
+            $this->curlWrapperFactoryMock,
+            $this->x509CertificateMock
+        );
+
+        $this->mockSetPropertiesFunction();
+
+        $this->curlWrapperMock->method('getHTTPCode')->willReturn(200);
+        static::assertTrue($actesFileSender->send('/path/to/filetosend.tar.gz'));
+
+        $expectedParameters = [
+            CURLOPT_SSL_VERIFYHOST => 0,
+            CURLOPT_CERTINFO => 1,
+            CURLOPT_CAPATH => '/path/to/truststore',
+            CURLOPT_SSL_CIPHER_LIST => 'DEFAULT@SECLEVEL=0 !LOW !MEDIUM !RC4 !aNULL !eNULL !LOW !MD5 !EXP AES256-SHA '
+        ];
+        self::assertEqualsCanonicalizing($expectedParameters, $this->setProperties);
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function testAuthenticationBasic(): void
+    {
+        $actesMinistereProperties = new ActesMinistereProperties(
+            'https://128.127.126.125:9456/path/to/upload.php',
+            ActesMinistereProperties::AUTHENTICATION_BASIC,
+            'login',
+            'password',
+            'client_certificate',
+            'client_certificate_key',
+            'client_certificate_password',
+            false,
+            __DIR__ . '/../fixtures/toto.txt',
+        );
+
+        $actesFileSender = new ActesFileSender(
+            $actesMinistereProperties,
+            '/path/to/truststore',
+            $this->curlWrapperFactoryMock,
+            $this->x509CertificateMock
+        );
+
+        $this->curlWrapperMock->expects(self::once())->method('setClientCertificate')
+            ->with(
+                'client_certificate',
+                'client_certificate_key',
+                'client_certificate_password'
+            );
+
+        $this->curlWrapperMock->expects(self::once())->method('httpAuthentication')
+            ->with('login', 'password');
+
+        $this->curlWrapperMock->method('getHTTPCode')->willReturn(200);
         static::assertTrue($actesFileSender->send('/path/to/filetosend.tar.gz'));
     }
 
