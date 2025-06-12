0f971b279848babbac5b406efbb69010d23b8e29
Julien LE GALL <julien.legall@libriciel.coop>
2025-06-12 12:02:02 +0200

    ajout de la possibilité de se connecter aux nouveaux serveurs DGCL

--- class/actes/ActesMinistereProperties.php
+++ class/actes/ActesMinistereProperties.php
@@ -17,7 +17,8 @@ class ActesMinistereProperties
         public string $client_certificate_key,
         public string $client_certificate_key_password,
         public string $adapt_protocol,
-        public string $server_certificate_path
+        public string $server_certificate_path,
+        public bool $use_legacy_protocol
     ) {
     }
 }
