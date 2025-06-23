bb763b1576278a6beb6d2e198d027ab0a61a9d87
cdutheil <cdutheil@libriciel.coop>
2025-06-23 16:26:02 +0200

    1339: essaye fix les tests lie a actespdf et CreatePESAllerTest

--- integration_tests/EndpointApiHelios/CreatePESAllerTest.php
+++ integration_tests/EndpointApiHelios/CreatePESAllerTest.php
@@ -64,6 +64,7 @@ class CreatePESAllerTest extends S2lowIntegrationTestCase
         $fileContent = file_get_contents($filePath);
         $fileType = 'application/xml';
         $fileError = UPLOAD_ERR_OK;
+        $toTestFilePath = sys_get_temp_dir() . '/' . $fileName;
 
         $toUploadFile = new UploadedFile(
             $filePath,
@@ -73,16 +74,18 @@ class CreatePESAllerTest extends S2lowIntegrationTestCase
             true
         );
 
-        $vfsUrl = vfsStream::url('test/' . $fileName);
-        copy($filePath, $vfsUrl);
-        $filePathFromUseCaseCode = $vfsUrl;
+        try {
+            copy($filePath, $toTestFilePath);
+        } catch (\Exception $e) {
+            $this->fail("Impossible de copier le fichier de test : $filePath");
+        }
 
         $_FILES['enveloppe'] = '';
         if ($data['with_enveloppe']) {
             $_FILES['enveloppe'] = [
                 'name' => $fileName,
                 'type' => $fileType,
-                'tmp_name' => $filePathFromUseCaseCode,
+                'tmp_name' => $toTestFilePath,
                 'error' => $fileError,
                 'size' => 107
             ];
--- test/PHPUnit/class/actes/ActesPdfTest.php
+++ test/PHPUnit/class/actes/ActesPdfTest.php
@@ -10,16 +10,16 @@ class ActesPdfTest extends S2lowTestCase
 {
     use ActesUtilitiesTestTrait;
 
-//    public function testCreatePdf()
-//    {
-//        $this->getObjectInstancier()->set(IActesPdf::class, new ActesPdf(SITEROOT . "public.ssl/custom/images/bandeau-s2low-190.jpg"));
-//
-//        $transaction_id = $this->createTransactionOfType(4);
-//
-//        $bordereauPdfGenerator = $this->getObjectInstancier()->get(BordereauPdfGenerator::class);
-//
-//        $this->assertNotEmpty($bordereauPdfGenerator->generate($transaction_id, "test_pdf.pdf", true, "S"));
-//    }
+    public function testCreatePdf()
+    {
+        $this->getObjectInstancier()->set(IActesPdf::class, new ActesPdf(SITEROOT . "public.ssl/custom/images/bandeau-s2low-190.jpg"));
+
+        $transaction_id = $this->createTransactionOfType(4);
+
+        $bordereauPdfGenerator = $this->getObjectInstancier()->get(BordereauPdfGenerator::class);
+
+        $this->assertNotEmpty($bordereauPdfGenerator->generate($transaction_id, "test_pdf.pdf", true, "S"));
+    }
 
     protected function getActesTransactionsSQL(): ActesTransactionsSQL
     {
