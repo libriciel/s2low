daac6e0e76a1fa628838ec1dbbd6b29c09695e47
Julien LE GALL <julien.legall@libriciel.coop>
2025-06-25 17:47:48 +0200

    ajout de la verification de la validite du path

---
+++ lib/CertificateChain.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace S2lowLegacy\Lib;
+
+class CertificateChain
+{
+    /**
+     * @var \S2lowLegacy\Lib\PemCertificate[]
+     */
+    private array $certificates;
+
+    public function __construct(array $certificates)
+    {
+        $this->certificates = $certificates;
+    }
+
+    public function hasValidPathToRoot(): bool
+    {
+        $expectedSubjectDN = $this->certificates[0]->getSubjectDN();
+        $lastCertificateKey = count($this->certificates) - 1;
+        foreach ($this->certificates as $key => $certificate) {
+            if ($certificate->getSubjectDN() !== $expectedSubjectDN) {
+                return false;
+            }
+            if ($key === $lastCertificateKey) {
+                break;
+            }
+            $expectedSubjectDN = $certificate->getIssuerDN();
+        }
+        return $certificate->isAutosigned();
+    }
+}
---
+++ test/PHPUnit/lib/CertificateChainTest.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace PHPUnit\lib;
+
+use Exception;
+use PHPUnit\Framework\TestCase;
+use S2lowLegacy\Lib\CertificateChain;
+use S2lowLegacy\Lib\PemCertificateFactory;
+
+class CertificateChainTest extends TestCase
+{
+    public const BASE_CERTIFICATES_DIR = __DIR__ . '/../core/fixtures/CertAutosignedRoot/';
+    private \S2lowLegacy\Lib\PemCertificate $x509_pem_certificate;
+    private \S2lowLegacy\Lib\PemCertificate $x509_ca_certificate;
+    private \S2lowLegacy\Lib\PemCertificate $x509_intermediate_certificate;
+
+    /**
+     * @throws Exception
+     */
+    public function __construct()
+    {
+        parent::__construct();
+        $factory = new PemCertificateFactory();
+        $this->x509_pem_certificate = $factory->getFromString(
+            file_get_contents(self::BASE_CERTIFICATES_DIR . 's2low-test-u.pem')
+        );
+        $this->x509_ca_certificate = $factory->getFromString(
+            file_get_contents(self::BASE_CERTIFICATES_DIR . 'AC_LIBRICIEL_RACINE_G1_CHAIN.pem')
+        );
+        $this->x509_intermediate_certificate = $factory->getFromString(
+            file_get_contents(self::BASE_CERTIFICATES_DIR . 'AC_LIBRICIEL_PERSONNEL_G2_CHAIN.pem')
+        );
+    }
+
+    public function testHasValidPathToRoot()
+    {
+        $certificateChain = new CertificateChain([
+            $this->x509_pem_certificate,
+            $this->x509_intermediate_certificate,
+            $this->x509_ca_certificate
+        ]);
+        self::assertTrue($certificateChain->hasValidPathToRoot());
+    }
+
+    public function testHasValidPathToRootBrokenPath()
+    {
+        $certificateChain = new CertificateChain([
+            $this->x509_pem_certificate,
+            $this->x509_ca_certificate
+        ]);
+        self::assertFalse($certificateChain->hasValidPathToRoot());
+    }
+
+    public function testHasValidPathToRootNoAutosignedAtRoot()
+    {
+        $certificateChain = new CertificateChain([
+            $this->x509_pem_certificate,
+            $this->x509_intermediate_certificate,
+        ]);
+        self::assertFalse($certificateChain->hasValidPathToRoot());
+    }
+}
