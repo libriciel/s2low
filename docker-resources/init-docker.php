29297e1ab185b5c018c7c5b8eb5fb32f7676d020
cdutheil <cdutheil@libriciel.coop>
2025-06-25 14:35:32 +0200

    1339: suppression de init-docker non utilise et suppression de commentaire inutile dans docker-s2low-entrypoint

--- docker-resources/init-docker.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-require_once __DIR__ . "/../init/init.php";
-$slowBootrap = \S2lowLegacy\Class\LegacyObjectsManager::getLegacyObjectInstancier()->get(S2lowBootstrap::class);
-
-$slowBootrap->bootstrap();
