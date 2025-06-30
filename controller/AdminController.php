a4fe4fafc302ae5410b5894f64971a0afe46d271
cdutheil <cdutheil@libriciel.coop>
2025-06-30 09:17:08 +0200

    1339: suppression d'un use inutile

--- controller/AdminController.php
+++ controller/AdminController.php
@@ -3,7 +3,6 @@
 namespace S2lowLegacy\Controller;
 
 use Exception;
-use S2lowLegacy\Class\LegacyObjectsManager;
 use S2lowLegacy\Class\PagerHTML;
 use S2lowLegacy\Lib\FancyDate;
 use S2lowLegacy\Lib\JSONoutput;
