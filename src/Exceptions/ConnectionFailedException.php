93ceea0266e1dfa6ed98d0bc2d1eae050c693559
cdutheil <cdutheil@libriciel.coop>
2025-11-07 14:48:27 +0100

    (1447) remplace un certif de test + fix test

---
+++ src/Exceptions/ConnectionFailedException.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace S2low\Exceptions;
+
+class ConnectionFailedException extends \RuntimeException
+{
+
+    public function __construct(string $message = ' impossible de continuer.')
+    {
+        parent::__construct('Une connexion a échoué : ' . $message);
+    }
+}
