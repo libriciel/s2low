528bdfdd2f28f9f804d12ffbe70e3e67e8a762ca
cdutheil <cdutheil@libriciel.coop>
2025-11-20 10:51:08 +0100

    #1451 Extraction de la logique de récupération des credentials dans une classe dédiée

---
+++ src/Security/CredentialsExtractor.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace S2low\Security;
+
+use Symfony\Component\HttpFoundation\Request;
+
+class CredentialsExtractor
+{
+    /**
+     * @return array{login: string|null, password: string|null}
+     */
+    public function extract(Request $request): array
+    {
+        $httpBasicAuthCredentials = $this->extractFromHttpBasicAuth($request);
+
+        if ($this->areCredentialsPresent($httpBasicAuthCredentials)) {
+            return $httpBasicAuthCredentials;
+        }
+
+        return $this->extractFromPostData($request);
+    }
+
+    /**
+     * @return array{login: string|null, password: string|null}
+     */
+    private function extractFromHttpBasicAuth(Request $request): array
+    {
+        return [
+            'login' => $request->server->get('PHP_AUTH_USER'),
+            'password' => $request->server->get('PHP_AUTH_PW'),
+        ];
+    }
+
+    /**
+     * @return array{login: string|null, password: string|null}
+     */
+    private function extractFromPostData(Request $request): array
+    {
+        return [
+            'login' => $request->request->get('login'),
+            'password' => $request->request->get('password'),
+        ];
+    }
+
+    /**
+     * @param array{login: string|null, password: string|null} $credentials
+     */
+    private function areCredentialsPresent(array $credentials): bool
+    {
+        return !empty($credentials['login']) && !empty($credentials['password']);
+    }
+}
