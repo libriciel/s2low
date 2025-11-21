436814ba32ce13c1009cbfb0624a6dd558ba92c6
cdutheil <cdutheil@libriciel.coop>
2025-11-21 16:12:30 +0100

    #1455 refactor: Modernisation des URLs (/connexion, /deconnexion) et suppression des routes legacy

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,15 +2,43 @@
 
 namespace S2low\Controller;
 
+use S2low\Security\CertificateExtractor;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
-    #[Route('/logout', name: 'app_logout', methods: ['GET'])]
+    #[Route('/connexion', name: 'app_login', methods: ['GET', 'POST'])]
+    public function login(
+        AuthenticationUtils $authenticationUtils,
+        CertificateExtractor $certificateExtractor,
+        Request $request
+    ): Response {
+        // Si l'utilisateur est déjà connecté, rediriger vers la page d'accueil
+        if ($this->getUser()) {
+            return $this->redirect('/');
+        }
+
+        // Si un certificat est présent, rediriger vers login.php pour utiliser l'authentification par certificat
+        if ($certificateExtractor->hasValidCertificate($request)) {
+            return $this->redirect('/login.php');
+        }
+
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('/security/connexion.html.twig', [
+            'last_username' => $lastUsername,
+            'error' => $error,
+        ]);
+    }
+
+    #[Route('/deconnexion', name: 'app_logout', methods: ['GET'])]
     public function logout(): void
     {
-        // Ce contrôleur ne sera jamais exécuté car Symfony intercepte la route
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
 }
