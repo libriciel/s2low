9bc556e66c205cfa9dd84da92ff4d3e640fd8114
Julien LE GALL <julien.legall@libriciel.coop>
2026-01-28 19:36:50 +0100

    traitement de VerifyPKCS7Signature

--- test/actes/test_actes_signature.php
+++ test/actes/test_actes_signature.php
@@ -2,8 +2,10 @@
 
 use S2low\Services\ProcessCommand\CommandLauncher;
 use S2low\Services\ProcessCommand\OpenSSLWrapper;
+use S2low\Services\Validators\PKCS7SignatureValidator;
 use S2lowLegacy\Class\VerifyPemCertificate;
 use S2lowLegacy\Class\VerifyPKCS7Signature;
+use S2lowLegacy\Lib\PemCertificate;
 use S2lowLegacy\Lib\PemCertificateFactory;
 
 $file_path = "/Users/eric/Desktop/test/034-123456725-20151201-TESTS132-AU-1-1_1.pdf";
@@ -19,12 +21,7 @@ $actesItems = $dom->children($namespaces["actes"]);
 
 $signature =  $actesItems->Document->Signature . "\n";
 
+/** @var PKCS7SignatureValidator $PKCS7SignatureValidator */
+$PKCS7SignatureValidator = \S2lowLegacy\Class\LegacyObjectsManager::getObject(PKCS7SignatureValidator::class);
 
-$verifyPKCS7Signature = new VerifyPKCS7Signature(
-    "/etc/tedetis/ssl/validca/",
-    new OpenSSLWrapper(new CommandLauncher()),
-    new VerifyPemCertificate(new OpenSSLWrapper(new CommandLauncher())),
-    new PemCertificateFactory(),
-);
-
-$verifyPKCS7Signature->verifySignature($signature, VerifyPemCertificate::CERTIFICATE_CHAIN_ERRORS);
+$PKCS7SignatureValidator->validate($signature, VerifyPemCertificate::CERTIFICATE_CHAIN_ERRORS);
