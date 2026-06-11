9533f9e502e6ece466944da4226bcb71f5a6af26
cdutheil <cdutheil@libriciel.coop>
2026-06-11 16:44:54 +0200

    1493 - Fix de la pipeline

--- public.ssl/custom/templates/xhtml_mail.tpl.php
+++ public.ssl/custom/templates/xhtml_mail.tpl.php
@@ -6,11 +6,11 @@
         <title><?php echo $this->title ?></title>
         <meta http-equiv="content-type" content="text/html; charset=utf-8" />
         <meta name="robots" content="noindex">
-        <link rel="stylesheet" type="text/css" href="/custom/styles/bootstrap.min.css" />
-        <link rel="stylesheet" type="text/css" href="/custom/styles/bootstrap-theme.min.css" />
+        <link rel="stylesheet" type="text/css" href="/custom/styles/bootstrap5.min.css" />
         <link rel="stylesheet" type="text/css" href="/custom/styles/style_bs.css" />
         <link rel="stylesheet" type="text/css" href="/custom/styles/style_mail.css" />
         <script src="/javascript/utils.js" type="text/javascript"></script>
+        <script src="/custom/js/bootstrap.bundle.min.js" type="text/javascript"></script>
         <?php echo $this->header ?>
     </head>
     <body>
