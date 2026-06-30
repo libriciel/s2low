e6f4543a83bfd51b66c8b2b45f34865cf0dcfb3d
Julien LE GALL <julien.legall@libriciel.coop>
2026-06-30 11:29:22 +0200

    corrections suite retour CD

--- migrations/Version20260624120414.php
+++ migrations/Version20260624120414.php
@@ -7,40 +7,35 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
 final class Version20260624120414 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return 'Add helios_group_id and actes_group_id initialized from authority_group_id';
+        return 'Ajout des champs helios_group_id et actes_group_id à la table authorities. Ils sont initialisés avec la valeur de authority_group_id';
     }
 
     public function up(Schema $schema): void
     {
-        $this->addSql('
-        ALTER TABLE authorities
-            ADD helios_group_id INT DEFAULT NULL,
-            ADD actes_group_id INT DEFAULT NULL
-    ');
+        $this->addSql(
+            'ALTER TABLE authorities
+                    ADD helios_group_id INT DEFAULT NULL,
+                    ADD actes_group_id INT DEFAULT NULL'
+        );
 
-        $this->addSql('
-        UPDATE authorities
-        SET
-            helios_group_id = authority_group_id,
-            actes_group_id = authority_group_id
-    ');
+        $this->addSql(
+            'UPDATE authorities SET
+                    helios_group_id = authority_group_id,
+                    actes_group_id = authority_group_id'
+        );
 
     }
 
     public function down(Schema $schema): void
     {
-        $this->addSql('
-        ALTER TABLE authorities
-            DROP COLUMN helios_group_id,
-            DROP COLUMN actes_group_id
-    ');
-
+        $this->addSql(
+            'ALTER TABLE authorities
+                    DROP COLUMN helios_group_id,
+                    DROP COLUMN actes_group_id'
+        );
     }
 }
